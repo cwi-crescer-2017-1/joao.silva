7e9232f6a20c989ea355099f4727bb9b7b43fda2
JPedroSSilva <joao.silva@cwi.com.br>
2017-04-14 14:45:15 -0300

    refactor(SaintTest): Implementação de um único Saint para todos os testes possíveis
    Implementação de um Saint("allMight") para substituir todos outros Saints criados dentro de métodos possíveis.
    Sendo assim o número de objetos do tipo Saint dentro da classe passou de sete com atributos diferentes para um com atributos que variam de um método para outro.

--- SaintSeiya/SaintTest.java
+++ SaintSeiya/SaintTest.java
@@ -5,74 +5,75 @@ import org.junit.Test;
 
 
 public class SaintTest{
+   private Saint allMight;
    //ARMADURA
    @Test
    public void vestirArmaduraDeixaArmaduraVestida(){
        //ARRANGE
-       Saint marcolino = new Saint("Marcolino Pereira", new Armadura("Churrasqueira elétrica", Categoria.OURO));
+       allMight = new Saint("Ikki", new Armadura("Fênix", Categoria.BRONZE));
        //ACT
-       marcolino.vestirArmadura();
+       allMight.vestirArmadura();
        //ASSERT
-       assertEquals(true,marcolino.getArmaduraVestida());
+       assertEquals(true,allMight.getArmaduraVestida());
     }
    @Test
    public void naoVestirArmaduraDeixaArmaduraNaoVestida(){
        //ARRANGE
-       Saint irineu = new Saint("Irineu", new Armadura("Sabe de nada", Categoria.PRATA));
+       allMight = new Saint("Irineu", new Armadura("Sabe de nada", Categoria.PRATA));
        //ACT -- teste de pré definição
        //ASSERT
-       assertEquals(false,irineu.getArmaduraVestida());
+       assertEquals(false,allMight.getArmaduraVestida());
    }
    //GENERO
    @Test
    public void aoCriarSaintGeneroENaoInformado(){
        //ARRANGE
-       Saint shaka = new Saint("Shaka", new Armadura("Virgem", Categoria.OURO));
+       allMight = new Saint("Shaka", new Armadura("Virgem", Categoria.OURO));
        //ACT -- teste de pré definição
        //ASSERT
-       assertEquals(Genero.NAO_INFORMADO, shaka.getGenero());
+       assertEquals(Genero.NAO_INFORMADO, allMight.getGenero());
    }
    @Test 
    public void alterarSaintGenero(){
-       Saint seiya = new Saint("Seiya", new Armadura("Pegaso",Categoria.BRONZE));
-       seiya.setGenero(Genero.MASCULINO);
-       assertEquals(Genero.MASCULINO, seiya.getGenero());
+       allMight = new Saint("Seiya", new Armadura("Pegaso",Categoria.BRONZE));
+       allMight.setGenero(Genero.MASCULINO);
+       assertEquals(Genero.MASCULINO, allMight.getGenero());
    }
    //STATUS DE VIDA
    @Test
    public void aoCriarSaintStatusDeVidaEVivo(){
        //ARRANGE
-       Saint yolo = new Saint("YOLO", new Armadura("Vida", Categoria.OURO));
+       allMight = new Saint("YOLO", new Armadura("Vida", Categoria.OURO));
        //ACT -- teste de pré definição
        //ASSERT
-       assertEquals(StatusDeVida.VIVO, yolo.getStatusDeVida());
+       assertEquals(StatusDeVida.VIVO, allMight.getStatusDeVida());
     }
    @Test
    public void alterarStatusDeVida(){
        //ARRANGE
-       Saint yolodie = new Saint("YOLO", new Armadura("Vida", Categoria.OURO));
+       allMight = new Saint("Marcolino Pereira", new Armadura("Churrasqueira elétrica", Categoria.OURO));
        //ACT
-       yolodie.setStatusDeVida(StatusDeVida.MORTO);
+       allMight.setStatusDeVida(StatusDeVida.MORTO);
        //ASSERT
-       assertEquals(StatusDeVida.MORTO, yolodie.getStatusDeVida());
+       assertEquals(StatusDeVida.MORTO, allMight.getStatusDeVida());
    }
    //VIDA
    @Test
    public void aoCriarSaintVidaIgualCem(){
        //ARRANGE
-       Saint ikki = new Saint("Ikki", new Armadura("Fênix", Categoria.BRONZE));
+       allMight = new Saint("Monstro", new Armadura("Whey", Categoria.BRONZE));
        //ACT - teste de pré definição
        //ASSERT
-       assertEquals(100.0, ikki.getVida(), 0);
+       assertEquals(100.0, allMight.getVida(), 0);
    }
    @Test
    public void aoPerderVida(){
        //ARRANGE
-       Saint aioros = new Saint("Aioros", new Armadura("Sagitário", Categoria.OURO));
+       allMight = new Saint("Aioros", new Armadura("Sagitário", Categoria.OURO));
        //ACT
-       aioros.perderVida(100.0);
+       allMight.perderVida(100.0);
        //ASSERT
-       assertEquals(0.0, aioros.getVida(), 0);
+       assertEquals(0.0, allMight.getVida(), 0);
    }
     
 }
