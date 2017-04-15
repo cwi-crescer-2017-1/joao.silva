77ee9abf17fe26c273575dfb224434dc8382a578
JPedroSSilva <joao.silva@cwi.com.br>
2017-04-15 04:05:57 -0300

    fix(Batalha): Simplificação de código e melhoria na declaração de váriaveis
    1. Foi simplificado a váriavel array que era utilizada para guardar os dois lutadores, nesta atualização os lutadores passam a ser guardados nas váriaveis lutadorUm e lutadorDois.
    2. As variáveis valorCategoria1 e valorCategoria2 passaram a existir apenas no método iniciar() e deixaram de pertencer a toda a classe Batalha pelo fato de serem utilizadas somente neste método.

--- SaintSeiya/Batalha.java
+++ SaintSeiya/Batalha.java
@@ -1,17 +1,17 @@
 public class Batalha{
-    private Saint[] lutador = new Saint[2];
-    private int valorCategoria1,valorCategoria2;
+    private Saint lutadorUm;
+    private Saint lutadorDois;
     public Batalha(Saint lutadorUm, Saint lutadorDois){
-        this.lutador[0] = lutadorUm;
-        this.lutador[1] = lutadorDois;
+        this.lutadorUm = lutadorUm;
+        this.lutadorDois = lutadorDois;
     }
     public void iniciar(){
-      valorCategoria1= this.lutador[0].getArmadura().getCategoria().getValor();
-      valorCategoria2= this.lutador[1].getArmadura().getCategoria().getValor();
+      int valorCategoria1= this.lutadorUm.getArmadura().getCategoria().getValor();
+      int valorCategoria2= this.lutadorDois.getArmadura().getCategoria().getValor();
       if(valorCategoria2>valorCategoria1){ //Se a Categoria do lutador 2 for maior que a do 1 o lutador 1 perde vida
-          this.lutador[0].perderVida(10.0);
+          this.lutadorUm.perderVida(10.0);
       }else{ //Caso a categoria seja a mesma ou a do lutador 1 for maior o lutador 2 perde vida
-          this.lutador[1].perderVida(10.0);
+          this.lutadorDois.perderVida(10.0);
       }
     }
 }
