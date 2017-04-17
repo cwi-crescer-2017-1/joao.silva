5713b3f11f1668b4a399e11dfe1a7b89fe87f8da
JPedroSSilva <joao.silva@cwi.com.br>
2017-04-17 17:21:05 -0300

    fix(SaintTest): adaptação da classe de teste para utilizar as classes filhas BronzeSaint, PrataSaint e OuroSaint

--- SaintSeiya/SaintTest.java
+++ SaintSeiya/SaintTest.java
@@ -5,108 +5,109 @@ import org.junit.Test;
 
 
 public class SaintTest{
-   private Saint allMight;
    private BronzeSaint allMightBronze;
+   private PrataSaint allMightPrata;
+   private OuroSaint allMightOuro;
    private double vidaAnterior;
    //Número de testes: 10
    //ARMADURA
    @Test
    public void vestirArmaduraDeixaArmaduraVestida() throws Exception{
        //ARRANGE
-       allMight = new Saint("Ikki", new Armadura("Fênix", Categoria.BRONZE));
+       allMightBronze = new BronzeSaint("Ikki", new Armadura("Fênix", Categoria.BRONZE));
        //ACT
-       allMight.vestirArmadura();
+       allMightBronze.vestirArmadura();
        //ASSERT
-       assertEquals(true,allMight.getArmaduraVestida());
+       assertEquals(true,allMightBronze.getArmaduraVestida());
     }
    @Test
    public void naoVestirArmaduraDeixaArmaduraNaoVestida() throws Exception{
        //ARRANGE
-       allMight = new Saint("Irineu", new Armadura("Sabe de nada", Categoria.PRATA));
+       allMightPrata = new PrataSaint("Irineu", new Armadura("Sabe de nada", Categoria.PRATA));
        //ACT - TESTE DE PRÉ DEFINIÇÃO
        //ASSERT
-       assertEquals(false,allMight.getArmaduraVestida());
+       assertEquals(false,allMightPrata.getArmaduraVestida());
    }
    //GENERO
    @Test
    public void aoCriarSaintGeneroENaoInformado() throws Exception{
        //ARRANGE
-       allMight = new Saint("Shaka", new Armadura("Virgem", Categoria.OURO));
+       allMightOuro = new OuroSaint("Shaka", new Armadura("Virgem", Categoria.OURO));
        //ACT - TESTE DE PRÉ DEFINIÇÃO
        //ASSERT
-       assertEquals(Genero.NAO_INFORMADO, allMight.getGenero());
+       assertEquals(Genero.NAO_INFORMADO, allMightOuro.getGenero());
    }
    @Test 
    public void alterarSaintGenero() throws Exception{
        //ARRANGE
-       allMight = new Saint("Seiya", new Armadura("Pegaso",Categoria.BRONZE));
+       allMightBronze = new BronzeSaint("Seiya", new Armadura("Pegaso",Categoria.BRONZE));
        //ACT
-       allMight.setGenero(Genero.MASCULINO);
+       allMightBronze.setGenero(Genero.MASCULINO);
        //ASSERT
-       assertEquals(Genero.MASCULINO, allMight.getGenero());
+       assertEquals(Genero.MASCULINO, allMightBronze.getGenero());
        //ACT
-       allMight.setGenero(Genero.FEMININO);
+       allMightBronze.setGenero(Genero.FEMININO);
        //ASSERT
-       assertEquals(Genero.FEMININO, allMight.getGenero());
+       assertEquals(Genero.FEMININO, allMightBronze.getGenero());
    }
    //STATUS //Dica de nome para o método: statusInicialDeveSerVivo, achei mais explicativo 
    @Test
    public void aoCriarSaintStatusDeVidaEVivo() throws Exception{
        //ARRANGE
-       allMight = new Saint("YOLO", new Armadura("Virgem", Categoria.OURO));
+       allMightOuro = new OuroSaint("YOLO", new Armadura("Virgem", Categoria.OURO));
        //ACT - TESTE DE PRÉ DEFINIÇÃO
        //ASSERT
-       assertEquals(Status.VIVO, allMight.getStatus());
+       assertEquals(Status.VIVO, allMightOuro.getStatus());
     }
    //VIDA
    @Test
    public void aoCriarSaintVidaIgualCem() throws Exception{ //aVidaInicialDeveSer100 -> Dica de outro nome para o teste
        //ARRANGE
-       allMight = new Saint("Monstro", new Armadura("Whey", Categoria.BRONZE));
+       allMightBronze = new BronzeSaint("Monstro", new Armadura("Whey", Categoria.BRONZE));
        //ACT - TESTE DE PRÉ DEFINIÇÃO
        //ASSERT
-       assertEquals(100.0, allMight.getVida(), 0.01);
+       assertEquals(100.0, allMightBronze.getVida(), 0.01);
    }
    //aoPerderVida
    @Test
    public void aoPerderVinteDeVida() throws Exception{
        //ARRANGE
-       allMight = new Saint("Aioros", new Armadura("Sagitário", Categoria.OURO));
+       allMightOuro = new OuroSaint("Aioros", new Armadura("Sagitário", Categoria.OURO));
        //ACT
-       vidaAnterior=allMight.getVida();
-       allMight.perderVida(20.0);
+       vidaAnterior=allMightOuro.getVida();
+       allMightOuro.perderVida(20.0);
        //ASSERT
-       assertEquals(vidaAnterior-20.0, allMight.getVida(), 0.01);
+       assertEquals(vidaAnterior-20.0, allMightOuro.getVida(), 0.01);
    }
    @Test
    public void aoPerderDuzendoDeVida() throws Exception{ //Atributo vida sem contenção para limitar número minimo de vida a zero, ajustar caso seja criada uma conteção
        //ARRANGE
-       allMight = new Saint("Aioros", new Armadura("Sagitário", Categoria.OURO));
+       allMightOuro = new OuroSaint("Aioros", new Armadura("Sagitário", Categoria.OURO));
        //ACT
-       vidaAnterior=allMight.getVida();
-       allMight.perderVida(200.0);
+       vidaAnterior=allMightOuro.getVida();
+       allMightOuro.perderVida(200.0);
        //ASSERT
-       assertEquals(vidaAnterior-200.0, allMight.getVida(), 0.01);
+       assertEquals(vidaAnterior-200.0, allMightOuro.getVida(), 0.01);
    }
    @Test
    public void aoPerderNumeroQuebradoDeVida() throws Exception{
        //ARRANGE
-       allMight = new Saint("Aioros", new Armadura("Sagitário", Categoria.OURO));
+       allMightOuro = new OuroSaint("Aioros", new Armadura("Sagitário", Categoria.OURO));
        //ACT
-       vidaAnterior=allMight.getVida();
-       allMight.perderVida(3.141592);
+       vidaAnterior=allMightOuro.getVida();
+       allMightOuro.perderVida(3.141592);
        //ASSERT
-       assertEquals(vidaAnterior-3.141592, allMight.getVida(), 0.01);
+       assertEquals(vidaAnterior-3.141592, allMightOuro.getVida(), 0.01);
    }
    @Test
    public void aoPerderNumeroNegativoDeVida() throws Exception{ //Teste deve ser removido caso se evite números negativos no método PerderVida()
        //ARRANGE
-       allMight = new Saint("Aioros", new Armadura("Sagitário", Categoria.OURO));
+       allMightOuro = new OuroSaint("Aioros", new Armadura("Sagitário", Categoria.OURO));
        //ACT
-       vidaAnterior=allMight.getVida();
-       allMight.perderVida(-100.0);
+       vidaAnterior=allMightOuro.getVida();
+       allMightOuro.perderVida(-100.0);
        //ASSERT
-       assertEquals(vidaAnterior+100.0, allMight.getVida(), 0.01);
+       assertEquals(vidaAnterior+100.0, allMightOuro.getVida(), 0.01);
    }
    //SENTIDOS
    @Test
@@ -120,24 +121,24 @@ public class SaintTest{
    @Test
    public void aoCriarSaintPrataNasceCom6SentidosDespertados() throws Exception{
        //ARRANGE
-       allMight = new Saint("Irineu", new Armadura("Sabe de nada", Categoria.PRATA));
+       allMightPrata = new PrataSaint("Irineu", new Armadura("Sabe de nada", Categoria.PRATA));
        //ACT - TESTE DE PRÉ DEFINIÇÃO
        //ASSERT
-       assertEquals(6, allMight.getQtSentidosDespertados());
+       assertEquals(6, allMightPrata.getQtSentidosDespertados());
    }
    @Test
    public void aoCriarSaintOuroNasceCom7SentidosDespertados() throws Exception{
        //ARRANGE
-       allMight = new Saint("Aioros", new Armadura("Sagitário", Categoria.OURO));
+       allMightOuro = new OuroSaint("Aioros", new Armadura("Sagitário", Categoria.OURO));
        //ACT - TESTE DE PRÉ DEFINIÇÃO
        //ASSERT
-       assertEquals(7, allMight.getQtSentidosDespertados());
+       assertEquals(7, allMightOuro.getQtSentidosDespertados());
    }
    //CONSTELAÇÃO
    @Test(expected=Exception.class)
    public void constelacaoInvalidaDeOuroDeveLancarErro() throws Exception{
        //ARRANGE
-       allMight = new Saint("YOLO", new Armadura("Café", Categoria.OURO));
+       allMightOuro = new OuroSaint("YOLO", new Armadura("Café", Categoria.OURO));
        //ACT - TESTE DE PRÉ DEFINIÇÃO
        //ASSERT - TESTE DE DECLARAÇÃO DE VARIÁVEL
     }
