e556e173a9f96470b92faecb39b0cf93e2892402
JPedroSSilva <joao.silva@cwi.com.br>
2017-04-17 17:20:05 -0300

    refactor(Saint): otimização da classe passando partes dela para suas classes filhas especificas

--- SaintSeiya/Saint.java
+++ SaintSeiya/Saint.java
@@ -6,33 +6,12 @@ public class Saint{
     private boolean armaduraVestida;
     private double vida = 100.0;
     protected int qtSentidosDespertados;
-    
+    //Extendem Saint: BronzeSaint, PrataSaint e OuroSaint
     public Saint(String nome, Armadura armadura) throws Exception{
        this.nome = nome;
        this.armadura = armadura;
        //O valor de qtSentidosDespertados soma com o valor da Categoria da Armadura, BRONZE(0), PRATA(1) e OURO(2)
        //logo Ouro fica com (5+2=7) sentidos e Prata (5+1=6) sentidos  
-       if(this.armadura.getCategoria() == Categoria.OURO){
-           this.qtSentidosDespertados = 7; 
-           String constelacao = armadura.getConstelacao();
-           if(!constelacao.equals("Áries")
-            && !constelacao.equals("Touro")
-            && !constelacao.equals("Gêmeos")
-            && !constelacao.equals("Câncer")
-            && !constelacao.equals("Virgem")
-            && !constelacao.equals("Leão")
-            && !constelacao.equals("Libra")
-            && !constelacao.equals("Escorpião")
-            && !constelacao.equals("Sagitário")
-            && !constelacao.equals("Capricórnio")
-            && !constelacao.equals("Aquário")
-            && !constelacao.equals("Peixes")){
-               //ERRO
-               throw new Exception("Constelação inválida");
-           }
-        }else if(this.armadura.getCategoria() == Categoria.PRATA){
-            this.qtSentidosDespertados = 6;
-        }
     }
     //ARMADURA
     public Armadura getArmadura(){
