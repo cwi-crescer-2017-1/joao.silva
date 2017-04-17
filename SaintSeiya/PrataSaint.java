5d9703f2f607fda88f1f81955b7beead699a6a3c
JPedroSSilva <joao.silva@cwi.com.br>
2017-04-17 17:18:38 -0300

    feat(PrataSaint): implementação da classe PrataSaint que extende a classe Saint

---
+++ SaintSeiya/PrataSaint.java
@@ -0,0 +1,6 @@
+public class PrataSaint extends Saint{
+    public PrataSaint(String nome, Armadura armadura) throws Exception{
+        super(nome, armadura);
+        this.qtSentidosDespertados = 6;
+    }
+}
