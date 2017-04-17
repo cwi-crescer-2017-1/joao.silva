eed6e00b3f3e6e67115618e041fd43fc0faa5940
JPedroSSilva <joao.silva@cwi.com.br>
2017-04-17 17:18:13 -0300

    feat(OuroSaint): Implementação da classe de OuroSaint que extende a classe Saint

---
+++ SaintSeiya/OuroSaint.java
@@ -0,0 +1,22 @@
+public class OuroSaint extends Saint{
+    public OuroSaint(String nome, Armadura armadura) throws Exception{
+        super(nome, armadura);
+        this.qtSentidosDespertados = 7;
+        String constelacao = armadura.getConstelacao();
+        if(!constelacao.equals("Áries")
+            && !constelacao.equals("Touro")
+            && !constelacao.equals("Gêmeos")
+            && !constelacao.equals("Câncer")
+            && !constelacao.equals("Virgem")
+            && !constelacao.equals("Leão")
+            && !constelacao.equals("Libra")
+            && !constelacao.equals("Escorpião")
+            && !constelacao.equals("Sagitário")
+            && !constelacao.equals("Capricórnio")
+            && !constelacao.equals("Aquário")
+            && !constelacao.equals("Peixes")){
+               //ERRO
+               throw new Exception("Constelação inválida");
+        }
+    }
+}
