02a588d7459a1b50e8d029bed396c1c54e6311f6
JPedroSSilva <joao.silva@cwi.com.br>
2017-04-17 22:48:01 -0300

    feat(Golpe): Adição de novos métodos para pegar valores de atributos da classe
    Implementação dos métodos:
    - getNome()
    - getFatorDano()
     e do construtor:
    - Golpe(String, int)

--- SaintSeiya/Golpe.java
+++ SaintSeiya/Golpe.java
@@ -1,4 +1,14 @@
 public class Golpe{
    private String nome;
    private int fatorDano;
+   public Golpe(String nome, int fatorDano){
+       this.nome = nome;
+       this.fatorDano = fatorDano;
+   }
+   public String getNome(){
+       return this.nome;
+   }
+   public int getFatorDano(){
+       return this.fatorDano;
+   }
 }
