803dcd2b09c0f3650a99182bd5ef47e5aed40260
JPedroSSilva <joao.silva@cwi.com.br>
2017-04-18 22:18:28 -0300

    feat(ListaSaints): finalização da implementação de métodos
    Finalização da implementação de todos os métodos pedidos no exercicio 2 da lista 3.

--- SaintSeiya/ListaSaints.java
+++ SaintSeiya/ListaSaints.java
@@ -1,27 +1,80 @@
 import java.util.ArrayList;
 public class ListaSaints{
     private ArrayList<Saint> lista = new ArrayList<Saint>();
+    public ListaSaints(){ }
     public void adicionar(Saint saint){
-        lista.add(saint);
+        this.lista.add(saint);
     }
     public Saint get(int indice){
-        return lista.get(indice);
+        return this.lista.get(indice);
     }
     public ArrayList<Saint> todos(){
-        return lista;
+        return this.lista;
     }
     public void remover(Saint saint){
-        lista.remove(saint);
+        this.lista.remove(saint);
     }
-    /*
     public Saint buscarPorNome(String nome){
-        Saint retornar;
         for(int i=0;i<this.lista.size();i++){
            if(this.lista.get(i).getNome().equals(nome)){
-                retornar = this.lista.get(i);
+               return this.lista.get(i);
            }
         }
-        return retornar;
+        return null;
     }
-    */
+    public ArrayList<Saint> buscarPorCategoria(Categoria categoria){
+        ArrayList<Saint> retorno = new ArrayList<Saint>();
+        for(Saint saint : lista){
+            if(saint.getArmadura().getCategoria() == categoria){
+                retorno.add(saint);
+            }
+        }
+        return retorno;
+    }
+    public ArrayList<Saint> buscarPorStatus(Status status){
+        ArrayList<Saint> retorno = new ArrayList<Saint>();
+        for(Saint saint : lista){
+            if(saint.getStatus() == status){
+                retorno.add(saint);
+            }
+        }
+        return retorno;
+    }
+    public Saint getSaintMaiorVida(){
+        Saint saintMaiorVida=null;
+        double maior = 0;
+        for(Saint saint : lista){
+            if(saint.getVida() > maior){
+                saintMaiorVida = saint;
+                maior=saint.getVida();
+            }
+        }
+        return saintMaiorVida;
+    }
+    public Saint getSaintMenorVida(){
+        Saint saintMenorVida=null;
+        double menor = 999999999;
+        for(Saint saint : lista){
+            if(saint.getVida() < menor){
+                saintMenorVida = saint;
+                menor = saint.getVida();
+            }
+        }
+        return saintMenorVida;
+    }
+    public void ordenar(){
+        double menor;
+        for(int x=0;x<this.lista.size();x++){ 
+            Saint saintTemp = null;
+            menor = 999999999;
+            for(int i=x;i<this.lista.size();i++){
+                if(this.get(i).getVida() < menor){
+                    menor=this.lista.get(i).getVida();
+                    saintTemp=this.get(x);
+                    lista.set(x,this.get(i));
+                    lista.set(i,saintTemp);
+                }
+            }
+        }
+    }
 }
