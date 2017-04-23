8bc35875e8c2d3d48d80c577071b4629d56296d4
JPedroSSilva <joao.silva@cwi.com.br>
2017-04-23 20:31:08 -0300

    feat(Golpear): feat(VestirArmadura): implementação da classe Golpear que implementa a interface Movimento

---
+++ SaintSeiya/Golpear.java
@@ -0,0 +1,27 @@
+public class Golpear implements Movimento{
+    private Saint golpeador,golpeado;
+    private int dano = 0;
+    public void executar(){
+       if(this.golpeador!=null){ //Só inicia se houver um golpeador
+           Golpe proximoGolpe = this.golpeador.getProximoGolpe();
+           if(proximoGolpe!=null){ //Só valida o dano se existir um proximoGolpe
+               this.dano = proximoGolpe.getFatorDano();
+               boolean armaduraVestida = this.golpeador.getArmaduraVestida();
+               if(armaduraVestida){
+                   this.dano = dano * (1+golpeador.getArmadura().getCategoria().getValor()); 
+                   //O dano bruto do golpe é multiplicado por: 1 + Valor da Categoria da Armadura caso a armadura esteja vestida
+               }
+           }
+       }
+       if(golpeado!=null){ //Caso o golpeado não exista o golpe não é realizado mas tem seu dano salvo
+           this.golpeado.perderVida(dano);
+       }
+    }
+    public Golpear(Saint golpeador, Saint golpeado){
+        this.golpeador = golpeador;
+        this.golpeado = golpeado;
+    }
+    public int getDano(){
+        return this.dano;
+    }
+}
---
+++ SaintSeiya/GolpearTest.java
@@ -0,0 +1,147 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GolpearTest{
+  Saint saint1, saint2, saint3;
+  Movimento movimento1,movimento2;
+  Golpear golpear1;
+  double vidaInicial;
+  @Test
+  public void umSaintSemArmaduraVestidaAplicaUmGolpeEmOutroSaint() throws Exception{
+      //ARRANGE
+      this.saint1 = new BronzeSaint("Ikki","Fênix"); 
+      this.saint2 = new PrataSaint("Dio","Mosca");
+      this.saint1.aprenderGolpe(new Golpe("Soco",5));
+      vidaInicial = this.saint2.getVida();
+      this.movimento1 = new Golpear(saint1, saint2);
+      //ACT
+      this.movimento1.executar();
+      //ASSERT
+      assertEquals(vidaInicial-5,this.saint2.getVida(),0.01);
+  }
+  @Test
+  public void umSaintBronzeComArmaduraVestidaAplicaUmGolpeEmOutroSaint() throws Exception{
+      //ARRANGE
+      this.saint1 = new BronzeSaint("Ikki","Fênix"); 
+      this.saint2 = new PrataSaint("Dio","Mosca");
+      this.saint1.aprenderGolpe(new Golpe("Soco",5));
+      this.saint1.vestirArmadura();
+      //vidaInicial guarda a vida do saint2 antes de tomar os golpes
+      vidaInicial = this.saint2.getVida();
+      this.movimento1 = new Golpear(saint1, saint2);
+      //ACT
+      this.movimento1.executar();
+      //ASSERT
+      assertEquals(vidaInicial-(5*2),this.saint2.getVida(),0.01);
+  }
+  @Test
+  public void umSaintOuroComArmaduraVestidaAplicaUmGolpeEmOutroSaint() throws Exception{
+      //ARRANGE
+      this.saint1 = new OuroSaint("Aioros","Sagitário"); 
+      this.saint2 = new PrataSaint("Dio","Mosca");
+      this.saint1.aprenderGolpe(new Golpe("Soco",5));
+      this.saint1.vestirArmadura();
+      vidaInicial = this.saint2.getVida();
+      this.movimento1 = new Golpear(saint1, saint2);
+      //ACT
+      this.movimento1.executar();
+      //ASSERT
+      assertEquals(vidaInicial-(5*4),this.saint2.getVida(),0.01);
+  }
+  @Test
+  public void umSaintPrataComArmaduraVestidaAplicaUmGolpeEmOutroSaint() throws Exception{
+      //ARRANGE
+      this.saint1 = new PrataSaint("Dio","Mosca"); 
+      this.saint2 = new BronzeSaint("Ikki","Fênix");
+      this.saint1.aprenderGolpe(new Golpe("Soco",5));
+      this.saint1.vestirArmadura();
+      vidaInicial = this.saint2.getVida();
+      this.movimento1 = new Golpear(saint1, saint2);
+      //ACT
+      this.movimento1.executar();
+      //ASSERT
+      assertEquals(vidaInicial-(5*3),this.saint2.getVida(),0.01);
+  }
+  @Test
+  public void doisSaintsComArmaduraVestidaAplicamUmGolpeCadaEmOutroSaint() throws Exception{
+      //ARRANGE
+      this.saint1 = new BronzeSaint("Ikki","Fênix"); 
+      this.saint1.aprenderGolpe(new Golpe("Soco",5));
+      this.saint1.vestirArmadura();
+      
+      this.saint2 = new PrataSaint("Dio","Mosca");
+      this.saint2.aprenderGolpe(new Golpe("Chute",10));
+      this.saint2.vestirArmadura();
+      
+      this.saint3 = new OuroSaint("Aioros","Sagitário");
+
+      //vidaInicial guarda a vida do saint3 antes de tomar os golpes
+      vidaInicial = this.saint3.getVida();
+      //declara o golpe do saint1 no saint3
+      this.movimento1 = new Golpear(saint1, saint3);
+      //declara o golpe do saint2 no saint3
+      this.movimento2 = new Golpear(saint2, saint3);
+      //ACT
+      //realiza o primeiro movimento
+      this.movimento1.executar();
+      //realiza o segundo movimento
+      this.movimento2.executar();
+      //ASSERT
+      assertEquals(vidaInicial-((5*2)+(10*3)),this.saint3.getVida(),0.01);
+  }
+  @Test
+  public void umSaintsComArmaduraVestidaAplicamUmGolpeMortalEmOutroSaint() throws Exception{
+      //ARRANGE
+      this.saint1 = new BronzeSaint("Ikki","Fênix"); 
+      this.saint1.aprenderGolpe(new Golpe("Super soco",50));
+      this.saint1.vestirArmadura();
+     
+      this.saint2 = new PrataSaint("Dio","Mosca"); // Deve iniciar com 100 de vida
+      
+      vidaInicial = this.saint2.getVida();
+      this.movimento1 = new Golpear(saint1, saint2);
+      //ACT
+      this.movimento1.executar();
+      //ASSERT
+      assertEquals(vidaInicial-((50*2)),this.saint2.getVida(),0.01);
+      assertEquals(Status.MORTO,this.saint2.getStatus());
+  }
+  @Test
+  public void umSaintComArmaduraVestidaAplicamUmGolpeEmUmSaintVazio() throws Exception{
+      //ARRANGE
+      this.saint1 = new BronzeSaint("Ikki","Fênix"); 
+      this.saint1.aprenderGolpe(new Golpe("Soco",5));
+      this.saint1.vestirArmadura();
+
+      this.golpear1 = new Golpear(saint1, saint2);
+      
+      //ACT
+      this.golpear1.executar(); 
+      
+      //ASSERT
+     assertEquals(10,this.golpear1.getDano()); //Movimento não implementa getDano, logo golpear não pode ser do tipo Movimento
+  }
+  @Test
+  public void umSaintComArmaduraVestidaAplicaDoisGolpesEmOutroSaint() throws Exception{
+      //ARRANGE
+      this.saint1 = new BronzeSaint("Ikki","Fênix"); 
+      this.saint1.aprenderGolpe(new Golpe("Soco",5));
+      this.saint1.aprenderGolpe(new Golpe("Chute", 10));
+      this.saint1.vestirArmadura();
+      
+      this.saint2 = new PrataSaint("Dio","Mosca"); 
+     
+      vidaInicial = this.saint2.getVida();
+      
+      //O esperado é que seja dado por primeiro o primeiro golpe registrado e por segundo o segundo golpe registrado
+      this.movimento1 = new Golpear(this.saint1, this.saint2);
+      this.movimento2 = new Golpear(this.saint1, this.saint2);
+      //ACT
+      this.movimento1.executar();
+      this.movimento2.executar();
+      //ASSERT
+      assertEquals(vidaInicial-((5*2)+(10*2)),this.saint2.getVida(),0.01);
+  }
+}
