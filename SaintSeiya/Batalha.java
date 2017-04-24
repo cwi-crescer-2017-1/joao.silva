9360d21ce2c02912b947f5314635c8488651efc4
JPedroSSilva <joao.silva@cwi.com.br>
2017-04-24 16:25:41 -0300

    doc(Comentario): remoção de comentário na classe Batalha

--- SaintSeiya/Batalha.java
+++ SaintSeiya/Batalha.java
@@ -47,29 +47,5 @@ public class Batalha{
                 golpeadorDoTurno=lutadorUm;
             }
       }while(aindaEstaoVivos);
-      /* Código antigo, antes da simplificação e de outras melhoras
-      Saint[] golpeador = new Saint[2];
-      int turno = 0;// - Irá variar entre 0 e 1 no calculo turno%2
-      int proximoTurno = 1;
-      if(valorCategoria2>valorCategoria1){ //Se a Categoria do lutador 2 for maior que a do 1 o lutador 2 inicia golpeando
-          golpeador[0]=lutadorDois; //LutadorDois golpeia primeiro
-          golpeador[1]=lutadorUm;
-      }else{ //Caso a categoria seja a mesma ou a do lutador 1 for maior o lutador1 inicia golpeando
-          golpeador[0]=lutadorUm; // LutadorUm golpeia primeiro
-          golpeador[1]=lutadorDois;
-      }
-      if(golpeador[0].getMovimentos().isEmpty()){ //Caso não possua movimentos
-          golpeador[0].adicionarMovimento(new Nada()); //Ganha um movimento de fazer nada
-      }
-      if(golpeador[1].getMovimentos().isEmpty()){ //Caso não possua movimentos
-          golpeador[1].adicionarMovimento(new Nada()); //Ganha um movimento de fazer nada
-      }
-      do{
-          golpeador[turno%2].getProximoMovimento().executar();
-          aindaEstaoVivos = golpeador[proximoTurno%2].getStatus() == Status.VIVO;
-          turno=proximoTurno;
-          proximoTurno++;
-      }while(aindaEstaoVivos && turno<1000);//A luta está virando a Guerra dos mil dias = empate*/
     }
-    
 }
