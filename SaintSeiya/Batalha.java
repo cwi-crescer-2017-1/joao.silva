f44fa62e73111dcdd7ea4f87dbfcc3d41b22eaee
JPedroSSilva <joao.silva@cwi.com.br>
2017-04-24 14:47:16 -0300

    refactor(Batalha): implementação de mudanças para a simplificação da classe Batalha e da melhorá de performance

--- SaintSeiya/Batalha.java
+++ SaintSeiya/Batalha.java
@@ -7,8 +7,54 @@ public class Batalha{
     public void iniciar() throws Exception{
       int valorCategoria1= this.lutadorUm.getArmadura().getCategoria().getValor();
       int valorCategoria2= this.lutadorDois.getArmadura().getCategoria().getValor();
-      Saint[] golpeador = new Saint[2];
       boolean aindaEstaoVivos;
+      boolean lutadorUmSemMovimentos= lutadorUm.getMovimentos().size()==0;
+      boolean lutadorDoisSemMovimentos = lutadorDois.getMovimentos().size()==0;
+      Saint golpeadorDoTurno;
+      int dano=0;
+      
+      //Seleciona o primeiro golpeador
+      if(valorCategoria2>valorCategoria1){ //Se a Categoria do lutador 2 for maior que a do 1 o lutador 2 inicia golpeando
+          golpeadorDoTurno=lutadorDois; //LutadorDois golpeia primeiro
+      }else{ //Caso a categoria seja a mesma ou a do lutador 1 for maior o lutador1 inicia golpeando
+          golpeadorDoTurno=lutadorUm; // LutadorUm golpeia primeiro
+      }
+      
+      //Verificação de movimento existente
+      if(lutadorUmSemMovimentos && lutadorDoisSemMovimentos){
+          return; //Ambos lutadores não possuem movimentos
+      }else{
+          if(lutadorUmSemMovimentos){
+              lutadorUm.adicionarMovimento(new Nada());//Lutador 1 não possue movimento mas o Lutador 2 possui
+          }
+          if(lutadorDoisSemMovimentos){
+              lutadorUm.adicionarMovimento(new Nada());//Lutador 1 não possue movimento mas o Lutador ' possui
+          }
+      }
+      
+      //Verificação de dano existente
+      for(int i=0;i<lutadorUm.getGolpes().size();i++){
+         dano+=lutadorUm.getProximoGolpe().getFatorDano();
+      }
+      for(int i=0;i<lutadorDois.getGolpes().size();i++){
+         dano+=lutadorDois.getProximoGolpe().getFatorDano();
+      }
+      if(dano==0){
+          return; //Lutadores não possuem dano
+      }
+      
+      //Realização da troca de golpes
+      do{
+          golpeadorDoTurno.getProximoMovimento().executar();
+          aindaEstaoVivos=lutadorUm.getStatus() == Status.VIVO && lutadorDois.getStatus() == Status.VIVO;
+          if(golpeadorDoTurno==lutadorUm){
+              golpeadorDoTurno=lutadorDois;
+            }else{//golpeadorDoTurno==lutadorDois
+                golpeadorDoTurno=lutadorUm;
+            }
+      }while(aindaEstaoVivos);
+      /* Código antigo, antes da simplificação e de outras melhoras
+      Saint[] golpeador = new Saint[2];
       int turno = 0;// - Irá variar entre 0 e 1 no calculo turno%2
       int proximoTurno = 1;
       if(valorCategoria2>valorCategoria1){ //Se a Categoria do lutador 2 for maior que a do 1 o lutador 2 inicia golpeando
@@ -29,7 +75,7 @@ public class Batalha{
           aindaEstaoVivos = golpeador[proximoTurno%2].getStatus() == Status.VIVO;
           turno=proximoTurno;
           proximoTurno++;
-      }while(aindaEstaoVivos && turno<1000);//A luta está virando a Guerra dos mil dias = empate
+      }while(aindaEstaoVivos && turno<1000);//A luta está virando a Guerra dos mil dias = empate*/
     }
     
 }
