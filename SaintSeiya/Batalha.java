259401e1f66504ba29ace1d91c6c21f287969743
JPedroSSilva <joao.silva@cwi.com.br>
2017-04-23 22:44:00 -0300

    feat(batalha): modificação de métodos na classe Batalha e de teste para estes métodos na classe BatalhaTest
    Foi modificado o método iniciar() da classe Batalha afim de realizar uma batalha até a morte ou empate, que anteriormente era até o primeiro golpe apenas.

--- SaintSeiya/Batalha.java
+++ SaintSeiya/Batalha.java
@@ -7,13 +7,29 @@ public class Batalha{
     public void iniciar() throws Exception{
       int valorCategoria1= this.lutadorUm.getArmadura().getCategoria().getValor();
       int valorCategoria2= this.lutadorDois.getArmadura().getCategoria().getValor();
-      final double dano = 10.0;
-      /*o final garante que o valor não será alterado de forma indesejável durante o código e 
-      ao ler o código o desenvolvedor já entende que o valor não deve ser alterado*/
-      if(valorCategoria2>valorCategoria1){ //Se a Categoria do lutador 2 for maior que a do 1 o lutador 1 perde vida
-          this.lutadorUm.perderVida(dano);
-      }else{ //Caso a categoria seja a mesma ou a do lutador 1 for maior o lutador 2 perde vida
-          this.lutadorDois.perderVida(dano);
+      Saint[] golpeador = new Saint[2];
+      boolean aindaEstaoVivos;
+      int turno = 0;// - Irá variar entre 0 e 1 no calculo turno%2
+      int proximoTurno = 1;
+      if(valorCategoria2>valorCategoria1){ //Se a Categoria do lutador 2 for maior que a do 1 o lutador 2 inicia golpeando
+          golpeador[0]=lutadorDois; //LutadorDois golpeia primeiro
+          golpeador[1]=lutadorUm;
+      }else{ //Caso a categoria seja a mesma ou a do lutador 1 for maior o lutador1 inicia golpeando
+          golpeador[0]=lutadorUm; // LutadorUm golpeia primeiro
+          golpeador[1]=lutadorDois;
       }
+      if(golpeador[0].getMovimentos().isEmpty()){ //Caso não possua movimentos
+          golpeador[0].adicionarMovimento(new Nada()); //Ganha um movimento de fazer nada
+      }
+      if(golpeador[1].getMovimentos().isEmpty()){ //Caso não possua movimentos
+          golpeador[1].adicionarMovimento(new Nada()); //Ganha um movimento de fazer nada
+      }
+      do{
+          golpeador[turno%2].getProximoMovimento().executar();
+          aindaEstaoVivos = golpeador[proximoTurno%2].getStatus() == Status.VIVO;
+          turno=proximoTurno;
+          proximoTurno++;
+      }while(aindaEstaoVivos && turno<1000);//A luta está virando a Guerra dos mil dias = empate
     }
+    
 }
--- SaintSeiya/BatalhaTest.java
+++ SaintSeiya/BatalhaTest.java
@@ -6,45 +6,115 @@ import org.junit.Test;
 public class BatalhaTest{
    private BronzeSaint seiya;
    private PrataSaint marin, algol;
-   private double vidaInicial1, vidaInicial2;
+   private OuroSaint aiolos,shura;
+   private Golpe golpear;
    //Número de testes: 3
+   
    @Test
-   public void aoBatalharCategoriaMaiorContraCategoriaMenor() throws Exception{ //Categoria do Lutador 1 maior que a do Lutador 2
+       public void aoBatalharCategoriaMenorContraCategoriaMaior() throws Exception{ //Categoria do Lutador 1 maior que a do Lutador 2
        //ARRANGE
        seiya = new BronzeSaint("Seiya", "Pégaso");
+       seiya.aprenderGolpe(new Golpe("Soco",5));
+       seiya.adicionarMovimento(new VestirArmadura(seiya));
+       
        marin = new PrataSaint("Marin", "Águia");
-       vidaInicial1 = seiya.getVida();
-       vidaInicial2 = marin.getVida();
+       marin.aprenderGolpe(new Golpe("Soco",5));
+       marin.adicionarMovimento(new VestirArmadura(marin));
+       
+       seiya.adicionarMovimento(new Golpear(seiya,marin));
+       marin.adicionarMovimento(new Golpear(marin,seiya));
+       
        //ACT
        new Batalha(seiya,marin).iniciar();
+       
        //ASSERT
-       assertEquals(vidaInicial1-10.0, seiya.getVida(), 0.01);
-       assertEquals(vidaInicial2, marin.getVida(), 0.01);
-    }
+       assertEquals(0, seiya.getVida(), 0.01);
+       assertEquals(Status.MORTO, seiya.getStatus());
+       assertEquals(Status.VIVO, marin.getStatus());
+   }
    @Test 
-   public void aoBatalharCategoriaMenorContraCategoriaMaior()throws Exception{ //Cegoria do Lutador 2 maior que a do Lutador 1
+   public void aoBatalharCategoriaMaiorContraCategoriaMenor()throws Exception{ //Cegoria do Lutador 2 maior que a do Lutador 1
        //ARRANGE
        seiya = new BronzeSaint("Seiya", "Pégaso");
+       seiya.aprenderGolpe(new Golpe("Soco",5));
+       seiya.adicionarMovimento(new VestirArmadura(seiya));
+       
        marin = new PrataSaint("Marin", "Águia");
-       vidaInicial1 = seiya.getVida();
-       vidaInicial2 = marin.getVida();
+       marin.aprenderGolpe(new Golpe("Soco",5));
+       marin.adicionarMovimento(new VestirArmadura(marin));
+       
+       seiya.adicionarMovimento(new Golpear(seiya,marin));
+       marin.adicionarMovimento(new Golpear(marin,seiya));
+       
        //ACT
        new Batalha(marin,seiya).iniciar();
+       
        //ASSERT
-       assertEquals(vidaInicial1-10.0, seiya.getVida(), 0.01);
-       assertEquals(vidaInicial2, marin.getVida(), 0.01);
+       assertEquals(0, seiya.getVida(), 0.01);
+       assertEquals(Status.MORTO, seiya.getStatus());
+       assertEquals(Status.VIVO, marin.getStatus());
     }
    @Test
    public void aoBatalharCategoriasIguais() throws Exception{
        //ARRANGE
-       marin = new PrataSaint("Marin", "Águia");
        algol = new PrataSaint("Algol", "Perseu");
-       vidaInicial1 = algol.getVida();
-       vidaInicial2 = marin.getVida();
+       algol.aprenderGolpe(new Golpe("Soco",5));
+       algol.adicionarMovimento(new VestirArmadura(algol));
+       
+       marin = new PrataSaint("Marin", "Águia");
+       marin.aprenderGolpe(new Golpe("Soco",5));
+       marin.adicionarMovimento(new VestirArmadura(marin));
+       
+       algol.adicionarMovimento(new Golpear(algol,marin));
+       marin.adicionarMovimento(new Golpear(marin,algol));
+       
        //ACT
-       new Batalha(marin,algol).iniciar();
+       new Batalha(algol,marin).iniciar();
+       
        //ASSERT
-       assertEquals(vidaInicial1-10.0, algol.getVida(), 0.01);
-       assertEquals(vidaInicial2, marin.getVida(), 0.01);
-    }
+       assertEquals(0, marin.getVida(), 0.01);
+       assertEquals(Status.MORTO, marin.getStatus());
+       assertEquals(Status.VIVO, algol.getStatus());
+   }
+   @Test
+   public void aoBatalharSaintPrataContraSaintSemMovimentos() throws Exception{
+       //ARRANGE
+       algol = new PrataSaint("Algol", "");
+       
+       marin = new PrataSaint("Marin", "Águia");
+       marin.aprenderGolpe(new Golpe("Soco",5));
+       marin.adicionarMovimento(new VestirArmadura(marin));
+       
+       marin.adicionarMovimento(new Golpear(marin,algol));
+       
+       //ACT
+       new Batalha(algol,marin).iniciar();
+       
+       //ASSERT
+       assertEquals(0, algol.getVida(), 0.01);
+       assertEquals(Status.MORTO, algol.getStatus());
+       assertEquals(Status.VIVO, marin.getStatus());
+   }
+   @Test
+   public void aoBatalharDoisSaintsDeOuroSemDanoNosMovimentos() throws Exception{
+       //ARRANGE
+       aiolos = new OuroSaint("Aiolos", "Sagitário");
+       aiolos.aprenderGolpe(new Golpe("Saudação",0));
+       aiolos.adicionarMovimento(new VestirArmadura(aiolos));
+       
+       shura = new OuroSaint("Shura", "Capricórnio");
+       shura.aprenderGolpe(new Golpe("Saudação",0));
+       shura.adicionarMovimento(new VestirArmadura(shura));
+       
+       shura.adicionarMovimento(new Golpear(shura,aiolos));
+       aiolos.adicionarMovimento(new Golpear(aiolos,shura));
+       
+       //ACT
+       new Batalha(aiolos,shura).iniciar();
+       
+       //ASSERT
+       assertEquals(Status.VIVO, aiolos.getStatus());
+       assertEquals(Status.VIVO, shura.getStatus());
+   }
 }
+   
