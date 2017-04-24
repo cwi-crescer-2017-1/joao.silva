027e0a7d1b6472c7b2450d71aa5cc5516d4a5bd0
JPedroSSilva <joao.silva@cwi.com.br>
2017-04-24 16:27:04 -0300

    fix(VestirArmadura): correção, remoção de trecho de código não utilizado

--- SaintSeiya/VestirArmadura.java
+++ SaintSeiya/VestirArmadura.java
@@ -2,7 +2,7 @@ public class VestirArmadura implements Movimento{
     private Saint saint;
     public void executar(){
         this.saint.vestirArmadura();
-        saint.removerMovimento(this);
+        //saint.removerMovimento(this);
     }
     public VestirArmadura(Saint saint) throws Exception{
         this.saint = saint;
