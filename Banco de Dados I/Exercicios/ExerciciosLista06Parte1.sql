ceeac95da1050cc2c912d1940742c9fa5657af12
JPedroSSilva <joao.silva@cwi.com.br>
2017-05-02 23:45:03 -0300

    feat(Lista06Parte2): implementação dos scripts referentes as respostas dos exercicios da Parte 2 da Lista 06

--- Banco de Dados I/Exercicios/ExerciciosLista06Parte1.sql
+++ Banco de Dados I/Exercicios/ExerciciosLista06Parte1.sql
@@ -3,7 +3,6 @@
 USE[Crescer17];
 
 --Exercicio 01
-
 SELECT TOP 1 SUBSTRING(C.Nome,0,CHARINDEX(' ',C.Nome)) AS PrimeiroNome,
 		COUNT(SUBSTRING(C.Nome,0,CHARINDEX(' ',C.Nome))) AS TotalOcorrencias
 FROM Cliente C 
@@ -11,13 +10,11 @@ GROUP BY SUBSTRING(C.Nome,0,CHARINDEX(' ',C.Nome))
 ORDER BY COUNT(SUBSTRING(C.Nome,0,CHARINDEX(' ',C.Nome))) DESC;
 
 --Exercicio 02
-
 SELECT COUNT(P.IDPedido) AS Quantidade, SUM(P.ValorPedido) AS ValorPedido 
 FROM Pedido P
 WHERE MONTH(P.DataPedido) = 4 AND YEAR(P.DataPedido) = 2017;
 
 --Exercicio 03
-
 SELECT Cidade.UF, COUNT(Cliente.IDCidade) AS TotalClientes 
 FROM Cidade INNER JOIN Cliente ON Cidade.IDCidade = Cliente.IDCidade
 WHERE Cidade.UF IN( 
@@ -34,3 +31,23 @@ WHERE Cidade.UF IN(
 	ORDER BY COUNT(Cl.IDCidade) DESC
 )
 GROUP BY Cidade.UF;
+
+--Exercicio 04
+INSERT INTO Produto(Nome ,PrecoCusto, PrecoVenda, Situacao)
+	VALUES('Galocha Maragato',35.67,77.95,'A');
+	--,GETDATE() -- , DataCadastro
+
+--Exercicio 05
+SELECT * FROM Produto Pr 
+LEFT JOIN PedidoItem Ip ON Pr.IDProduto = Ip.IDProduto
+LEFT JOIN Pedido Pe ON Ip.IDPedido = Pe.IDPedido
+WHERE Pe.IDPEDIDO IS NULL;
+
+--Exercicio 06
+SELECT TOP 30 SUM(Ip.Quantidade*Pr.PrecoVenda) AS QuantidadeVendida2016, 
+	Pr.IDProduto, Pr.Nome FROM Produto Pr 
+INNER JOIN PedidoItem Ip ON Pr.IDProduto = Ip.IDProduto
+INNER JOIN Pedido Pe ON Ip.IDPedido = Pe.IDPedido
+WHERE YEAR(DataPedido) = 2016
+GROUP BY Pr.IDProduto, Pr.Nome
+ORDER BY SUM(Ip.Quantidade*Pr.PrecoVenda) DESC;
