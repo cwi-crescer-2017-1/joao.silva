4b13fd821b19856a713c9c0cb81d8aa9cf3c5b1d
JPedroSSilva <joao.silva@cwi.com.br>
2017-05-14 18:28:00 -0300

    feat(To-Do): implementado aviso com horario e alerta que variam entre si

--- HTML e CSS/TO-DO List/script.js
+++ HTML e CSS/TO-DO List/script.js
@@ -1,5 +1,19 @@
 var h2 = document.querySelector('h2');
 h2.innerText = new Date().toLocaleString();
+let countAtualizaAviso=0;
+function atualizaAviso(){
+  horaInicialEmMS = new Date().getTime();
+  if(countAtualizaAviso<5){
+    exibeHora();
+    countAtualizaAviso++;
+  }else if(countAtualizaAviso<8){
+    exibeAlerta();
+    countAtualizaAviso++;
+  }else{
+    countAtualizaAviso=0;
+  }
+}
+var exibeHora = ()=>{h2.innerText = new Date().toLocaleString();};
+var exibeAlerta = ()=>{h2.innerText = "Remember: Only important things!"};
 
-var atualizaHora = ()=>{h2.innerText = new Date().toLocaleString();};
-setInterval(atualizaHora, 1000);
+setInterval(atualizaAviso,1000);
