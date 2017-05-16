52193785d92c1f5621c2c85b95fee0376b6da0a2
JPedroSSilva <joao.silva@cwi.com.br>
2017-05-16 13:42:54 -0300

    feat(Lista02Atualizada): acréscimo de comentário explicativo

--- JavaScript/Lista02Atualizada/specs/seriesSpecs.js
+++ JavaScript/Lista02Atualizada/specs/seriesSpecs.js
@@ -1,5 +1,5 @@
 series.push({"titulo":"TesteUndefined","anoEstreia":2014,"diretor":["Tester"],"genero":["Drama","Drama","Terror"],"elenco":["Debug","Error","Undefined"],"temporadas":undefined,"numeroEpisodios":undefined,"distribuidora":"JS"});
-
+//Adiciona uma serie com um campo undefined
 describe("seriesInvalidas()", function() {
     it("Teste para o array series", function() {
         var resultado = "Séries inválidas: Band of Brothers - Mr. Robot - Narcos - TesteUndefined";
