bf54367027e5ae3eaa919edf4a920a568f8a2200
JPedroSSilva <joao.silva@cwi.com.br>
2017-05-18 17:26:34 -0300

    feat(Aul2Atividade2): implementação da tabela de mascada

---
+++ AngularJS/Aula2Atividade2/js/main.js
@@ -0,0 +1,43 @@
+var modulo = angular.module('MascadaAPP',[]);
+modulo.controller('mascada',['$scope',function(model){
+    model.instrutores = [{
+        nome: 'Bernardo',
+        aula: [{
+            numero: 1,
+            nome: 'OO'
+        },
+        {
+            numero: 4,
+            nome: 'Javascript'
+        }
+        ]
+    },
+    {
+        nome: 'Nunes',
+        aula: [{
+        numero: 2,
+        nome: 'Banco de Dados I'
+        }]
+    },
+    {
+        nome: 'Pedro (PHP)',
+        aula: [{
+        numero: 3,
+        nome: 'HTML e CSS'
+        }]
+    },
+    {
+        nome: 'Zanatta',
+        aula: [{
+        numero: 5,
+        nome: 'AngularJS'
+        }]
+    }
+    ];
+}]);
+modulo.filter('mascada',function(){
+    return function(nome){
+        return nome.replace(/Nunes/i,'$ Nunes $');
+    }
+});
+
