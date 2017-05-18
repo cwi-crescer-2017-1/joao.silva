11309008702a9e4291778543c8a229d067c46495
JPedroSSilva <joao.silva@cwi.com.br>
2017-05-18 16:03:43 -0300

    fix(erroNaFormatacaoDeData); fixado erro na formatação de data e adicionado outro método para a mesma função

--- AngularJS/Aula2Atividade1/js/formatadorData.js
+++ AngularJS/Aula2Atividade1/js/formatadorData.js
@@ -1,7 +1,18 @@
 function formatarData(data){
-    var parts = data.split('/');
-    console.log(parts[0]);
-    console.log(parts[1]);
-    console.log(parts[2]);
-    return new Date(parts[0],parts[1]-1,parts[2]);    
+    /*
+    let pattern = '((\d{2})\/(\d{2})\/(\d{4}))|((\d{2})\.(\d{2})\.(\d{4}))|((\d{2})\\(\d{2})\\(\d{4}))|((\d{2})\-(\d{2})\-(\d{4}))';
+    let replace = '$1.$2.$3';
+    data.replace(replace,pattern);
+    return new Date(data);
+    */
+    if(data.indexOf('/')>-1){
+        var parts = data.split('/');
+        return new Date(parts[0],parts[1]-1,parts[2]);
+    }else if(data.indexOf('-')>-1){
+        return data;
+    }else if(data.indexOf('.')>-1){
+        return new Date(data);
+    }else{
+        return null;
+    }    
 }
