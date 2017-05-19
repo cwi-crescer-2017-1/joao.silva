833d2335f7bcbceaecf9d8b9df020e3ee88837a4
JPedroSSilva <joao.silva@cwi.com.br>
2017-05-19 18:20:21 -0300

    feat(Aula3Atividade1): melhora na implementação dos alertas de falta de preenchimento de campo obrigatorio

--- AngularJS/Aula3Atividade1/js/main.js
+++ AngularJS/Aula3Atividade1/js/main.js
@@ -1,16 +1,19 @@
 var modulo = angular.module('Aula3Atividade1',[]);
 modulo.controller('AtividadeForm',['$scope', function (model){
-    model.instrutores = [{nome: 'Bernardo', sobrenome: 'Rezende', idade: 30, email: 'bernardo@cwi.com.br', jaDeuAula: true, aula: 'OO'}
+    model.instrutores = [{nome: 'Bernardo', sobrenome: 'Rezende', idade: "30", email: 'bernardo@cwi.com.br', jaDeuAula: true, aula: 'OO'}
 ];
     model.aulas = ['HTML e CSS', 'JavaScript', 'Banco de dados', 'AngularJS', 'Orientação a objetos'];
     model.adicionarInstrutor = adicionarInstrutor;
+    model.erro = "cadastro.nome.$error.maxlength || cadastro.nome.$error.minlength || cadastro.nome.$error.required || cadastro.sobrenome.$error.maxlength ||  cadastro.idade.$error.required || cadastro.email.$error.required";
     function adicionarInstrutor(){
-        model.instrutores.push({nome: model.cadastro.nome.$viewValue, 
+        let novoInstrutor={nome: model.cadastro.nome.$viewValue, 
             sobrenome:model.cadastro.sobrenome.$viewValue,
             idade:model.cadastro.idade.$viewValue, 
             email:model.cadastro.email.$viewValue,
             jaDeuAula: model.cadastro.jaDeuAula.$viewValue, 
-            aula:model.cadastro.aula.$viewValue});
+            aula:model.cadastro.aula.$viewValue};
+        model.instrutores.push(novoInstrutor);
+        model.instrutor = {};
         console.log(model.instrutores);
     }
 }])
