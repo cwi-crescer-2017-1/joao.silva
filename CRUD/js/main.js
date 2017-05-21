fbd1f8ae38fe80f040966a57dad76a23076977e5
JPedroSSilva <joao.silva@cwi.com.br>
2017-05-21 15:37:36 -0300

    feat(ListagemAulasEInstrutores): implementada a lista de aulas e de instrutores

--- CRUD/js/main.js
+++ CRUD/js/main.js
@@ -1,36 +1,65 @@
 var modulo = angular.module('CRUD',[]);
 var oi;
-modulo.controller('Crescer',['$scope', function (model){
-    model.aulas = [{},
-                   {id:0,nome:'Tester'},
+modulo.controller('Crescer',['$scope','$filter', function (model,filter){
+    let idNovaAula=4;
+    let idNovoInstrutor=4;
+    model.aulas = [{},{id:0,nome:'Tester'},
                    {id:1,nome:'Heroísmo'},
                    {id:2,nome:'Poções'},
                    {id:3,nome:'Defesa contra as artes das trevas'}];
-    model.instrutores = [{id:0,nome:'Teste',sobrenome:'Tester',idade:'22',email:'teste@tester.com.br',dandoAula:true,aula:[0],urlFoto:'../img/perfil_padrao.jpeg'},
-                         {id:1,nome:'Saitama',sobrenome:'',idade:'26',email:'opm@heroassociation.jp',dandoAula:false,aula:[],urlFoto:'../img/perfil_padrao.jpeg'},
-                         {id:2,nome:'Toshinori',sobrenome:'Yagi',idade:'36',email:'yagi@gmail.jp',dandoAula:true,aula:[1],urlFoto:'../img/perfil_padrao.jpeg'},
-                         {id:3,nome:'Severus',sobrenome:'Snape',idade:'38',email:'severus@discipline.uk',dandoAula:true,aula:[2,3],urlFoto:'../img/perfil_padrao.jpeg'}];
-    model.aulasSelecionadas = [];
-    model.adicionarAula =function(aula){
-        console.log(aula);
+    model.instrutores = [{id:0,nome:'Teste',sobrenome:'Tester',idade:'22',email:'teste@tester.com.br',dandoAula:true,aula:[0],urlFoto:'img/perfil_padrao.jpg'},
+                         {id:1,nome:'Saitama',sobrenome:'',idade:'26',email:'opm@heroassociation.jp',dandoAula:false,aula:[],urlFoto:'img/perfil_padrao.jpg'},
+                         {id:2,nome:'Toshinori',sobrenome:'Yagi',idade:'36',email:'yagi@gmail.jp',dandoAula:true,aula:[1],urlFoto:'img/perfil_padrao.jpg'},
+                         {id:3,nome:'Severus',sobrenome:'Snape',idade:'38',email:'severus@discipline.uk',dandoAula:true,aula:[2,3],urlFoto:'img/perfil_padrao.jpg'}];
+    model.aulasSelecionadas=[];
+    model.adicionarAula =function(nomeaula){
+        let novaAula={id:idNovaAula,nome:nomeaula};
+        idNovaAula++;
+        model.aulas.push(novaAula);
+    }
+    model.stringAulas = function(aulasId){
+        let aulasNomes=[];
+        for(let i=0;i<aulasId.length;i++){
+            aulasNomes.push(model.pegarNomeAulaPorId(aulasId[i]));
+        }
+        aulasNomes = filter('orderBy')(aulasNomes, '');
+        return aulasNomes.join(", ");
+    }
+    model.temAula = function(dandoAula){
+        if(dandoAula===true){
+            return "Sim";
+        }else{
+            return "Não";
+        }
     }         
     model.adicionarInstrutor = function(instrutor){
-        console.log(instrutor);
+        let aulasInstrutor = model.aulasSelecionadas.map(aulaSelecionada => aulaSelecionada.id);
+        let instrutorDandoAula;
+        /* para select multiple for(let i=0;i<instrutor.aulas.length;i++){
+            instrutor.aulas[i]=Number(instrutor.aulas[i]);
+        }*/
+        if(instrutor.dandoAula==='s'){instrutorDandoAula=true}else{instrutorDandoAula=false};
+        let novoInstrutor = {id:idNovoInstrutor,nome: instrutor.nome,sobrenome:instrutor.sobrenome,idade:instrutor.idade,email:instrutor.email,dandoAula:instrutorDandoAula,aula:aulasInstrutor,urlFoto:instrutor.urlFoto};
+        //para select multiple let novoInstrutor = {id:idNovoInstrutor,nome: instrutor.nome,sobrenome:instrutor.sobrenome,idade:instrutor.idade,email:instrutor.email,dandoAula:instrutorDandoAula,aula:instrutor.aulas,urlFoto:instrutor.urlFoto};
+        model.instrutores.push(novoInstrutor);
     }
     model.selecaoAulas = function(idAula){
         if(!model.aulaJaSelecionada(idAula)){
-            model.selecionarAula(idAula);
+            model.adicionarAulaASelecao(idAula);
         }      
     }
-    model.selecionarAula = function(idAula){
+    model.adicionarAulaASelecao = function(idAula){
+        let selecionada = {nome:model.pegarNomeAulaPorId(idAula),id:idAula};
+        model.aulasSelecionadas.push(selecionada);
+    }
+     model.pegarNomeAulaPorId = function(idAula){
         for(aula of model.aulas){
             if(aula.id===idAula){ 
-                let selecionada = {nome:aula.nome,id:aula.id};
-                model.aulasSelecionadas.push(selecionada);
+                return aula.nome;
             }
         } 
     }
-    model.aulaJaSelecionada = function(idAula){ //Evita a todo custo que uma aula já adicionada seja adicionada novamente
+    model.aulaJaSelecionada = function(idAula){
         for(aulaSelecionada of model.aulasSelecionadas){
             if(aulaSelecionada.id===idAula){
                 return true;
@@ -41,16 +70,14 @@ modulo.controller('Crescer',['$scope', function (model){
     model.remocaoAulasSelecionadas = function (idAula){
         for(let i=0;i<model.aulasSelecionadas.length;i++){
             if(model.aulasSelecionadas[i].id===idAula){
-             console.log(i);
              model.aulasSelecionadas.splice(i,1);
             }
         }
     }
     model.aulaValida = function(idAula){
-        console.log(idAula);
         if(typeof idAula === 'undefined'){
             return true;
         }
         return false;
     }
-    }])
+}]);
