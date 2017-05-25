f16387a5ba6a55f5a2a65ca7a20698889aa11f21
JPedroSSilva <joao.silva@cwi.com.br>
2017-05-25 11:30:45 -0300

    feat(aliquotaINSS): implementada classe para obter e calcular a aliquota de INSS de determinado salário

---
+++ C#/Exercicio3/JoaoSilva/Exercicio3/calculoINSS.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio3
+{
+    public class aliquotaINSS
+    {
+        public aliquotaINSS(double totalProventos){
+            TotalProventos = totalProventos;
+        }
+        private double TotalProventos;
+        public double Valor(){
+            if (TotalProventos <= 1000)
+            {
+                return 0.08;
+            }else if (TotalProventos<=1500)
+            {
+                return 0.09;
+            }
+            else
+            {
+                return 0.1;
+            }
+        }
+    }
+}
