30ace67812d6b460de068624b469e6fa438152c5
JPedroSSilva <joao.silva@cwi.com.br>
2017-05-25 11:30:02 -0300

    feat(CalculoFolhaPagamento): implementados métodos para calcular os dados da folha de pagamento e para criar um demonstrativo

--- C#/Exercicio3/JoaoSilva/Exercicio3/CalculoFolhaPagamento.cs
+++ C#/Exercicio3/JoaoSilva/Exercicio3/CalculoFolhaPagamento.cs
@@ -8,10 +8,62 @@ namespace Exercicio3.Entidades
 {
     public class CalculoFolhaPagamento:IFolhaPagamento
     {
-        public Demonstrativo GerarDemonstrativo(int horasCategoria, double salarioBase, double horasExtras, double horasDescontadas)
+        public CalculoFolhaPagamento(int horasCategoria, double salarioBase, double horasExtras, double horasDescontadas)
         {
-            return null;
+            HorasCategoria = horasCategoria;
+            SalarioBase = salarioBase;
+            HorasExtras = horasExtras;
+            HorasDescontadas = horasDescontadas;
+        }
+        public int HorasCategoria { get; private set; }
+        public double SalarioBase { get; private set; }
+        public double HorasExtras { get; private set; }
+        public double HorasDescontadas { get; private set; }
+
+        public Demonstrativo GerarDemonstrativo()
+        {
+            double salarioPorHora = calcularSalarioPorHora(SalarioBase,HorasCategoria);
+            double totalHorasDescontadas = calcularTotalHorasDescontadas(salarioPorHora, HorasDescontadas);
+            double totalHorasExtras = calcularTotalHorasExtras(salarioPorHora, HorasExtras);
+            HorasCalculadas horasExtras = new HorasCalculadas(HorasExtras, totalHorasExtras);
+            HorasCalculadas horasDescontadas = new HorasCalculadas(HorasDescontadas, totalHorasDescontadas);
+            double totalProventos = arredondar(calcularTotalProventos(totalHorasExtras,totalHorasDescontadas));
+            double aliquotaINSS = new aliquotaINSS(totalProventos).Valor();
+            double totalINSS = arredondar(aliquotaINSS * totalProventos);
+            Desconto inss = new Desconto(aliquotaINSS, totalINSS);
+            double aliquotaIRRF = new aliquotaIRRF(totalProventos, totalINSS).Valor();
+            double totalIRRF = arredondar(aliquotaIRRF * totalProventos);
+            Desconto irrf = new Desconto(aliquotaIRRF, totalIRRF);
+            double totalDescontos = irrf.Valor+inss.Valor;
+            double totalLiquido = totalProventos - totalDescontos;
+            double totalFGTS = calcularFGTS(SalarioBase);
+            Desconto fgts = new Desconto(0.11,totalFGTS); 
+            return new Demonstrativo(SalarioBase, HorasCategoria, horasExtras, horasDescontadas,totalProventos,inss,irrf,totalDescontos,totalLiquido,fgts);
         }
 
+        private double calcularSalarioPorHora(double salarioBase, double horasCategoria)
+        {
+            return salarioBase / horasCategoria;
+        }
+        private double calcularTotalHorasExtras(double salarioPorHora, double horasExtras)
+        {
+            return salarioPorHora * horasExtras;
+        }
+        private double calcularTotalHorasDescontadas(double salarioPorHora, double horasDescontadas)
+        {
+            return salarioPorHora * horasDescontadas;
+        }
+        private double calcularTotalProventos(double totalHorasExtras, double totalHorasDescontadas)
+        {
+            return SalarioBase + totalHorasExtras - totalHorasDescontadas;
+        }
+        private double calcularFGTS(double totalProventos)
+        {
+            return arredondar(totalProventos * 0.11);
+        }
+        private double arredondar(double valor)
+        {
+            return Math.Truncate(valor * 100) / 100;
+        }
     }
 }
