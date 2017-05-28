139a8cc2b2cb36d2567197f4247f790f8d2953eb
JPedroSSilva <joao.silva@cwi.com.br>
2017-05-28 17:59:46 -0300

    feat(ChatController): implementação da controller do chat

--- C#/WebAPIChat-joao.silva/WebAPIChat/Controllers/ChatController.cs
+++ C#/WebAPIChat-joao.silva/WebAPIChat/Controllers/ChatController.cs
@@ -4,14 +4,46 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text.RegularExpressions;
 using System.Web.Http;
+using WebAPIChat.Exceptions;
 using WebAPIChat.Models;
 
 namespace WebAPIChat.Controllers
 {
-    public class ChatController
+    public class ChatController : ApiController
     {
         private static List<Mensagem> mensagens = new List<Mensagem>();
-        private static List<Usuario> usuarios = new List<Usuario>();
+        private static object @lock = new object();
+        public List<Mensagem> Get()
+        {
+            if (mensagens.Count() > 10) {
+                List<Mensagem> listaRetorno = new List<Mensagem>();
+                for (var i = mensagens.Count() - 1; i > mensagens.Count - 10; i--)
+                {
+                    listaRetorno.Add(mensagens[i]);
+                }
+                return listaRetorno;
+            }
+            return mensagens;
+        }
+
+        public bool Post(string texto,string nomeRemetente,int idRemetente,string imgRemetente)
+        {
+            lock (@lock)
+            {
+                var textoAprimorado = ProcurarNunes(texto);
+                Mensagem mensagem = new Mensagem(textoAprimorado, new InformacoesUsuario(nomeRemetente, idRemetente, imgRemetente));
+                mensagens.Add(mensagem);
+                return true;
+            }
+        }
+        public string ProcurarNunes(string texto)
+        {
+            string pattern = @"Nunes";
+            string replacement = "$$$$$$$$$ $$$$$$$$$";
+            string textoAprimorado = Regex.Replace(texto, pattern, replacement, RegexOptions.IgnoreCase);
+            return textoAprimorado;
+        }
     }
 }
