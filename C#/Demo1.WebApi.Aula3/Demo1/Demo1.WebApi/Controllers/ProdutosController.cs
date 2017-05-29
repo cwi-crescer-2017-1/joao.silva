ae34218b04ddf05bb83ff1723ff1379459342069
JPedroSSilva <joao.silva@cwi.com.br>
2017-05-29 14:49:30 -0300

    feat(Demo1Aula3): implementação de uma demonstração de WebAPI

---
+++ C#/Demo1.WebApi.Aula3/Demo1/Demo1.WebApi/Controllers/ProdutosController.cs
@@ -0,0 +1,45 @@
+﻿using Demo1.WebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Demo1.WebApi.Controllers
+{
+    public class ProdutosController : ApiController
+    {
+        public IHttpActionResult Post(Produto produto)
+        {
+            return Ok(string.Empty);
+        }
+
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
