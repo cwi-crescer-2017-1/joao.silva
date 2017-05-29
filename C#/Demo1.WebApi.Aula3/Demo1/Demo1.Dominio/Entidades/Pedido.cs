f99a0e1ee7a4cf34f24fcc509b7c3d2f275db4fd
JPedroSSilva <joao.silva@cwi.com.br>
2017-05-29 18:19:05 -0300

    feat(EntidadePedido): implementação da entidade de pedido

---
+++ C#/Demo1.WebApi.Aula3/Demo1/Demo1.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1.Dominio.Entidades
+{
+    public class Pedido
+    {
+          public int Id { get; set; }
+          public string NomeCliente { get; set; }
+          public List<ItemPedido> Itens { get; set; }
+
+          public bool Validar(out List<string> mensagens)
+          {
+              mensagens = new List<string>();
+
+              if (Itens.Count < 0)
+              {
+                  mensagens.Add("O pedido deve ter ao mínimo um item.");
+              }
+              if (string.IsNullOrWhiteSpace(NomeCliente))
+              {
+                  mensagens.Add("Nome do cliente deve ser informado.");
+              }
+              if()
+              return mensagens.Count() == 0;
+        }
+    }
+}
