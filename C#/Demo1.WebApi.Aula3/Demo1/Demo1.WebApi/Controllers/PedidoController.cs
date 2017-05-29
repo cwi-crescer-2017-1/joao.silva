724c4088c79c10c6732c82ed13996c426ba49919
JPedroSSilva <joao.silva@cwi.com.br>
2017-05-29 18:17:35 -0300

    feat(PedidoController): implementação da controller do pedido

---
+++ C#/Demo1.WebApi.Aula3/Demo1/Demo1.WebApi/Controllers/PedidoController.cs
@@ -0,0 +1,27 @@
+﻿using Demo1.Dominio.Entidades;
+using Demo1.Infraestrutura.Repositorios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace Demo1.WebApi.Controllers
+{
+    public class PedidoController : ApiController
+    {
+        PedidoRepositorio _pedidoRepositorio = new PedidoRepositorio();
+        public IHttpActionResult Post(Pedido pedido)
+        {
+            var mensagens = new List<string>();
+            if (!pedido.Validar(out mensagens))
+            {
+                return BadRequest(string.Join(" - ", mensagens));
+            }
+
+            _pedidoRepositorio.Criar(pedido);
+
+            return Ok(pedido);
+        }
+    }
+}
