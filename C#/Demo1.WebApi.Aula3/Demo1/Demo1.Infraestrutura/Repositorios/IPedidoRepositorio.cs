a18df7da7347c0afaa47d0b358921e31f5cc82f9
JPedroSSilva <joao.silva@cwi.com.br>
2017-05-29 18:17:11 -0300

    feat(IPedidoRepositorio): inserção da interface do repositorio de pedido

---
+++ C#/Demo1.WebApi.Aula3/Demo1/Demo1.Infraestrutura/Repositorios/IPedidoRepositorio.cs
@@ -0,0 +1,18 @@
+﻿using Demo1.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1.Infraestrutura.Repositorios
+{
+    interface IPedidoRepositorio
+    {
+        void Alterar(Pedido pedido);
+        void Criar(Pedido pedido);
+        void Excluir(int id);
+        IEnumerable<Pedido> Listar();
+        Pedido Obter(int id);
+    }
+}
