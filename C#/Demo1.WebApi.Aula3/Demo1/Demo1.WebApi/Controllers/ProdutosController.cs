b5672689603bee88fc3917ea4786df7d6685944f
JPedroSSilva <joao.silva@cwi.com.br>
2017-05-29 23:04:16 -0300

    feat(ProdutoController): adição dos métodos do ProdutoController

--- C#/Demo1.WebApi.Aula3/Demo1/Demo1.WebApi/Controllers/ProdutosController.cs
+++ C#/Demo1.WebApi.Aula3/Demo1/Demo1.WebApi/Controllers/ProdutosController.cs
@@ -31,5 +31,26 @@ namespace Demo1.WebApi.Controllers
 
             return Ok(produtos);
         }
+        public IHttpActionResult Put(Produto produto)
+        {
+            var mensagens = new List<string>();
+
+            if (!produto.Validar(out mensagens))
+                return BadRequest(string.Join(".", mensagens.ToArray()));
+
+            _produtoRepositorio.Alterar(produto);
+
+            return Ok(produto);
+        }
+        public IHttpActionResult Get(int id)
+        {
+            return Ok(_produtoRepositorio.Obter(id));
+        }
+        public IHttpActionResult Delete(int id)
+        {
+            _produtoRepositorio.Excluir(id);
+
+            return Ok();
+        }
     }
 }
