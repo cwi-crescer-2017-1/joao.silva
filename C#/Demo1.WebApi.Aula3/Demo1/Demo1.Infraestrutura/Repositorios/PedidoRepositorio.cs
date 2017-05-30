da0fce121354abc9f78c1d9da99fcb688ea9d932
JPedroSSilva <joao.silva@cwi.com.br>
2017-05-29 23:02:21 -0300

    feat(PedidoRepositorio): implementação dos métodos do PedidoRepositorio

--- C#/Demo1.WebApi.Aula3/Demo1/Demo1.Infraestrutura/Repositorios/PedidoRepositorio.cs
+++ C#/Demo1.WebApi.Aula3/Demo1/Demo1.Infraestrutura/Repositorios/PedidoRepositorio.cs
@@ -18,7 +18,39 @@ namespace Demo1.Infraestrutura.Repositorios
 
         public void Alterar(Pedido pedido)
         {
-            throw new NotImplementedException();
+            using (var conexao = new SqlConnection(stringConexao))
+            {
+                conexao.Open();
+
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText =
+                        @"UPDATE Pedido SET NomeCliente = @NomeCliente WHERE Id = @id";
+
+                    comando.Parameters.AddWithValue("@nome", pedido.NomeCliente);
+                    comando.Parameters.AddWithValue("@id", pedido.Id);
+
+                    comando.ExecuteNonQuery();
+                }
+                foreach (ItemPedido item in pedido.Itens)
+                {
+                    using (var comando = conexao.CreateCommand())
+                    {
+                        comando.CommandText =
+                            @"UPDATE ItemPedido SET PedidoId = @PedidoId, ProdutoId = @ProdutoId, Quantidade = @Quantidade 
+                                                       WHERE Id = @id";
+
+                        comando.Parameters.AddWithValue("@PedidoId", pedido.Id);
+                        comando.Parameters.AddWithValue("@ProdutoId", item.ProdutoId);
+                        comando.Parameters.AddWithValue("@Quantidade", item.Quantidade);
+                        comando.Parameters.AddWithValue("@id", item.Id);
+
+                        comando.ExecuteNonQuery();
+                    }
+
+                }
+
+            }
         }
 
         public void Criar(Pedido pedido)
@@ -75,17 +107,121 @@ namespace Demo1.Infraestrutura.Repositorios
 
         public void Excluir(int id)
         {
-            throw new NotImplementedException();
+            using (var conexao = new SqlConnection(stringConexao))
+            {
+                conexao.Open();
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText = "DELETE Pedido WHERE Id = @id";
+                    comando.Parameters.AddWithValue("@id", id);
+                    comando.ExecuteNonQuery();
+                }
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText = "DELETE ItemPedido WHERE PedidoID = @id";
+                    comando.Parameters.AddWithValue("@id", id);
+                    comando.ExecuteNonQuery();
+                }
+            }
         }
 
-        public IEnumerable<Pedido> Listar()
+        public List<Pedido> Listar()
         {
-            throw new NotImplementedException();
+            var pedidos = new List<Pedido>();
+            using (var conexao = new SqlConnection(stringConexao))
+            {
+                conexao.Open();
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText =
+                        @"SELECT Id, NomeCliente FROM Pedido";
+
+                    var dataReader = comando.ExecuteReader();
+                    while (dataReader.Read())
+                    {
+                        var pedido = new Pedido();
+
+                        pedido.Id = (int)dataReader["Id"];
+                        pedido.NomeCliente = (string)dataReader["NomeCliente"];
+
+                        pedidos.Add(pedido);
+                    }
+                }
+                foreach (Pedido pedido in pedidos)
+                {
+                    using (var comando = conexao.CreateCommand())
+                    {
+                        comando.CommandText =
+                            @"SELECT Id, ProdutoId, Quantidade 
+                                   FROM ItemPedido WHERE PedidoId = @PedidoId";
+                        
+                        comando.Parameters.AddWithValue("@PedidoId", pedido.Id);
+
+                        var dataReader = comando.ExecuteReader();
+                        while (dataReader.Read())
+                        {
+                            var itemPedido = new ItemPedido();
+
+                            itemPedido.Id = (int)dataReader["Id"];
+                            itemPedido.ProdutoId = (int)dataReader["ProdutoId"];
+                            itemPedido.Quantidade = (int)dataReader["Quantidade"];
+                            pedido.Itens.Add(itemPedido);
+                        }
+                    }
+
+                }
+            }
+            return pedidos;
         }
 
         public Pedido Obter(int id)
         {
-            throw new NotImplementedException();
+            Pedido pedido = null;
+
+            using (var conexao = new SqlConnection(stringConexao))
+            {
+                conexao.Open();
+
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText =
+                        "SELECT Id, NomeCliente FROM Pedido WHERE Id = @id";
+
+                    comando.Parameters.AddWithValue("@id", id);
+
+                    var dataReader = comando.ExecuteReader();
+                    if (dataReader.Read() != false)
+                    {
+                        pedido = new Pedido();
+                        pedido.Id = (int)dataReader["Id"];
+                        pedido.NomeCliente = (string)dataReader["NomeCliente"];
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText =
+                        @"SELECT Id, PedidoId, ProdutoId, Quantidade 
+                                   FROM ItemPedido WHERE PedidoId = @PedidoId";
+
+                    comando.Parameters.AddWithValue("@PedidoId", pedido.Id);
+
+                    var dataReader = comando.ExecuteReader();
+                    while (dataReader.Read())
+                    {
+                        var itemPedido = new ItemPedido();
+
+                        itemPedido.Id = (int)dataReader["Id"];
+                        itemPedido.ProdutoId = (int)dataReader["ProdutoId"];
+                        itemPedido.Quantidade = (int)dataReader["Quantidade"];
+                        pedido.Itens.Add(itemPedido);
+                    }
+                }
+            }
+            return pedido;
         }
     }
 }
