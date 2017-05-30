7d6bd9f8bf01af33bb12b158f40e34de9d2282d9
JPedroSSilva <joao.silva@cwi.com.br>
2017-05-29 23:03:17 -0300

    feat(PordutoRepositorio): adição dos métodos do ProdutoRepositorio

--- C#/Demo1.WebApi.Aula3/Demo1/Demo1.Infraestrutura/Repositorios/ProdutoRepositorio.cs
+++ C#/Demo1.WebApi.Aula3/Demo1/Demo1.Infraestrutura/Repositorios/ProdutoRepositorio.cs
@@ -76,6 +76,77 @@ namespace Demo1.Infraestrutura.Repositorios
             }
                 return produtos;
         }
+        public void Alterar(Produto produto)
+        {
+            using (var conexao = new SqlConnection(stringConexao))
+            {
+                conexao.Open();
 
+                // Executa o INSERT
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText =
+                        @"UPDATE PRODUTO SET Nome = @nome, Preco = @preco, Estoque = @estoque WHERE Id = @id";
+
+                    comando.Parameters.AddWithValue("@nome", produto.Nome);
+                    comando.Parameters.AddWithValue("@preco", produto.Preco);
+                    comando.Parameters.AddWithValue("@estoque", produto.Estoque);
+                    comando.Parameters.AddWithValue("@id", produto.Id);
+
+                    // Executa o comando e 
+                    // retorna somente a quantidade de linhas afetadas
+                    comando.ExecuteNonQuery();
+                }
+            }
+        }
+        public void Excluir(int id)
+        {
+            using (var conexao = new SqlConnection(stringConexao))
+            {
+                conexao.Open();
+
+                // Executa o INSERT
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText = "DELETE Produto WHERE Id = @id";
+
+                    comando.Parameters.AddWithValue("@id", id);
+
+                    // Executa o comando e 
+                    // retorna somente a quantidade de linhas afetads
+                    comando.ExecuteNonQuery();
+                }
+            }
+        }
+        public Produto Obter(int id)
+        {
+            Produto produto = null;
+
+            using (var conexao = new SqlConnection(stringConexao))
+            {
+                conexao.Open();
+
+                using (var comando = conexao.CreateCommand())
+                {
+                    comando.CommandText =
+                        "SELECT Id, Nome, Preco, Estoque FROM Produto WHERE Id = @id";
+
+                    comando.Parameters.AddWithValue("@id", id);
+
+                    var dataReader = comando.ExecuteReader();
+                    while (dataReader.Read())
+                    {
+                        produto = new Produto();
+                        produto.Id = (int)dataReader["Id"];
+                        produto.Nome = (string)dataReader["Nome"];
+                        produto.Preco = (decimal)dataReader["Preco"];
+                        produto.Estoque = (int)dataReader["Estoque"];
+                        return produto;
+                    }
+                }
+            }
+
+            return produto;
+        }
     }
 }
