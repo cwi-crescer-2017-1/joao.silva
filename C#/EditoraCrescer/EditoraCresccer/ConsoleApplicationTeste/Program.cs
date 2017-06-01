c8eefec1be3dc0cfa86a13b83da79cc2831238c2
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-01 19:16:09 -0300

    fix(Program): modificação para novo modelo

--- C#/EditoraCrescer/EditoraCresccer/ConsoleApplicationTeste/Program.cs
+++ C#/EditoraCrescer/EditoraCresccer/ConsoleApplicationTeste/Program.cs
@@ -13,28 +13,30 @@ namespace ConsoleApplicationTeste
         static void Main(string[] args)
         {
             var autor1 = new Autor() { Nome = "Tolkien" };
-            var autor2 = new Autor() { Nome = "Machado de Assis" };
-
+            // var autor2 = new Autor() { Nome = "Machado de Assis" };
+            var revisor1 = new Revisor() { Nome = "Machado de Assis" };
             using (var contexto = new Contexto())
             {
                 //Inclusão
                 contexto.Autores.Add(autor1);
-                contexto.Autores.Add(autor2);
+                contexto.Revisores.Add(revisor1);
                 contexto.SaveChanges();
 
                 var livro = new Livro()
                 {
                     Autor = autor1,
+                    Revisor = revisor1,
+                    IdAutor = autor1.Id,
+                    IdRevisor = revisor1.Id,
                     Titulo = "O senhor dos anéis",
                     Descricao = "Um livro bem legal",
                     Genero = "Aventura",
-                    DataPublicacao = DateTime.Now
+                    DataPublicacao = DateTime.Now,
+                    DataRevisao = DateTime.Now,
+                    Capa = "Capa de alta qualidade"
                 };
 
                 contexto.Livros.Add(livro);
-                contexto.SaveChanges();
-
-                contexto.Autores.Remove(autor1);
                 contexto.SaveChanges();
             }
         }
