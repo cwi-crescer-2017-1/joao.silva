45534098b532c81c75078f8045218546c19a1c08
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-02 18:14:50 -0300

    feat(get): modificação no modo de requisitar informação pela url por causa de erros

--- C#/EditoraCrescerFront/services/servicesLivros.js
+++ C#/EditoraCrescerFront/services/servicesLivros.js
@@ -1,28 +1,25 @@
 modulo.factory('servicesLivros',function($http){ //servicesInstrutores deve estar presente para executar a aulaService!
     
-    let urlBase = 'http://localhost:60184/api';
+    let urlBase = 'http://localhost:60184/api/';
 
     //Retorna lista de aulas
     function listLimitada(quantidade,skip){
-        let propriedades = {
-             url: urlBase+'/Livros',
-             method: 'GET',
-             params: {
-                 quantidade:quantidade,
-                 skip:skip
-             }
-        }
-        return $http(propriedades);
+        // let propriedades = {
+        //      url: urlBase+'/Livros',
+        //      method: 'GET',
+        //      params: {
+        //          quantidade:quantidade,
+        //          skip:skip
+        //      }
+        // }
+        return $http.get(urlBase+'Livros/ListaLimitada/'+quantidade+'/'+skip);
     }
     function listLancamentos(){
-        let propriedades = {
-             url: urlBase+'/Livros/Lancamentos',
-             method: 'GET'
-        }
-        return $http(propriedades);
+        return $http.get(urlBase+'/Livros/Lancamentos');
     }
     return { 
         listLimitada:listLimitada,
-        listLancamentos: listLancamentos
+        listLancamentos:listLancamentos
     }; 
+
 });
