3d5e3b6388b5366ad0b70e546de58955b2839874
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-02 18:09:10 -0300

    feat(BasicAuthentication): implementação do exemplo de autentição

---
+++ C#/EditoraCrescer/EditoraCresccer/EditoraCrescer.Api/Controllers/BasicAuthController.cs
@@ -0,0 +1,21 @@
+﻿using EditoraCrescer.Api.App_Start;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace EditoraCrescer.Api.Controllers
+{
+    [BasicAuthentication(Roles="Publicador")]
+
+    public class BasicAuthController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            return Request.CreateResponse(System.Net.HttpStatusCode.OK);
+        }
+
+    }
+}
