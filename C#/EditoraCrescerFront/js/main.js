dc3137f3e801a5240653e2b4508d87aceb738b1e
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-02 18:09:32 -0300

    doc(main.js): adição de comentários para guiar no desenvolvimento

--- C#/EditoraCrescerFront/js/main.js
+++ C#/EditoraCrescerFront/js/main.js
@@ -1,9 +1,14 @@
-var modulo = angular.module('EditoraCWI', ['ngRoute','toastr']); 
+var modulo = angular.module('EditoraCWI', ['ngRoute','toastr']);  //Adicionar dependencia do 'auth' e opcional do ngStorage
 modulo.config(function ($routeProvider) {  
   $routeProvider 
     .when('/paginaInicial', { 
       controller: 'PaginaInicial', 
       templateUrl: '../html/paginaInicialLivros.html' 
-    }) 
+    })  
+    /*resolve:{
+      autenticado: function(authService){
+        return authService.isAutenticadoPromisse();
+      }
+    }*/
     .otherwise({redirectTo: '/paginaInicial'}); 
 }); 
