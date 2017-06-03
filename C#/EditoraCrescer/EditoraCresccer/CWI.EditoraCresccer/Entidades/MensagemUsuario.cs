fc3bf448b90d666f2ebdf784afe7adb939d4eed9
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-03 20:36:02 -0300

    feat(MensagemUsuario): implementação de entidade base para mensagens de erro sobre o usuario

---
+++ C#/EditoraCrescer/EditoraCresccer/CWI.EditoraCresccer/Entidades/MensagemUsuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CWI.EditoraCresccer.Entidades
+{
+    public class MensagemUsuario
+    {
+        public Usuario Usuario { get; set; }
+        public List<string> Mensagens { get; set; }
+        public bool Resultado { get; set; }
+
+        public MensagemUsuario(Usuario usuario, List<string> mensagens, bool resultado)
+        {
+            Usuario = usuario;
+            Mensagens = mensagens;
+            Resultado = resultado;
+        }
+    }
+}
