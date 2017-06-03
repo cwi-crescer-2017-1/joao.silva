d35966e0f9c39de275a71c842a58568ac82c4aa4
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-03 20:34:51 -0300

    feat(UsuarioEPermissao): implementação das entidades de permissão e de usuário

---
+++ C#/EditoraCrescer/EditoraCresccer/CWI.EditoraCresccer/Entidades/Permissao.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CWI.EditoraCresccer.Entidades
+{
+    public class Permissao
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        protected Permissao(){}
+
+        public Permissao(string nome){
+            Nome = nome;
+        }
+
+    }
+}
