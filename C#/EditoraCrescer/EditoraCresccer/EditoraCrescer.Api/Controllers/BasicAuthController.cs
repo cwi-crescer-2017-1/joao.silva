393f45a3e5c444c6a4b3a482647ad437100e7648
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-03 20:41:11 -0300

    fix(BasicAuthController): correção na chamada da basicAuthorization

--- C#/EditoraCrescer/EditoraCresccer/EditoraCrescer.Api/Controllers/BasicAuthController.cs
+++ C#/EditoraCrescer/EditoraCresccer/EditoraCrescer.Api/Controllers/BasicAuthController.cs
@@ -1,14 +1,10 @@
-﻿using EditoraCrescer.Api.App_Start;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using EditoraCrescer.Api;
 using System.Net.Http;
-using System.Web;
 using System.Web.Http;
 
 namespace EditoraCrescer.Api.Controllers
 {
-    [BasicAuthentication(Roles="Publicador")]
+    [BasicAuthorization(Roles="Publicador")]
 
     public class BasicAuthController : ApiController
     {
