7b55b6d7531f65dd3561ff12714394df4d6deff8
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-05 03:18:49 -0300

    feat(LivroRepositorio): implementação de novas métodos relacionados a publicação e revisão e alteração de métodos antigos para enviar lista com apenas livros publicados ou com todos os livros

--- C#/EditoraCrescer/EditoraCresccer/CWI.EditoraCresccer/Repositorios/LivroRepositorio.cs
+++ C#/EditoraCrescer/EditoraCresccer/CWI.EditoraCresccer/Repositorios/LivroRepositorio.cs
@@ -11,11 +11,24 @@ namespace CWI.EditoraCresccer.Repositorios
     public class LivroRepositorio : IDisposable
     {
         private Contexto contexto = new Contexto();
-
+        private Contexto contextoTeste = new Contexto();
 
         public object ObterTodos()
         {
             return contexto.Livros
+                            .Select(livro => new
+                            {
+                                Isbn = livro.Isbn,
+                                Titulo = livro.Titulo,
+                                NomeAutor = livro.Autor.Nome,
+                                Genero = livro.Genero,
+                                Capa = livro.Capa
+                            }).ToList();
+        }
+        public object ObterTodosPublicados()
+        {
+            return contexto.Livros
+                            .Where(livro=> livro.DataPublicacao != null)
                             .Select(livro => new
                             {
                                 Isbn = livro.Isbn,
@@ -36,6 +49,7 @@ namespace CWI.EditoraCresccer.Repositorios
         {
             return contexto.Livros
                      .Where(x => x.Genero == genero)
+                     .Where(livro => livro.DataPublicacao != null)
                      .Select(livro => new
                      {
                          Isbn = livro.Isbn,
@@ -47,8 +61,9 @@ namespace CWI.EditoraCresccer.Repositorios
         }
         public object ObterLancamentos()
         {
+            DateTime dataLimiteLancamentos = DateTime.Now.AddDays(-7);
             return contexto.Livros
-                    .Where(livro => DbFunctions.DiffDays(livro.DataPublicacao, DateTime.Now) <= 7)
+                    .Where(livro => livro.DataPublicacao != null && livro.DataPublicacao >= dataLimiteLancamentos)
                     .Select(livro => new
                     {
                         Isbn = livro.Isbn,
@@ -65,8 +80,20 @@ namespace CWI.EditoraCresccer.Repositorios
         }
         public dynamic ObterListaLimitada(int quantidade, int skip)
         {
-            var livros = contexto.Livros.
-                OrderBy(x => x.Isbn)
+            var livros = contexto.Livros
+                .Where(livro => livro.DataPublicacao != null)
+                .OrderBy(x => x.Isbn)
+                .Skip(skip)
+                .Take(quantidade)
+                .Include(l => l.Autor)
+                .Include(r => r.Revisor);
+
+            return new { livros = livros.ToList(), quantidade = contexto.Livros.Count() };
+        }      
+        public dynamic ObterListaLimitadaCompleta(int quantidade, int skip)
+        {
+            var livros = contexto.Livros
+                .OrderBy(x => x.Isbn)
                 .Skip(skip)
                 .Take(quantidade)
                 .Include(l => l.Autor)
@@ -76,11 +103,62 @@ namespace CWI.EditoraCresccer.Repositorios
         }
         public Livro Criar(Livro livro)
         {
+            bool livroJaCadastrado = contexto.Livros.FirstOrDefault(l => l.Isbn == livro.Isbn) != null;
+            if (livroJaCadastrado)
+            {
+                return null;
+            }
             contexto.Livros.Add(livro);
             livro.Autor = contexto.Autores.FirstOrDefault(x => x.Id == livro.IdAutor);
-            livro.Revisor = contexto.Revisores.FirstOrDefault(x => x.Id == livro.IdRevisor);
             contexto.SaveChanges();
             return livro;
+        }
+        public Livro RevisarLivro(int idRevisor, int isbn)
+        {
+            Livro livro = contexto.Livros.FirstOrDefault(x => x.Isbn == isbn);
+            Usuario revisor = contexto.Usuarios.FirstOrDefault(u => u.Id == idRevisor);
+            bool livroExiste = livro != null;
+            bool revisorExiste = revisor != null;
+            if (livroExiste && revisorExiste)
+            {         
+                livro.IdRevisor = revisor.Id;
+                livro.DataRevisao = DateTime.Now;
+                contexto.Entry(livro).State = EntityState.Modified;
+                contexto.SaveChanges();
+                return livro;
+            }
+            return null;
+        }
+        public Livro PublicarLivro(int isbn)
+        {
+            Livro livro = contexto.Livros.FirstOrDefault(x => x.Isbn == isbn);
+            bool livroExiste = livro != null;
+            if (livroExiste)
+            {
+                if(livro.IdRevisor != null && livro.DataRevisao != null)
+                {
+                    livro.DataPublicacao = DateTime.Now;
+                    contexto.Entry(livro).State = EntityState.Modified;
+                    contexto.SaveChanges();
+                    return livro;
+                }
+                return null;
+            }
+            return null;
+        }
+        public Livro PedirNovaRevisao(int isbn)
+        {
+            Livro livro = contexto.Livros.FirstOrDefault(x => x.Isbn == isbn);
+            bool livroExiste = livro != null;
+            if (livroExiste)
+            {
+                livro.IdRevisor = null;
+                livro.DataRevisao = null;
+                contexto.Entry(livro).State = EntityState.Modified;
+                contexto.SaveChanges();
+                return livro;
+            }
+            return null;
         }
         public Livro Delete(int isbn)
         {
@@ -95,6 +173,11 @@ namespace CWI.EditoraCresccer.Repositorios
         }
         public Livro Modificar(int isbn, Livro livro)
         {
+            bool livroNaoExiste = contextoTeste.Livros.FirstOrDefault(l => l.Isbn == livro.Isbn) == null;
+            if (livroNaoExiste)
+            {
+                return null;
+            }
             contexto.Entry(livro).State = EntityState.Modified;
             contexto.SaveChanges();
             return contexto.Livros.FirstOrDefault(x => x.Isbn == isbn);
