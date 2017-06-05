59fb8b1d954f79facbb0c153c28b96929ac5b8b1
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-05 03:31:38 -0300

    feat(ServicesLivros): implementação de novos métodos na service
    listLimitadaCompleta() = Retorna a lista com todos os livros, incluindo os não publicados.
    Corração na chamada dentro de listLancamentos()

--- C#/EditoraCrescerFront/services/servicesLivros.js
+++ C#/EditoraCrescerFront/services/servicesLivros.js
@@ -4,22 +4,45 @@ modulo.factory('servicesLivros',function($http){ //servicesInstrutores deve esta
 
     //Retorna lista de aulas
     function listLimitada(quantidade,skip){
-        // let propriedades = {
-        //      url: urlBase+'/Livros',
-        //      method: 'GET',
-        //      params: {
-        //          quantidade:quantidade,
-        //          skip:skip
-        //      }
-        // }
         return $http.get(urlBase+'Livros/ListaLimitada/'+quantidade+'/'+skip);
     }
+    function listLimitadaCompleta(quantidade,skip){
+        return $http.get(urlBase+'Livros/ListaLimitadaCompleta/'+quantidade+'/'+skip); 
+    }
     function listLancamentos(){
-        return $http.get(urlBase+'/Livros/Lancamentos');
+        return $http.get(urlBase+'Livros/Lancamentos');
+    }
+    function alterarLivro(livro){
+        return $http.put(urlBase+'Livros/'+livro.Isbn, livro);
+    }
+    function revisarLivro(idRevisor,isbn){
+        return $http.put(urlBase+'Livros/revisar/'+idRevisor+'/'+isbn);
+    }
+    function publicarLivro(isbn){
+        return $http.put(urlBase+'Livros/publicar/'+isbn);
+    }
+    function pedirNovaRevisao(isbn){
+        return $http.put(urlBase+'Livros/novaRevisao/'+isbn);
     }
     return { 
         listLimitada:listLimitada,
-        listLancamentos:listLancamentos
+        listLancamentos:listLancamentos,
+        alterarLivro:alterarLivro,
+        listLimitadaCompleta:listLimitadaCompleta,
+        revisarLivro:revisarLivro,
+        publicarLivro:publicarLivro,
+        pedirNovaRevisao:pedirNovaRevisao
     }; 
 
 });
+
+/*
+let propriedades = {
+   url: urlBase+'Livros/revisar/',
+   method: 'PUT',
+   params: {
+      idRevisor:idRevisor,
+      isbn:isbn
+   }
+} 
+*/
