4e65298e4403692e501ec27fee410fa946fd9ae4
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-05 03:19:38 -0300

    feat(ControllerBasica): adição da ControllerBasica ao projeto

---
+++ C#/EditoraCrescer/EditoraCresccer/EditoraCrescer.Api/Controllers/ControllerBasica.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EditoraCrescer.Api.Controllers
+{
+    public class ControllerBasica : ApiController
+    {
+
+        public HttpResponseMessage ResponderOK(object dados = null)
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, new { dados });
+        }
+
+        public HttpResponseMessage ResponderErro(params string[] mensagens)
+        {
+            return Request.CreateResponse(HttpStatusCode.BadRequest, new { mensagens });
+        }
+
+        public HttpResponseMessage ResponderErro(IEnumerable<string> mensagens)
+        {
+            return Request.CreateResponse(HttpStatusCode.BadRequest, new { mensagens });
+        }
+    }
+}
