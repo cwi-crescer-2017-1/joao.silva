e0b41fb2cd5ec89dbd7778206072247888ad0d82
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-06 17:53:48 -0300

    feat(EntityFramework): arquivo para configurar o EF

---
+++ C#/ProdutoraCrescer/ProdutoraCrescer.Api/ProdutoraCrescer.Infraestrutura/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace ProdutoraCrescer.Infraestrutura.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<ProdutoraCrescer.Infraestrutura.Contexto>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(ProdutoraCrescer.Infraestrutura.Contexto context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
