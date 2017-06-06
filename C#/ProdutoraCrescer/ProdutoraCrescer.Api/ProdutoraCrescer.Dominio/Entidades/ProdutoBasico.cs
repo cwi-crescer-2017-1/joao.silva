ac4797fc7d2bbcb460f6ab335f20182188147820
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-06 17:50:35 -0300

    feat(ProdutoBasico): classe abstrata para criação de produto

---
+++ C#/ProdutoraCrescer/ProdutoraCrescer.Api/ProdutoraCrescer.Dominio/Entidades/ProdutoBasico.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace ProdutoraCrescer.Dominio.Entidades
+{
+    public abstract class ProdutoBasico
+    {
+        public int Id { get; protected set; }
+        public string Nome { get; protected set; }
+        public decimal CustoDiaria { get; protected set; }
+        public decimal CustoMulta { get; protected set; }
+        protected List<string> Mensagens { get; set; }
+
+        public ProdutoBasico()
+        {
+            Mensagens = new List<string>();
+        }
+
+        public abstract bool Validar();
+    }
+}
