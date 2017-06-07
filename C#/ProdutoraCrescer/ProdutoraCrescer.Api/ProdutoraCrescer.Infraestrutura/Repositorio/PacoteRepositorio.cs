c87b663055930261f9aa718901deebbb4f017055
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-07 00:57:47 -0300

    feat(PacoteRepositorio): implementação do repositorio para a Entidade Pacote

---
+++ C#/ProdutoraCrescer/ProdutoraCrescer.Api/ProdutoraCrescer.Infraestrutura/Repositorio/PacoteRepositorio.cs
@@ -0,0 +1,34 @@
+﻿using ProdutoraCrescer.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProdutoraCrescer.Infraestrutura.Repositorio
+{
+    public class PacoteRepositorio : IDisposable
+    {
+        private Contexto contexto;
+
+        public PacoteRepositorio()
+        {
+            contexto = new Contexto();
+        }
+
+        public Pacote ObterPorId(int id)
+        {
+            return contexto.Pacotes.FirstOrDefault(pacote => pacote.Id == id);
+        }
+
+        public List<Pacote> ObterLista()
+        {
+            return contexto.Pacotes.ToList();
+        }
+
+        public void Dispose()
+        {
+            contexto.Dispose();
+        }
+    }
+}
