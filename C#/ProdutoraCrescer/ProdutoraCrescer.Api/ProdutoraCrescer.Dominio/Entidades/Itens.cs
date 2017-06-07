45177cc0d9c7575c4412ac7d80a73e3a8027a262
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-07 01:03:24 -0300

    feat(Itens): implementação de objeto suporte para criar reserva

---
+++ C#/ProdutoraCrescer/ProdutoraCrescer.Api/ProdutoraCrescer.Dominio/Entidades/Itens.cs
@@ -0,0 +1,20 @@
+﻿namespace ProdutoraCrescer.Dominio.Entidades
+{
+    public class Itens
+    {
+        public Pacote Pacote { get; private set; }
+        public Festa Festa { get; private set; }
+        public Opcional Opcional { get; private set; }
+        public Usuario Usuario { get; private set; }
+        public Cliente Cliente { get; private set; }
+
+        public Itens(Pacote pacote, Festa festa, Opcional opcional, Usuario usuario, Cliente cliente)
+        {
+            Pacote = pacote;
+            Festa = festa;
+            Opcional = opcional;
+            Usuario = usuario;
+            Cliente = cliente;
+        }
+    }
+}
