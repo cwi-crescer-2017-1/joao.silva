3fb252266d0a4d19817cc543594fb7778f999c09
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-07 18:55:08 -0300

    feat(serviceRegistro): adição do cargo na função de registrar

--- C#/EditoraCrescerFront/services/serviceRegistro.js
+++ C#/EditoraCrescerFront/services/serviceRegistro.js
@@ -2,14 +2,21 @@ modulo.factory('serviceRegistro',function($http){
 
     let urlBase = 'http://localhost:60184/api/';
 
-    function registrar(nome,email,senha){
+    function registrar(nome,email,senha,cargo){
         let usuario = {};
         usuario.Nome = nome;
         usuario.Email = email;
         usuario.Senha =  senha;
-        return $http.post(urlBase+'usuarios/registrar',usuario);
+        usuario.Cargo = cargo;
+        return $http.post(urlBase+'usuario/registrar',usuario);
     }
+
+    function obter(){
+        return $http.get(urlBase+'usuario/');
+    }
+
     return { 
-        registrar:registrar
+        registrar:registrar,
+        obter:obter
     }; 
 });
