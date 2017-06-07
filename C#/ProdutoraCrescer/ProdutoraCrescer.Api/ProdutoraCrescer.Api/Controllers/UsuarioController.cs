14c73fd12bbaab53faef79cbfa8a5358a9c1d209
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-07 01:05:10 -0300

    feat(UsuarioController): implementação da controller para o Usuario

---
+++ C#/ProdutoraCrescer/ProdutoraCrescer.Api/ProdutoraCrescer.Api/Controllers/UsuarioController.cs
@@ -0,0 +1,49 @@
+﻿using ProdutoraCrescer.Infraestrutura.Repositorio;
+using System.Net.Http;
+using System.Threading;
+using System.Web.Http;
+using ProdutoraCrescer.Api.App_Start;
+using System.Collections.Generic;
+
+namespace ProdutoraCrescer.Api.Controllers
+{
+    [AllowAnonymous]
+    [RoutePrefix("api/usuario")]
+    public class UsuarioController : ControllerBasica
+    {
+        readonly UsuarioRepositorio repositorio;
+
+        public UsuarioController()
+        {
+            repositorio = new UsuarioRepositorio();
+        }
+
+        [HttpPost]
+        [Route("registrar")]
+        public HttpResponseMessage Post(dynamic usuario) //Nome,Senha,Email,Cargo
+        {
+            List<string> resposta = repositorio.Criar(usuario);
+            if (resposta == null)
+            {
+                return ResponderOK(usuario);
+            }
+            else
+            {
+                return ResponderErro(resposta.ToArray());
+            }
+        }
+
+        [HttpGet, BasicAuthorization]
+        [Route()]
+        public HttpResponseMessage Obter()
+        {
+            var usuario = repositorio.Obter(Thread.CurrentPrincipal.Identity.Name);
+
+            if (usuario == null)
+            {
+                return ResponderErro("Usuário não encontrado.");
+            }
+            return ResponderOK(new { usuario.Id, usuario.Nome, usuario.Permissao, usuario.Email });
+        }
+    }
+}
