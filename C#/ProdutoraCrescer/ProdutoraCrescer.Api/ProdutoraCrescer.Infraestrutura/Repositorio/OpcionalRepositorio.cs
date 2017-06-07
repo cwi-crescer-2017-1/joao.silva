10af6818e2074703bff39ed96cead0a5b9e8613d
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-07 00:58:08 -0300

    feat(OpcionalRepositorio): implementação de um repositorio para a Entidade Opcional

---
+++ C#/ProdutoraCrescer/ProdutoraCrescer.Api/ProdutoraCrescer.Infraestrutura/Repositorio/OpcionalRepositorio.cs
@@ -0,0 +1,32 @@
+﻿using ProdutoraCrescer.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProdutoraCrescer.Infraestrutura.Repositorio
+{
+    public class OpcionalRepositorio : IDisposable
+    {
+        private Contexto contexto;
+
+        public OpcionalRepositorio()
+        {
+            contexto = new Contexto();
+        }
+
+        public Opcional ObterPorId(int id)
+        {
+            return contexto.Opcionais.FirstOrDefault(opcional => opcional.Id == id);
+        }
+
+        public List<Opcional> ObterLista()
+        {
+            return contexto.Opcionais.ToList();
+        }
+
+        public void Dispose()
+        {
+            contexto.Dispose();
+        }
+    }
+}
