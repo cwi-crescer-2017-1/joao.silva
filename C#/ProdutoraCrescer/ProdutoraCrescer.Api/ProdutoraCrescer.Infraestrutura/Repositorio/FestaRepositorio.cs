775a9ed6e2f5a4d24ca14acde4367695ec996d9e
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-07 00:58:33 -0300

    feat(FestaRepositorio): implementação do repositório para a Entidade Festa

---
+++ C#/ProdutoraCrescer/ProdutoraCrescer.Api/ProdutoraCrescer.Infraestrutura/Repositorio/FestaRepositorio.cs
@@ -0,0 +1,32 @@
+﻿using ProdutoraCrescer.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProdutoraCrescer.Infraestrutura.Repositorio
+{
+    public class FestaRepositorio : IDisposable
+    {
+        private Contexto contexto;
+
+        public FestaRepositorio()
+        {
+            contexto = new Contexto();
+        }
+
+        public Festa ObterPorId(int id)
+        {
+            return contexto.Festas.FirstOrDefault(festa => festa.Id == id);
+        }
+
+        public List<Festa> ObterLista()
+        {
+            return contexto.Festas.ToList();
+        }
+
+        public void Dispose()
+        {
+            contexto.Dispose();
+        }
+    }
+}
