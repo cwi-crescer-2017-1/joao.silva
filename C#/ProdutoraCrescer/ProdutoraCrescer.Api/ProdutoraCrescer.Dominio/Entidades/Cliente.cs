b2fcffd48839db20059ddc79723506683f14afcc
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-07 01:04:22 -0300

    feat(ClienteValidacao): implementação de validações para casos nulos

--- C#/ProdutoraCrescer/ProdutoraCrescer.Api/ProdutoraCrescer.Dominio/Entidades/Cliente.cs
+++ C#/ProdutoraCrescer/ProdutoraCrescer.Api/ProdutoraCrescer.Dominio/Entidades/Cliente.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace ProdutoraCrescer.Dominio.Entidades
 {
@@ -13,9 +13,9 @@ namespace ProdutoraCrescer.Dominio.Entidades
         public string Genero { get; private set; }
         public DateTime DataNascimento { get; private set; }//DATATIME.UTCNOW - Horario Universal
         public string Email { get; private set; }
-        private List<string> Mensagens { get; set; }
+        public List<string> Mensagens { get; private set; }
 
-        protected Cliente() { } //Construtor para o EF
+        protected Cliente() { Mensagens = new List<string>(); } //Construtor para o EF
 
         public Cliente(string nome, string endereco, string cpf, string genero, DateTime dataNascimento, string email)
         {
@@ -26,34 +26,50 @@ namespace ProdutoraCrescer.Dominio.Entidades
             Genero = genero;
             DataNascimento = dataNascimento;
             Email = email;
+            Mensagens = new List<string>();
         }
 
         public bool Validar()
         {
             Mensagens.Clear();
 
-            if (string.IsNullOrWhiteSpace(Nome))
+            if (string.IsNullOrWhiteSpace(Nome) || Nome.Length > 100)
+            {
                 Mensagens.Add("Nome é inválido.");
+            }
 
-            if(Nome.Length > 100)
-                Mensagens.Add("Nome é inválido.");
-
-            if (string.IsNullOrWhiteSpace(Endereco))
+            if (string.IsNullOrWhiteSpace(Endereco) || Endereco.Length > 500)
+            {
                 Mensagens.Add("Endereco é inválido.");
-
-            if(Endereco.Length > 500)
-                Mensagens.Add("Endereco muito grande");
+            }
 
             if (string.IsNullOrWhiteSpace(CPF) || CPF.Length != 11)
+            {
                 Mensagens.Add("CPF é inválido.");
+            }
 
             if (string.IsNullOrWhiteSpace(Genero))
+            {
                 Mensagens.Add("Genero é inválido.");
+            }
 
-            if (string.IsNullOrWhiteSpace(Email))
+            if (string.IsNullOrWhiteSpace(Email) || EmailInvalido() || Email.Length > 100)
+            {
                 Mensagens.Add("Email é inválido.");
+            }
 
             return Mensagens.Count == 0;
+        }
+
+        private bool EmailInvalido()
+        {
+            Regex rg = new Regex(@"^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$");
+
+            if (rg.IsMatch(Email))
+            {
+                return false;
+            }
+            return true;
         }
     }
 }
