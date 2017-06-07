0f565d0622f411e1d3aefae41272a4e5b5926457
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-07 01:03:48 -0300

    fix(Validacao): diminuição da repetição de código dentro da validação de Nome

--- C#/ProdutoraCrescer/ProdutoraCrescer.Api/ProdutoraCrescer.Dominio/Entidades/Festa.cs
+++ C#/ProdutoraCrescer/ProdutoraCrescer.Api/ProdutoraCrescer.Dominio/Entidades/Festa.cs
@@ -1,8 +1,10 @@
-﻿namespace ProdutoraCrescer.Dominio.Entidades
+﻿using System.Collections.Generic;
+
+namespace ProdutoraCrescer.Dominio.Entidades
 {
     public class Festa : ProdutoBasico
     {
-        protected Festa() { }
+        protected Festa() { Mensagens = new List<string>();  }
 
         public Festa(string nome, decimal custoDiaria, decimal custoMulta)
         {
@@ -10,23 +12,27 @@
             Nome = nome;
             CustoDiaria = custoDiaria;
             CustoMulta = custoMulta;
+            Mensagens = new List<string>();
         }
 
         public override bool Validar()
         {
             Mensagens.Clear();
 
-            if (string.IsNullOrWhiteSpace(Nome))
+            if (string.IsNullOrWhiteSpace(Nome) || Nome.Length > 100)
+            {
                 Mensagens.Add("Nome é inválido.");
+            }
 
-            if (Nome.Length > 100)
-                Mensagens.Add("Nome é inválido.");
-
-            if (CustoDiaria<0)
+            if (CustoDiaria < 0)
+            {
                 Mensagens.Add("Custo diário é inválido.");
+            }
 
-            if (CustoMulta<0)
+            if (CustoMulta < 0)
+            {
                 Mensagens.Add("Custo da multa é inválido.");
+            }
 
             return Mensagens.Count == 0;
         }
