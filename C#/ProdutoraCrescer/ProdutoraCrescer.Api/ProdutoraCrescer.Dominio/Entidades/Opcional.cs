fa09939b92cf399dc88c6845799c24d46630fb08
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-07 01:02:54 -0300

    feat(Reservar): implementação de método para reservar um opcional e correção na chamada de Mensagens junto com diminuição de repetição no Validar em Nome

--- C#/ProdutoraCrescer/ProdutoraCrescer.Api/ProdutoraCrescer.Dominio/Entidades/Opcional.cs
+++ C#/ProdutoraCrescer/ProdutoraCrescer.Api/ProdutoraCrescer.Dominio/Entidades/Opcional.cs
@@ -1,10 +1,12 @@
-﻿namespace ProdutoraCrescer.Dominio.Entidades
+﻿using System.Collections.Generic;
+
+namespace ProdutoraCrescer.Dominio.Entidades
 {
     public class Opcional : ProdutoBasico
     {
         public int Quantidade { get; private set; }
 
-        protected Opcional() { }
+        protected Opcional() { Mensagens = new List<string>(); }
 
         public Opcional(string nome, decimal custoDiaria, decimal custoMulta, int quantidade)
         {
@@ -13,26 +15,41 @@
             CustoDiaria = custoDiaria;
             CustoMulta = custoMulta;
             Quantidade = quantidade;
+            Mensagens = new List<string>();
         }
 
+        public bool ReservarOpcional()
+        {
+            if (Quantidade > 1)
+            {
+                Quantidade = Quantidade - 1;
+                return true;
+            }
+            return false;
+        }
         public override bool Validar()
         {
             Mensagens.Clear();
 
-            if (string.IsNullOrWhiteSpace(Nome))
+            if (string.IsNullOrWhiteSpace(Nome) || Nome.Length > 100)
+            {
                 Mensagens.Add("Nome é inválido.");
-
-            if (Nome.Length > 100)
-                Mensagens.Add("Nome é inválido.");
+            }
 
             if (CustoDiaria < 0)
+            {
                 Mensagens.Add("Custo diário inválido.");
+            }
 
             if (CustoMulta < 0)
+            {
                 Mensagens.Add("Custo da multa inválido.");
+            }
 
             if (Quantidade < 0)
+            {
                 Mensagens.Add("Quantidade inválida.");
+            }
 
             return Mensagens.Count == 0;
         }
