b6929700697f0da7adb50044553e27dc9e7a61b9
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-08 00:19:32 -0300

    feat(controllerMenu): implementação de métodos para autenticação

--- C#/ProdutoraCrescer/ProdutoraCrescer.Front/controllers/controllerMenu.js
+++ C#/ProdutoraCrescer/ProdutoraCrescer.Front/controllers/controllerMenu.js
@@ -1,14 +1,28 @@
 modulo.controller('controllerMenu',['$scope','toastr','$location','authService','authConfig','serviceUsuario','serviceCliente','serviceFesta','serviceOpcional','servicePacote','serviceReserva', function(model,toastr,$location,authService,authConfig,serviceUsuario,serviceCliente,serviceFesta,serviceOpcional,servicePacote,serviceReserva){
     model.logout = logout;
     model.atrasos = [];
+    model.gerente = false;
+
+    if(authService.isntAutenticado()){
+        $location.path('/home');   
+    }
+    if(authService.possuiPermissao('Gerente')){
+        model.gerente = true;   
+    }
+
+    model.gerencia = gerencia;
+
+    function gerencia(){
+        $location.path('/gerencia');
+    }
+
+    model.logout = logout;
     function logout(){
         authService.logout();
-        livroSendoPublicado = null;
-        livroSendoRevisado = null;
         toastr.info('Deslogado');
-        model.logado = false;
-        model.usuario = {};
+        $location.path('/home'); 
     }
+
     relatorioAtrasos();
     function relatorioAtrasos() {
         serviceReserva.ObterRelatorioAtrasos().then(function(response){
