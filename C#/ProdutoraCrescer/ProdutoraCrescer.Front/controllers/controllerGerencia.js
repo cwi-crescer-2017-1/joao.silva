e1d1cb5b6bac54183bb94b4a2c55642f433108f0
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-08 00:19:14 -0300

    feat(Data): implementação de método para formatação de data e para autenticação

--- C#/ProdutoraCrescer/ProdutoraCrescer.Front/controllers/controllerGerencia.js
+++ C#/ProdutoraCrescer/ProdutoraCrescer.Front/controllers/controllerGerencia.js
@@ -1,15 +1,77 @@
-modulo.controller('controllerGerencia',['$scope','toastr','$location','authService','authConfig','serviceUsuario', function(model,toastr,$location,authService,authConfig,serviceUsuario){
-    model.logado = false;
+modulo.controller('controllerGerencia',['$scope','toastr','$location','authService','authConfig','serviceUsuario','serviceReserva', function(model,toastr,$location,authService,authConfig,serviceUsuario,serviceReserva){
+    model.reservas = {};
+    if(authService.isntAutenticado()){
+        $location.path('/home');
+    }
+    if(authService.naoPossuiPermissao('Gerente')){
+        $location.path('/adminstrativo'); 
+    }
 
-    if(authService.isAutenticado()){
-        $location.path('/adminstrativo');
-        model.logado = true;
+    model.menuAdministrativo = menuAdministrativo;
+    function menuAdministrativo(){
+        $location.path('/adminstrativo');  
     }
 
     model.logout = logout;
     function logout(){
         authService.logout();
         toastr.info('Deslogado');
-        model.logado = false;
+        $location.path('/home'); 
     }
+
+    model.relatorioMensal = relatorioMensal;
+    relatorioMensal(today());
+    function relatorioMensal(data) {
+        if(typeof data !== "undefined"){
+            dataFinal = FormataDataBD(data);
+            serviceReserva.ObterRelatorioLocacaoMensal(dataFinal).then(function(response){
+            model.reservas = response.data.dados.resposta.Reservas;
+            model.ValorRelatorio = response.data.dados.resposta.Valor;
+            });
+        }else{
+            toastr.error("Data inválida");
+        }
+    }
+
+    model.Devolvido = Devolvido;
+    function Devolvido(devolvido){
+        if(typeof devolvido === "undefined" || devolvido === null || devolvido === false){
+            return "Não";
+        }else{
+            return "Sim";
+        }
+    }
+
+    model.FormataData = FormataDataUsuario;
+    function FormataDataUsuario(dataString){
+        if(typeof dataString === "string"){
+            var regex = /-/gi;
+            dataString = dataString.substring(0,10);
+            dataString = dataString.replace(regex, '/');
+            return dataString; 
+        }
+        return "NaN";
+    }
+
+    function today(){
+        return new Date();
+    }
+
+    function FormataDataBD(data){
+        let dd = data.getDate();
+        let mm = data.getMonth()+1;
+        let yyyy = data.getFullYear();
+
+        if(dd<10) {
+            dd='0'+dd
+        } 
+
+        if(mm<10) {
+            mm='0'+mm
+        } 
+
+        data = yyyy+'-'+mm+'-'+dd;
+        return data;
+    }
+
 }]);
