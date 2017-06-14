af9410c7be8854007b29cb1a700e980e4d1fd489
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-14 18:20:08 -0300

    fix(UF): adição do estado na verificação de duplicidade

--- Banco de Dados II/Listas/Lista2-banco-II.sql
+++ Banco de Dados II/Listas/Lista2-banco-II.sql
@@ -1,24 +1,28 @@
 --Lista 02 Banco de Dados II
 
 --Exercicio 01
+CREATE INDEX IX_Cidade_NomeUF on Cidade(Nome,UF);
+
+CREATE INDEX IX_Cliente_Cidade on Cliente(IDCidade); -- DE 6 SEGUNDOS PARA 1 SEGUNDO
+
 DECLARE
  CURSOR C_Cidade_Duplicadas IS
-      Select Ci.Nome
+      Select Ci.Nome, Ci.UF
          From Cidade Ci
          Group by Ci.NOME, Ci.UF
          Having Count(Ci.NOME)>1;
          
-  CURSOR C_Cliente_Cidade(vNomeCidade in varchar2) IS
+  CURSOR C_Cliente_Cidade(vNomeCidade in varchar2, vUF in varchar2) IS
       Select cl.Nome as Nome_Cliente,c.Nome as Nome_Cidade
         From Cliente cl 
         Join Cidade c On cl.IDCidade = c.IDCidade
-        WHERE c.NOME = vNomeCidade;     
+        WHERE c.NOME = vNomeCidade AND c.UF = vUF;     
   BEGIN
     FOR cidade IN C_Cidade_Duplicadas LOOP
       DBMS_OUTPUT.PUT_LINE( '-----------------------');
       DBMS_OUTPUT.PUT_LINE( 'Cidade: '|| cidade.Nome );
       DBMS_OUTPUT.PUT_LINE( '-----------------------');
-      FOR cliente IN C_Cliente_Cidade(cidade.Nome) LOOP
+      FOR cliente IN C_Cliente_Cidade(cidade.Nome, cidade.UF) LOOP
         DBMS_OUTPUT.PUT_LINE( 'Nome: '||cliente.Nome_Cliente || ' - Cidade: '|| cliente.Nome_Cidade);
       END LOOP;
     END LOOP;
@@ -46,12 +50,15 @@ END;
 
 --Exercicio 03
 SELECT * FROM PEDIDO ORDER BY DATAPEDIDO ASC;
+
+CREATE INDEX IX_Pedido_Cliente On Pedido(IDCliente); 
 DECLARE
   CURSOR C_Cliente_Sem_Pedido IS
-    Select Distinct c.IDCliente, c.Nome, c.Situacao
+    Select c.IDCliente, c.Nome, c.Situacao
       From Cliente c
       Join Pedido Pd On c.IDCliente = Pd.IDCliente
       Where Pd.DataPedido > Add_months(TRUNC(sysdate),-6)
+      Group By c.IDCliente, c.Nome, c.Situacao
       Order By c.IDCliente;
 BEGIN  
     FOR cliente IN C_Cliente_Sem_Pedido LOOP
@@ -63,19 +70,9 @@ BEGIN
 END;
 
 --Exercicio 04
-/*
-Previsão de Materiais
-
-Faça uma rotina que receba dois parâmetros:
-
-IDProduto
-Mês e Ano
-E então faça uma rotina que verifique no ANO/MÊS para o produto informado qual a lista de materiais (incluindo a quantidade) 
-é necessário para atender todos os Pedidos desde período. Deve imprimir o nome do material e quantidade total.
-*/
 
 DECLARE
-  vIdProduto number := 1;
+  vIdProduto number := 4246;
   vAno number := 2017;
   vMes number := 1;
   vQuantidade number;
@@ -85,11 +82,9 @@ DECLARE
      Inner Join ProdutoMaterial pm On p.IDProduto = pm.IDProduto
      Inner Join Material m On pm.IDMaterial = m.IDMaterial
      Where p.IDProduto = vIdProduto;
-     
-BEGIN  
-    FOR vMaterial IN C_Produto_Materiais LOOP
-        Select SUM(pm.Quantidade)
-        INTO vQuantidade
+  
+  Cursor C_Material_Quantidade(vIDMaterial in number) IS
+    Select SUM(pm.Quantidade) as Quantidade
         From Material m
         Inner Join ProdutoMaterial pm On m.IDMaterial = pm.IDMaterial
         Inner Join Produto p On pm.IDProduto = p.IDProduto
@@ -99,12 +94,17 @@ BEGIN
             EXTRACT(YEAR FROM pe.DataPedido) = vAno 
             AND EXTRACT(MONTH FROM pe.DataPedido) = vMes 
             AND p.IDProduto = vIdProduto
-            AND pm.IDMaterial = vMaterial.IDMaterial
+            AND pm.IDMaterial = vIDMaterial
         Group By m.IDMaterial,m.Descricao
-        Order By 1;
-        DBMS_OUTPUT.PUT_LINE( 'Material: '||vMaterial.Descricao || ' - Quantidade: '|| vQuantidade);
+        Order By 1; 
+BEGIN  
+    FOR vProduto IN C_Produto_Materiais LOOP
+        FOR vMaterial IN C_Material_Quantidade(vProduto.IDMaterial) LOOP
+          DBMS_OUTPUT.PUT_LINE( 'Material: '||vProduto.Descricao || ' - Quantidade: '|| vMaterial.Quantidade);
+        END LOOP;
     END LOOP;
 END;
+
 
 
 
