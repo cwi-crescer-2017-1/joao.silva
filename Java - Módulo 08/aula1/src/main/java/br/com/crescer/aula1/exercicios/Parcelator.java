aea8547c49080bc30d1dd4f1ea7ce7c4df3d0687
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-21 16:05:18 -0300

    fix(Sobra): subtração da sobra no calculo da parcela

--- Java - Módulo 08/aula1/src/main/java/br/com/crescer/aula1/exercicios/Parcelator.java
+++ Java - Módulo 08/aula1/src/main/java/br/com/crescer/aula1/exercicios/Parcelator.java
@@ -7,11 +7,13 @@ package br.com.crescer.aula1.exercicios;
 
 import br.com.crescer.aula1.exercicios.Interfaces.IParcelator;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.TreeMap;
+
 
 /**
  *
@@ -21,15 +23,21 @@ public class Parcelator implements IParcelator{
 
     @Override
     public Map<String, BigDecimal> calcular(BigDecimal valorParcelar, int numeroParcelas, double taxaJuros, Date dataPrimeiroVencimento) {
-        Map<String, BigDecimal> parcelaVencimento = new TreeMap<>();
+        Map<String, BigDecimal> parcelaVencimento = new LinkedHashMap<>();
         Calendar calendar = Calendar.getInstance();
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd"); 
-        double mensal = (valorParcelar.doubleValue()*(taxaJuros/100+1.0))/numeroParcelas;
-        BigDecimal mensalidade  = BigDecimal.valueOf(mensal);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy"); 
+        BigDecimal BigQtdParcelas = BigDecimal.valueOf(numeroParcelas);
+        BigDecimal BigTaxaJuros = BigDecimal.valueOf(taxaJuros);
+        BigDecimal totalAPagar;
+        totalAPagar = valorParcelar.multiply(BigTaxaJuros.divide(BigDecimal.valueOf(100)).add(BigDecimal.ONE));
+        BigDecimal mensalidade;
+        mensalidade = totalAPagar.divide(BigQtdParcelas, 2, RoundingMode.HALF_UP);
         calendar.setTime(dataPrimeiroVencimento);
-            for(int i=0; i<numeroParcelas; i++){
+        BigDecimal sobra = mensalidade.multiply(BigQtdParcelas).subtract(totalAPagar);
+        for(int i=0; i<numeroParcelas; i++){
             String data = simpleDateFormat.format(calendar.getTime());
-            parcelaVencimento.put(data, mensalidade);
+            parcelaVencimento.put(data, mensalidade.subtract(sobra));
+            sobra = BigDecimal.ZERO;
             calendar.add(Calendar.MONTH, 1);
         }
         return parcelaVencimento;
