a74c2ac450830b2e1b52dd94beaa859ea5bea697
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-21 01:04:42 -0300

    feat(StringUtils): implementação da classe que implementa a interface IStringUtils com os métodos isEmpty, Inverter, contaVogais e isPalindromo prontos, além de um método padronizaString que retorna uma String "limpa" (sem acentos e em UpperCase) porém ainda com espaços

---
+++ Java - Módulo 08/aula1/src/main/java/br/com/crescer/aula1/exercicios/StringUtils.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.crescer.aula1.exercicios;
+
+import br.com.crescer.aula1.exercicios.Interfaces.IStringUtils;
+import java.text.Normalizer;
+import java.util.Arrays;
+
+/**
+ *
+ * @author jpedr
+ */
+public class StringUtils implements IStringUtils{
+
+    public StringUtils(){}
+    @Override
+    public boolean isEmpty(String string) {
+        if(string != null){
+           boolean stringNaoVazia = string.trim().length()>0;
+           if(stringNaoVazia){
+               return false;
+           }
+        }
+        return true;      
+    }
+
+    @Override
+    public String inverter(String string) {
+        StringBuilder reversa = new StringBuilder(string);
+        reversa.reverse();
+        return reversa.toString();
+    }
+
+    @Override
+    public int contaVogais(String string) {
+        if(!isEmpty(string)){
+            String strigPadronizada = padronizaString(string);
+            return (int) Arrays
+            	            .asList(strigPadronizada.split(""))
+                            .stream()
+                            .filter(s-> "AEIOU".contains(s))
+                            .count();
+        }
+        return 0;
+    }
+    
+    @Override
+    public boolean isPalindromo(String string) {
+        if(!isEmpty(string)){
+            String strigPadronizadaSemEspaco = padronizaString(string).replaceAll(" ","");
+            String stringInvertida = inverter(strigPadronizadaSemEspaco);
+            if(strigPadronizadaSemEspaco.equals(stringInvertida)){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    private String padronizaString(String string){
+        return Normalizer
+                    .normalize(string, Normalizer.Form.NFD)
+                    .replaceAll("[^\\p{ASCII}]", "").toUpperCase();
+    }
+    
+}
