768b212434ac39d8c47945c8e07d8341d1740082
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-21 01:00:54 -0300

    feat(InterfaceIStringUtils): adição da interface StringUtils dos exercicios da aula 1

---
+++ Java - Módulo 08/aula1/src/main/java/br/com/crescer/aula1/exercicios/Interfaces/IStringUtils.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.crescer.aula1.exercicios.Interfaces;
+
+/**
+ * @author carloshenrique
+ */
+public interface IStringUtils {
+    
+    boolean isEmpty(String string);
+    
+    String inverter(String string);
+    
+    int contaVogais(String string);
+    
+    boolean isPalindromo(String string);
+    
+}
