315ac7f59cd4b36755c244879c883da9bb46f712
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-21 23:15:16 -0300

    feat(InterfacesExercicio): adição das interfaces para a resolução da atividade de java dia 2

---
+++ Java - Módulo 08/Aula2/src/main/java/br/com/crescer/aula2/exercicios/ReaderUtils.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.crescer.aula2.exercicios;
+
+/**
+ * @author carloshenrique
+ */
+public interface ReaderUtils {
+    
+    String read(String string);
+    
+}
---
+++ Java - Módulo 08/Aula2/src/main/java/br/com/crescer/aula2/exercicios/WriterUtils.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.crescer.aula2.exercicios;
+
+/**
+ * @author carloshenrique
+ */
+public interface WriterUtils {
+
+    void write(String file, String conteudo);
+
+}
