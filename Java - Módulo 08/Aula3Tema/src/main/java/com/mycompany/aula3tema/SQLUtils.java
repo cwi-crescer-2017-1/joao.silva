4a25fb7617ec4d9aadd1714a3e139fe790283f36
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-23 09:07:11 -0300

    feat(SQLUtils):adição da interface SQLUtils no projeto Aula3Tema

---
+++ Java - Módulo 08/Aula3Tema/src/main/java/com/mycompany/aula3tema/SQLUtils.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.aula3tema;
+
+import java.io.File;
+
+/**
+ *
+ * @author jpedr
+ */
+/**
+ * @author carloshenrique
+ */
+public interface SQLUtils {
+
+    void runFile(String filename);
+
+    String executeQuery(String query);
+    
+    void importCSV(File file);
+    
+    File importCSV(String query);
+
+}
