843363b362464ffc7f5a6c389c1b04da0efcd3b9
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-23 13:23:00 -0300

    feat(PessoaDao): classes e interfaces de dao para pessoa

---
+++ Java - Módulo 08/aula3/src/main/java/br/com/crescer/aula3/orm/dao/pessoa/PessoaDao.java
@@ -0,0 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.crescer.aula3.orm.dao.pessoa;
+
+/**
+ *
+ * @author joao.silva
+ */
+public interface PessoaDao extends Dao<Pessoa> {}
