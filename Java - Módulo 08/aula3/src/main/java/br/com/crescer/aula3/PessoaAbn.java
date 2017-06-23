49d007dc4ed5a353ad0a3393550b7454e2d086aa
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-23 13:23:28 -0300

    feat(PessoaAbn): classe de pessoa teste

---
+++ Java - Módulo 08/aula3/src/main/java/br/com/crescer/aula3/PessoaAbn.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.crescer.aula3;
+
+/**
+ *
+ * @author joao.silva
+ */
+public class PessoaAbn {
+    
+    private long id;
+    private String Nome;
+    
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+}
