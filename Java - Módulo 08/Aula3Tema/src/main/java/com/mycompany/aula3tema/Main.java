2ce4b85ba924ca4d88905064a9d71b0f5f87feb3
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-23 11:37:15 -0300

    feat(Main): arquivo de apoio

--- Java - Módulo 08/Aula3Tema/src/main/java/com/mycompany/aula3tema/Main.java
+++ Java - Módulo 08/Aula3Tema/src/main/java/com/mycompany/aula3tema/Main.java
@@ -5,6 +5,8 @@
  */
 package com.mycompany.aula3tema;
 
+import java.io.File;
+
 /**
  *
  * @author jpedr
@@ -13,6 +15,11 @@ public class Main {
     public static void main(String[] args){
         String query = "Select * From Estado";
         SQLUtils sqlUtil= new SQLUtilsImpl();
-        System.out.println(sqlUtil.executeQuery(query));
+//        System.out.println(sqlUtil.executeQuery(query));
+//        String arquivoSQL = "C:\\Users\\jpedr\\Desktop\\Teste.sql";
+//        sqlUtil.runFile(arquivoSQL);
+        String arquivoCSV = "C:\\Users\\jpedr\\Documents\\Pais.csv";
+        File file = new File(arquivoCSV);
+        sqlUtil.importCSV(file);
     }
 }
