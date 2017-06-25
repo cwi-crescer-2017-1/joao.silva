248439fcdbf03c0784a3aaa47698228fccf48a7c
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-25 19:59:49 -0300

    feat(Cliente): implementação da classe pojo do Cliente

---
+++ Java - Módulo 08/ExercicioAula4/Pojo/src/main/java/br/com/crescer/Cliente.java
@@ -0,0 +1,174 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.crescer;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import static javax.persistence.GenerationType.SEQUENCE;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author jpedr
+ */
+@Entity
+@Table(name = "CLIENTE")
+public class Cliente implements Serializable {
+
+    protected Cliente() {}
+   
+    public Cliente(String nome,String cpf,String celular){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.celular = celular;
+    }
+    
+    @Id
+    @GeneratedValue(strategy = SEQUENCE, generator = "SEQ_CLIENTE")
+    @SequenceGenerator(name = "SEQ_CLIENTE", sequenceName = "SEQ_CLIENTE")
+    @Basic(optional = false)
+    @Column(name = "ID")
+    private Long id;
+
+    @Basic(optional = false)
+    @Column(name = "NOME")
+    private String nome;
+    
+    @Basic(optional = false)
+    @Column(name = "CPF")
+    private String cpf;
+    
+    @Basic(optional = false)
+    @Column(name = "CELULAR")
+    private String celular;
+
+    @Column(name = "RG")
+    private String rg;
+
+    @Column(name = "BAIRRO")
+    private String bairro;
+
+    @Column(name = "CIDADE")
+    private String cidade;
+
+    @Column(name = "NUMERO_CASA")
+    private String numeroCasa;
+
+    @Column(name = "RUA")
+    private String rua;
+
+    @Column(name = "EMAIL")
+    private String email;
+
+    @Column(name = "TELEFONE")
+    private String telefone;
+    
+    @Temporal(TemporalType.DATE)
+    @Column(name = "NASCIMENTO")
+    private Date nascimento;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getNumeroCasa() {
+        return numeroCasa;
+    }
+
+    public void setNumeroCasa(String numeroCasa) {
+        this.numeroCasa = numeroCasa;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Date getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(Date nascimento) {
+        this.nascimento = nascimento;
+    }
+    
+    
+}
