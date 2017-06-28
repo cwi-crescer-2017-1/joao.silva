357c1d78c960baefd3d0b9a98fb52d176e85bb30
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-28 17:35:00 -0300

    feat(Run): implementação de executavel para inicialização do servidor

---
+++ Java - Módulo 08/aula7/src/main/java/br/com/crescer/Run.java
@@ -0,0 +1,13 @@
+package br.com.crescer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Run {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Run.class, args);
+    }
+
+}
