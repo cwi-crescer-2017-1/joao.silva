1ca787909b5d385e8c579387966ea521dde5ca77
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-28 17:33:43 -0300

    feat(Repository): implementação do repositório para o autor

---
+++ Java - Módulo 08/aula7/src/main/java/br/com/crescer/repository/AtorRepository.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.crescer.repository;
+
+import br.com.crescer.entity.Ator;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author joao.silva
+ */
+public interface AtorRepository extends CrudRepository<Ator, Long>{
+    
+}
