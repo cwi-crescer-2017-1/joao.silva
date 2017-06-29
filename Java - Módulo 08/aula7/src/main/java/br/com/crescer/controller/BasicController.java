8c7b968459f7f22033c62129e69e6167021e2de1
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-29 01:20:38 -0300

    feat(BasicController): implementação de uma controller básica para crud

---
+++ Java - Módulo 08/aula7/src/main/java/br/com/crescer/controller/BasicController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.crescer.controller;
+
+import java.util.List;
+import org.springframework.data.domain.Page;
+
+/**
+ *
+ * @author jpedr
+ * @param <Entity>
+ * @param <ID>
+ */
+public interface BasicController<Entity,ID> {
+    Entity save(Entity e);
+    
+    void delete(Entity e);
+
+    Entity findOne(ID id);
+
+    List<Entity> findAll();
+    
+    Page<Entity> findPage(int page, int size);
+}
