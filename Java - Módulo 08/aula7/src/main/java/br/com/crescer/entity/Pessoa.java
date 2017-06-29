bf3b597926fdcb1e02be96badd9d915f7622e8c7
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-29 16:02:16 -0300

    feat(Pessoa): entidade de pessoa auto gerada baseada na tabela pessoa do banco de dados

---
+++ Java - Módulo 08/aula7/src/main/java/br/com/crescer/entity/Pessoa.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.crescer.entity;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author joao.silva
+ */
+@Entity
+@Table(name = "PESSOA", catalog = "", schema = "MODULO8")
+public class Pessoa implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "ID")
+    private Integer id;
+    @Size(max = 60)
+    @Column(name = "NOME")
+    private String nome;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+}
