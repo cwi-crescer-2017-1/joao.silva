61b05a52b07b2a6014de4628046d8a8feaa31544
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-29 01:13:22 -0300

    feat(GeneroRepository): implementação da interface do repositório de Genero

---
+++ Java - Módulo 08/aula7/src/main/java/br/com/crescer/repository/GeneroRepository.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.crescer.repository;
+
+import br.com.crescer.entity.Genero;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+/**
+ *
+ * @author jpedr
+ */
+public interface GeneroRepository extends PagingAndSortingRepository<Genero, Long>{
+    
+}
