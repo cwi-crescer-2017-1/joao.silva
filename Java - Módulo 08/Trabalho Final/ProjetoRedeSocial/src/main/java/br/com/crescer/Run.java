9a2dc5d30c1e8dc3449cd54d1e406c3932e68d29
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-30 18:02:39 -0300

    feat(Run): arquivo de inicialização do projeto

---
+++ Java - Módulo 08/Trabalho Final/ProjetoRedeSocial/src/main/java/br/com/crescer/Run.java
@@ -0,0 +1,14 @@
+package br.com.crescer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author joao.silva
+ */
+@SpringBootApplication
+public class Run {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Run.class, args);
+    }
+}
