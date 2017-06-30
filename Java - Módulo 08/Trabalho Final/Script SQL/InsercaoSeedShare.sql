39a80e5b101fb3f19d22b8ff8490f119f563e9b7
JPedroSSilva <joao.silva@cwi.com.br>
2017-06-30 00:48:19 -0300

    feat(ScriptBanco): script para gerar o banco de dados e para inserir dados no mesmo

---
+++ Java - Módulo 08/Trabalho Final/Script SQL/InsercaoSeedShare.sql
@@ -0,0 +1,110 @@
+INSERT INTO Estado VALUES(99999991, 'Acre', 'Brasil');
+INSERT INTO Estado VALUES(99999992, 'Alagoas', 'Brasil');
+INSERT INTO Estado VALUES(99999993, 'Amazonas', 'Brasil');
+INSERT INTO Estado VALUES(99999994, 'Amapá', 'Brasil');
+INSERT INTO Estado VALUES(99999995, 'Bahia', 'Brasil');
+INSERT INTO Estado VALUES(99999996, 'Ceará', 'Brasil');
+INSERT INTO Estado VALUES(99999997, 'Distrito Federal', 'Brasil');
+INSERT INTO Estado VALUES(99999998, 'Espírito Santo', 'Brasil');
+INSERT INTO Estado VALUES(99999999, 'Goiás', 'Brasil');
+INSERT INTO Estado VALUES(99999981, 'Maranhão', 'Brasil');
+INSERT INTO Estado VALUES(99999982, 'Minas Gerais', 'Brasil');
+INSERT INTO Estado VALUES(99999983, 'Mato Grosso do Sul', 'Brasil');
+INSERT INTO Estado VALUES(99999984, 'Mato Grosso', 'Brasil');
+INSERT INTO Estado VALUES(99999985, 'Pará', 'Brasil');
+INSERT INTO Estado VALUES(99999986, 'Paraíba', 'Brasil');
+INSERT INTO Estado VALUES(99999987, 'Pernambuco', 'Brasil');
+INSERT INTO Estado VALUES(99999988, 'Piauí', 'Brasil');
+INSERT INTO Estado VALUES(99999989, 'Paraná', 'Brasil');
+INSERT INTO Estado VALUES(99999971, 'Rio de Janeiro', 'Brasil');
+INSERT INTO Estado VALUES(99999972, 'Rio Grande do Norte', 'Brasil');
+INSERT INTO Estado VALUES(99999973, 'Rondônia', 'Brasil');
+INSERT INTO Estado VALUES(99999974, 'Roraima', 'Brasil');
+INSERT INTO Estado VALUES(99999975, 'Rio Grande do Sul', 'Brasil');
+INSERT INTO Estado VALUES(99999976, 'Santa Catarina', 'Brasil');
+INSERT INTO Estado VALUES(99999977, 'Sergipe', 'Brasil');
+INSERT INTO Estado VALUES(99999978, 'São Paulo', 'Brasil');
+INSERT INTO Estado VALUES(99999979, 'Tocantins', 'Brasil');
+
+INSERT INTO USUARIO(ID,Email,Senha) VALUES(99999997,'teste@teste.com','585d4b0d23d42ca88d1b7d28169cab58537dda22');
+INSERT INTO USUARIO(ID,Email,Senha) VALUES(99999998,'teste2@teste.com','541c0711c966b053bda0281e0adc64ab8099b4f8');
+INSERT INTO USUARIO(ID,Email,Senha) VALUES(99999999,'teste3@teste.com','5158a5c05dc512826190080b688e528240f3594c');
+
+INSERT INTO Perfil (ID,Nome,Interesses,Status,Sexo,Data_Nascimento,ID_Estado,Foto_Url) VALUES (99999997,'Fusce','Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna et arcu imperdiet ullamcorper. Duis at lacus. Quisque purus','Lorem ipsum dolor','Donec',to_date('1962-03-11','YYYY/MM/DD'),99999998,'http://3.bp.blogspot.com/_HwxB51wkeTE/THa6_MV1zvI/AAAAAAAAElw/I-QSY-TJjbw/s1600/batman-for-facebook.jpg');
+INSERT INTO Perfil (ID,Nome,Interesses,Status,Sexo,Data_Nascimento,ID_Estado,Foto_Url) VALUES (99999998,'Curabitur','Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna et arcu imperdiet ullamcorper. Duis at','Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed','blandit',to_date('1866-04-16','YYYY/MM/DD'),99999992,'http://3.bp.blogspot.com/_HwxB51wkeTE/THa6_MV1zvI/AAAAAAAAElw/I-QSY-TJjbw/s1600/batman-for-facebook.jpg');
+INSERT INTO Perfil (ID,Nome,Interesses,Status,Sexo,Data_Nascimento,ID_Estado,Foto_Url) VALUES (99999999,'Sed','Lorem ipsum dolor sit','Lorem ipsum dolor','tempor',to_date('1907-09-30','YYYY/MM/DD'),99999991,'http://3.bp.blogspot.com/_HwxB51wkeTE/THa6_MV1zvI/AAAAAAAAElw/I-QSY-TJjbw/s1600/batman-for-facebook.jpg');
+
+Update Usuario Set ID_PERFIL = 99999997 Where ID = 99999997;
+Update Usuario Set ID_PERFIL = 99999998 Where ID = 99999998;
+Update Usuario Set ID_PERFIL = 99999999 Where ID = 99999999;
+
+INSERT INTO RELACIONAMENTO(ID,ID_PERFIL_SOLICITANTE,ID_PERFIL_SOLICITADO,DATA_SOLICITACAO,PENDENTE) VALUES(99999997,99999997,99999998,SYSDATE,1);
+INSERT INTO RELACIONAMENTO(ID,ID_PERFIL_SOLICITANTE,ID_PERFIL_SOLICITADO,DATA_SOLICITACAO,DATA_RESPOSTA,PENDENTE,RESPOSTA) VALUES(99999998,99999997,99999999,SYSDATE,SYSDATE,0,1);
+INSERT INTO RELACIONAMENTO(ID,ID_PERFIL_SOLICITANTE,ID_PERFIL_SOLICITADO,DATA_SOLICITACAO,DATA_RESPOSTA,PENDENTE,RESPOSTA) VALUES(99999999,99999998,99999999,SYSDATE,SYSDATE,0,0);
+
+INSERT INTO Postagem (ID,ID_Perfil,Url_img,Descricao,Data) VALUES (99999991,99999997,'https://ichef.bbci.co.uk/news/ws/624/amz/worldservice/live/assets/images/2015/11/26/151126130740_planta_624x351_thinstock_nocredit.jpg','Lorem ipsum dolor',to_date('1907-09-30','YYYY/MM/DD'));
+INSERT INTO Postagem (ID,ID_Perfil,Url_img,Descricao,Data) VALUES (99999992,99999997,'https://ichef.bbci.co.uk/news/ws/624/amz/worldservice/live/assets/images/2015/11/26/151126130740_planta_624x351_thinstock_nocredit.jpg','Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna et arcu',to_date('1907-09-30','YYYY/MM/DD'));
+INSERT INTO Postagem (ID,ID_Perfil,Url_img,Descricao,Data) VALUES (99999993,99999998,'https://ichef.bbci.co.uk/news/ws/624/amz/worldservice/live/assets/images/2015/11/26/151126130740_planta_624x351_thinstock_nocredit.jpg','Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor.',to_date('1907-09-30','YYYY/MM/DD'));
+INSERT INTO Postagem (ID,ID_Perfil,Url_img,Descricao,Data) VALUES (99999994,99999997,'https://ichef.bbci.co.uk/news/ws/624/amz/worldservice/live/assets/images/2015/11/26/151126130740_planta_624x351_thinstock_nocredit.jpg','Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',to_date('1907-09-30','YYYY/MM/DD'));
+INSERT INTO Postagem (ID,ID_Perfil,Url_img,Descricao,Data) VALUES (99999995,99999997,'https://ichef.bbci.co.uk/news/ws/624/amz/worldservice/live/assets/images/2015/11/26/151126130740_planta_624x351_thinstock_nocredit.jpg','Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed',to_date('1907-09-30','YYYY/MM/DD'));
+INSERT INTO Postagem (ID,ID_Perfil,Url_img,Descricao,Data) VALUES (99999996,99999998,'https://ichef.bbci.co.uk/news/ws/624/amz/worldservice/live/assets/images/2015/11/26/151126130740_planta_624x351_thinstock_nocredit.jpg','Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam',to_date('1907-09-30','YYYY/MM/DD'));
+INSERT INTO Postagem (ID,ID_Perfil,Url_img,Descricao,Data) VALUES (99999997,99999999,'https://ichef.bbci.co.uk/news/ws/624/amz/worldservice/live/assets/images/2015/11/26/151126130740_planta_624x351_thinstock_nocredit.jpg','Lorem ipsum dolor sit amet, consectetuer',to_date('1907-09-30','YYYY/MM/DD'));
+INSERT INTO Postagem (ID,ID_Perfil,Url_img,Descricao,Data) VALUES (99999998,99999998,'https://ichef.bbci.co.uk/news/ws/624/amz/worldservice/live/assets/images/2015/11/26/151126130740_planta_624x351_thinstock_nocredit.jpg','Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna et arcu',to_date('1907-09-30','YYYY/MM/DD'));
+INSERT INTO Postagem (ID,ID_Perfil,Url_img,Descricao,Data) VALUES (99999999,99999999,'https://ichef.bbci.co.uk/news/ws/624/amz/worldservice/live/assets/images/2015/11/26/151126130740_planta_624x351_thinstock_nocredit.jpg','Lorem ipsum dolor sit amet, consectetuer adipiscing',to_date('1907-09-30','YYYY/MM/DD'));
+INSERT INTO Postagem (ID,ID_Perfil,Url_img,Descricao,Data) VALUES (99999981,99999998,'https://ichef.bbci.co.uk/news/ws/624/amz/worldservice/live/assets/images/2015/11/26/151126130740_planta_624x351_thinstock_nocredit.jpg','Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer',to_date('1907-09-30','YYYY/MM/DD'));
+INSERT INTO Postagem (ID,ID_Perfil,Url_img,Descricao,Data) VALUES (99999982,99999997,'https://ichef.bbci.co.uk/news/ws/624/amz/worldservice/live/assets/images/2015/11/26/151126130740_planta_624x351_thinstock_nocredit.jpg','Lorem ipsum dolor sit',to_date('1907-09-30','YYYY/MM/DD'));
+INSERT INTO Postagem (ID,ID_Perfil,Url_img,Descricao,Data) VALUES (99999983,99999998,'https://ichef.bbci.co.uk/news/ws/624/amz/worldservice/live/assets/images/2015/11/26/151126130740_planta_624x351_thinstock_nocredit.jpg','Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed',to_date('1907-09-30','YYYY/MM/DD'));
+INSERT INTO Postagem (ID,ID_Perfil,Url_img,Descricao,Data) VALUES (99999984,99999998,'https://ichef.bbci.co.uk/news/ws/624/amz/worldservice/live/assets/images/2015/11/26/151126130740_planta_624x351_thinstock_nocredit.jpg','Lorem',to_date('1907-09-30','YYYY/MM/DD'));
+INSERT INTO Postagem (ID,ID_Perfil,Url_img,Descricao,Data) VALUES (99999985,99999999,'https://ichef.bbci.co.uk/news/ws/624/amz/worldservice/live/assets/images/2015/11/26/151126130740_planta_624x351_thinstock_nocredit.jpg','Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer',to_date('1907-09-30','YYYY/MM/DD'));
+INSERT INTO Postagem (ID,ID_Perfil,Url_img,Descricao,Data) VALUES (99999986,99999997,'https://ichef.bbci.co.uk/news/ws/624/amz/worldservice/live/assets/images/2015/11/26/151126130740_planta_624x351_thinstock_nocredit.jpg','Lorem ipsum dolor sit',to_date('1907-09-30','YYYY/MM/DD'));
+INSERT INTO Postagem (ID,ID_Perfil,Url_img,Descricao,Data) VALUES (99999987,99999999,'https://ichef.bbci.co.uk/news/ws/624/amz/worldservice/live/assets/images/2015/11/26/151126130740_planta_624x351_thinstock_nocredit.jpg','Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna',to_date('1907-09-30','YYYY/MM/DD'));
+INSERT INTO Postagem (ID,ID_Perfil,Url_img,Descricao,Data) VALUES (99999988,99999997,'https://ichef.bbci.co.uk/news/ws/624/amz/worldservice/live/assets/images/2015/11/26/151126130740_planta_624x351_thinstock_nocredit.jpg','Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer',to_date('1907-09-30','YYYY/MM/DD'));
+INSERT INTO Postagem (ID,ID_Perfil,Url_img,Descricao,Data) VALUES (99999989,99999998,'https://ichef.bbci.co.uk/news/ws/624/amz/worldservice/live/assets/images/2015/11/26/151126130740_planta_624x351_thinstock_nocredit.jpg','Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna',to_date('1907-09-30','YYYY/MM/DD'));
+INSERT INTO Postagem (ID,ID_Perfil,Url_img,Descricao,Data) VALUES (99999971,99999999,'https://ichef.bbci.co.uk/news/ws/624/amz/worldservice/live/assets/images/2015/11/26/151126130740_planta_624x351_thinstock_nocredit.jpg','Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut',to_date('1907-09-30','YYYY/MM/DD'));
+INSERT INTO Postagem (ID,ID_Perfil,Url_img,Descricao,Data) VALUES (99999972,99999997,'https://ichef.bbci.co.uk/news/ws/624/amz/worldservice/live/assets/images/2015/11/26/151126130740_planta_624x351_thinstock_nocredit.jpg','Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam',to_date('1907-09-30','YYYY/MM/DD'));
+INSERT INTO Postagem (ID,ID_Perfil,Url_img,Descricao,Data) VALUES (99999973,99999998,'https://ichef.bbci.co.uk/news/ws/624/amz/worldservice/live/assets/images/2015/11/26/151126130740_planta_624x351_thinstock_nocredit.jpg','Lorem ipsum dolor',to_date('1907-09-30','YYYY/MM/DD'));
+INSERT INTO Postagem (ID,ID_Perfil,Url_img,Descricao,Data) VALUES (99999974,99999998,'https://ichef.bbci.co.uk/news/ws/624/amz/worldservice/live/assets/images/2015/11/26/151126130740_planta_624x351_thinstock_nocredit.jpg','Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur',to_date('1907-09-30','YYYY/MM/DD'));
+INSERT INTO Postagem (ID,ID_Perfil,Url_img,Descricao,Data) VALUES (99999975,99999997,'https://ichef.bbci.co.uk/news/ws/624/amz/worldservice/live/assets/images/2015/11/26/151126130740_planta_624x351_thinstock_nocredit.jpg','Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna',to_date('1907-09-30','YYYY/MM/DD'));
+INSERT INTO Postagem (ID,ID_Perfil,Url_img,Descricao,Data) VALUES (99999976,99999997,'https://ichef.bbci.co.uk/news/ws/624/amz/worldservice/live/assets/images/2015/11/26/151126130740_planta_624x351_thinstock_nocredit.jpg','Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer',to_date('1907-09-30','YYYY/MM/DD'));
+INSERT INTO Postagem (ID,ID_Perfil,Url_img,Descricao,Data) VALUES (99999977,99999998,'https://ichef.bbci.co.uk/news/ws/624/amz/worldservice/live/assets/images/2015/11/26/151126130740_planta_624x351_thinstock_nocredit.jpg','Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna et arcu',to_date('1907-09-30','YYYY/MM/DD'));
+INSERT INTO Postagem (ID,ID_Perfil,Url_img,Descricao,Data) VALUES (99999978,99999997,'https://ichef.bbci.co.uk/news/ws/624/amz/worldservice/live/assets/images/2015/11/26/151126130740_planta_624x351_thinstock_nocredit.jpg','Lorem ipsum dolor sit amet,',to_date('1907-09-30','YYYY/MM/DD'));
+INSERT INTO Postagem (ID,ID_Perfil,Url_img,Descricao,Data) VALUES (99999979,99999999,'https://ichef.bbci.co.uk/news/ws/624/amz/worldservice/live/assets/images/2015/11/26/151126130740_planta_624x351_thinstock_nocredit.jpg','Lorem ipsum dolor sit amet,',to_date('1907-09-30','YYYY/MM/DD'));
+INSERT INTO Postagem (ID,ID_Perfil,Url_img,Descricao,Data) VALUES (99999961,99999997,'https://ichef.bbci.co.uk/news/ws/624/amz/worldservice/live/assets/images/2015/11/26/151126130740_planta_624x351_thinstock_nocredit.jpg','Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer',to_date('1907-09-30','YYYY/MM/DD'));
+INSERT INTO Postagem (ID,ID_Perfil,Url_img,Descricao,Data) VALUES (99999962,99999999,'https://ichef.bbci.co.uk/news/ws/624/amz/worldservice/live/assets/images/2015/11/26/151126130740_planta_624x351_thinstock_nocredit.jpg','Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing lacus. Ut nec urna et',to_date('1907-09-30','YYYY/MM/DD'));
+INSERT INTO Postagem (ID,ID_Perfil,Url_img,Descricao,Data) VALUES (99999963,99999998,'https://ichef.bbci.co.uk/news/ws/624/amz/worldservice/live/assets/images/2015/11/26/151126130740_planta_624x351_thinstock_nocredit.jpg','Lorem ipsum',to_date('1907-09-30','YYYY/MM/DD'));
+INSERT INTO Postagem (ID,ID_Perfil,Url_img,Descricao,Data) VALUES (99999964,99999998,'https://ichef.bbci.co.uk/news/ws/624/amz/worldservice/live/assets/images/2015/11/26/151126130740_planta_624x351_thinstock_nocredit.jpg','Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer aliquam adipiscing',to_date('1907-09-30','YYYY/MM/DD'));
+INSERT INTO Postagem (ID,ID_Perfil,Url_img,Descricao,Data) VALUES (99999965,99999997,'https://ichef.bbci.co.uk/news/ws/624/amz/worldservice/live/assets/images/2015/11/26/151126130740_planta_624x351_thinstock_nocredit.jpg','Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur sed tortor. Integer',to_date('1907-09-30','YYYY/MM/DD'));
+
+INSERT INTO Curtida (ID,DATA,ID_POSTAGEM,ID_PERFIL) VALUES (99999991,SYSDATE,99999965,99999999);
+INSERT INTO Curtida (ID,DATA,ID_POSTAGEM,ID_PERFIL) VALUES (99999992,SYSDATE,99999991,99999999);
+INSERT INTO Curtida (ID,DATA,ID_POSTAGEM,ID_PERFIL) VALUES (99999993,SYSDATE,99999992,99999999);
+INSERT INTO Curtida (ID,DATA,ID_POSTAGEM,ID_PERFIL) VALUES (99999994,SYSDATE,99999994,99999999);
+INSERT INTO Curtida (ID,DATA,ID_POSTAGEM,ID_PERFIL) VALUES (99999995,SYSDATE,99999995,99999999);
+INSERT INTO Curtida (ID,DATA,ID_POSTAGEM,ID_PERFIL) VALUES (99999996,SYSDATE,99999982,99999999);
+
+INSERT INTO Curtida (ID,DATA,ID_POSTAGEM,ID_PERFIL) VALUES (99999997,SYSDATE,99999997,99999997);
+INSERT INTO Curtida (ID,DATA,ID_POSTAGEM,ID_PERFIL) VALUES (99999998,SYSDATE,99999999,99999997);
+INSERT INTO Curtida (ID,DATA,ID_POSTAGEM,ID_PERFIL) VALUES (99999999,SYSDATE,99999985,99999997);
+INSERT INTO Curtida (ID,DATA,ID_POSTAGEM,ID_PERFIL) VALUES (99999981,SYSDATE,99999987,99999997);
+INSERT INTO Curtida (ID,DATA,ID_POSTAGEM,ID_PERFIL) VALUES (99999982,SYSDATE,99999971,99999997);
+
+INSERT INTO Comentario (ID,DATA,ID_POSTAGEM,ID_PERFIL,TEXTO) VALUES (99999991,SYSDATE,99999965,99999999,'haha');
+INSERT INTO Comentario (ID,DATA,ID_POSTAGEM,ID_PERFIL,TEXTO) VALUES (99999992,SYSDATE,99999991,99999999,'kkk');
+INSERT INTO Comentario (ID,DATA,ID_POSTAGEM,ID_PERFIL,TEXTO) VALUES (99999993,SYSDATE,99999992,99999999,'perfeito');
+INSERT INTO Comentario (ID,DATA,ID_POSTAGEM,ID_PERFIL,TEXTO) VALUES (99999994,SYSDATE,99999994,99999999,'curti');
+INSERT INTO Comentario (ID,DATA,ID_POSTAGEM,ID_PERFIL,TEXTO) VALUES (99999995,SYSDATE,99999995,99999999,'amei');
+INSERT INTO Comentario (ID,DATA,ID_POSTAGEM,ID_PERFIL,TEXTO) VALUES (99999996,SYSDATE,99999982,99999999,'ehuehuehuehue');
+
+INSERT INTO Comentario (ID,DATA,ID_POSTAGEM,ID_PERFIL,TEXTO) VALUES (99999997,SYSDATE,99999997,99999997,'comentário construtivo');
+INSERT INTO Comentario (ID,DATA,ID_POSTAGEM,ID_PERFIL,TEXTO) VALUES (99999998,SYSDATE,99999999,99999997,'o mundo é dos nets');
+INSERT INTO Comentario (ID,DATA,ID_POSTAGEM,ID_PERFIL,TEXTO) VALUES (99999999,SYSDATE,99999985,99999997,'fsafasda');
+INSERT INTO Comentario (ID,DATA,ID_POSTAGEM,ID_PERFIL,TEXTO) VALUES (99999981,SYSDATE,99999987,99999997,'a vida');
+INSERT INTO Comentario (ID,DATA,ID_POSTAGEM,ID_PERFIL,TEXTO) VALUES (99999982,SYSDATE,99999971,99999997,'ehuehueheu');
+
+Select * From Estado;
+Select * From Usuario;
+Select * From Perfil;
+Select * From Relacionamento;
+Select * From Postagem;
+Select * From Curtida;
+Select * From Comentario;
---
+++ Java - Módulo 08/Trabalho Final/Script SQL/SeedShareScriptCreate.sql
@@ -0,0 +1,273 @@
+/*create tablespace SeedShare
+   datafile 'C:\oraclexe\app\oracle\oradata\XE\SEEDSHARE.DBF'
+   size 100m
+   autoextend on
+   next 100m
+   maxsize 2048m;
+
+Create user Seed identified by Seed default tablespace SeedShare;
+grant connect, resource, create view to Seed;*/
+   
+CREATE TABLE Usuario (
+	ID INTEGER NOT NULL,
+	Email Varchar2(50) NOT NULL,
+	Senha Varchar2(100) NOT NULL,
+	ID_Perfil INTEGER,
+	constraint USUARIO_PK PRIMARY KEY (ID),
+    constraint UK_Usuario_Email unique (Email),
+    constraint UK_Usuario_Perfil unique (ID_Perfil)
+);
+
+CREATE SEQUENCE SEQ_USUARIO
+    MINVALUE 1 
+    MAXVALUE 999999999999999999999999999 
+    INCREMENT BY 1 
+    START WITH 1 
+    CACHE 10 
+    NOORDER  
+    NOCYCLE;
+
+
+CREATE TABLE Perfil (
+	ID INTEGER NOT NULL,
+	Nome Varchar2(50) NOT NULL,
+    Foto_Url Varchar(2000) NOT NULL,
+	Interesses Varchar2(300),
+	Status Varchar2(100),
+	Sexo Varchar2(20) NOT NULL,
+	Data_Nascimento DATE NOT NULL,
+	ID_Estado INTEGER NOT NULL,
+	constraint PERFIL_PK PRIMARY KEY (ID)
+);
+
+CREATE SEQUENCE SEQ_PERFIL
+    MINVALUE 1 
+    MAXVALUE 999999999999999999999999999 
+    INCREMENT BY 1 
+    START WITH 1 
+    CACHE 10 
+    NOORDER  
+    NOCYCLE;
+
+
+CREATE TABLE Estado (
+	ID INTEGER NOT NULL,
+	Nome varchar2(50) NOT NULL,
+	Pais varchar2(50) NOT NULL,
+	constraint ESTADO_PK PRIMARY KEY (ID),
+    constraint UK_Estado_Pais unique (Nome,Pais)
+);
+CREATE SEQUENCE SEQ_ESTADO
+    MINVALUE 1 
+    MAXVALUE 999999999999999999999999999 
+    INCREMENT BY 1 
+    START WITH 1 
+    CACHE 10 
+    NOORDER  
+    NOCYCLE;
+
+
+CREATE TABLE Alteracao_Senha (
+	ID INTEGER NOT NULL,
+	Email Varchar2(50) NOT NULL,
+	Codigo Varchar2(25) NOT NULL,
+	Prazo DATE NOT NULL,
+	constraint ALTERACAO_SENHA_PK PRIMARY KEY (ID),
+    constraint UK_Alteracao_Email unique (Email)
+);
+CREATE SEQUENCE SEQ_ALTERACAO_SENHA
+    MINVALUE 1 
+    MAXVALUE 999999999999999999999999999 
+    INCREMENT BY 1 
+    START WITH 1 
+    CACHE 10 
+    NOORDER  
+    NOCYCLE;
+
+
+CREATE TABLE Relacionamento (
+	ID INTEGER NOT NULL,
+	ID_Perfil_Solicitante INTEGER NOT NULL,
+	ID_Perfil_Solicitado INTEGER NOT NULL,
+	Data_Solicitacao DATE NOT NULL,
+	Data_Resposta DATE,
+	Pendente NUMBER(1) NOT NULL,
+	Resposta NUMBER(1),
+	constraint RELACIONAMENTO_PK PRIMARY KEY (ID),
+    constraint UK_Solicitante_Solicitado unique (ID_Perfil_Solicitante,ID_Perfil_Solicitado)
+);
+
+CREATE SEQUENCE SEQ_RELACIONAMENTO
+    MINVALUE 1 
+    MAXVALUE 999999999999999999999999999 
+    INCREMENT BY 1 
+    START WITH 1 
+    CACHE 25 
+    NOORDER  
+    NOCYCLE;
+
+CREATE TABLE Postagem (
+	ID INTEGER NOT NULL,
+    ID_Perfil INTEGER NOT NULL,
+	Url_Img Varchar2(2000),
+	Descricao Varchar2(500),
+	Data Date NOT NULL,
+	constraint POSTAGEM_PK PRIMARY KEY (ID)
+);
+CREATE SEQUENCE SEQ_POSTAGEM
+    MINVALUE 1 
+    MAXVALUE 999999999999999999999999999 
+    INCREMENT BY 1 
+    START WITH 1 
+    CACHE 50 
+    NOORDER  
+    NOCYCLE;
+
+CREATE TABLE Curtida (
+	ID INTEGER NOT NULL,
+	Data Date NOT NULL,
+	ID_Postagem INTEGER NOT NULL,
+	ID_Perfil INTEGER NOT NULL,
+    constraint UK_Curtida_Postagem_Perfil Unique(ID_Postagem,ID_Perfil),
+	constraint CURTIDA_PK PRIMARY KEY (ID)
+);
+
+CREATE SEQUENCE SEQ_CURTIDA
+    MINVALUE 1 
+    MAXVALUE 999999999999999999999999999 
+    INCREMENT BY 1 
+    START WITH 1 
+    CACHE 50 
+    NOORDER  
+    NOCYCLE;
+
+CREATE TABLE Comentario(
+	ID INTEGER NOT NULL,
+	Texto Varchar2(300) NOT NULL,
+	ID_Postagem INTEGER NOT NULL,
+	ID_Perfil INTEGER NOT NULL,
+    Data Date NOT NULL,
+	constraint COMENTARIO_PK PRIMARY KEY ("ID")
+);
+
+CREATE SEQUENCE SEQ_COMENTARIO
+    MINVALUE 1 
+    MAXVALUE 999999999999999999999999999 
+    INCREMENT BY 1 
+    START WITH 1 
+    CACHE 50 
+    NOORDER  
+    NOCYCLE;
+
+
+ALTER TABLE Usuario ADD CONSTRAINT FK_Usuario FOREIGN KEY (ID_Perfil) REFERENCES Perfil(ID);
+
+ALTER TABLE Perfil ADD CONSTRAINT FK_Perfil FOREIGN KEY (ID_Estado) REFERENCES Estado(ID);
+CREATE INDEX IX_Pefil_Estado ON Perfil(ID_Estado);
+
+ALTER TABLE Alteracao_Senha ADD CONSTRAINT FK_Alteracao_Senha FOREIGN KEY (Email) REFERENCES Usuario(Email);
+
+ALTER TABLE Relacionamento ADD CONSTRAINT FK_Relacionamento_Solicitante FOREIGN KEY (ID_Perfil_Solicitante) REFERENCES Perfil(ID);
+CREATE INDEX IX_Relacionamento_Solicitante ON Relacionamento(ID_Perfil_Solicitante);
+
+ALTER TABLE Relacionamento ADD CONSTRAINT FK_Relacionamento_Solicitado FOREIGN KEY (ID_Perfil_Solicitado) REFERENCES Perfil(ID);
+CREATE INDEX IX_Relacionamento_Solicitado ON Relacionamento(ID_Perfil_Solicitado);
+
+ALTER TABLE Postagem ADD CONSTRAINT FK_Postagem_Perfil FOREIGN KEY (ID_Perfil) REFERENCES Perfil(ID);
+CREATE INDEX IX_Postagem_ID_Perfil ON Postagem(ID_Perfil);
+
+ALTER TABLE Curtida ADD CONSTRAINT FK_Curtida_Postagem FOREIGN KEY (ID_Postagem) REFERENCES Postagem(ID);
+CREATE INDEX IX_Curtida_ID_Postagem ON Curtida(ID_Postagem);
+
+ALTER TABLE Curtida ADD CONSTRAINT FK_Curtida_Perfil FOREIGN KEY (ID_Perfil) REFERENCES Perfil(ID);
+CREATE INDEX IX_Curtida_ID_Perfil ON Curtida(ID_Perfil);
+
+ALTER TABLE Comentario ADD CONSTRAINT FK_Comentario_Postagem FOREIGN KEY (ID_Postagem) REFERENCES Postagem(ID);
+CREATE INDEX IX_Comentario_ID_Postagem ON Comentario(ID_Postagem);
+
+ALTER TABLE Comentario ADD CONSTRAINT FK_Comentario_Perfil FOREIGN KEY (ID_Perfil) REFERENCES Perfil(ID);
+CREATE INDEX IX_Comentario_ID_Perfil ON Comentario(ID_Perfil);
+
+
+/*
+CREATE trigger BI_COMENTARIO
+  before insert on COMENTARIO
+  for each row
+begin
+  select COMENTARIO_SEQ.nextval into :NEW.ID from dual;
+end;
+*/
+/*
+CREATE trigger "BI_CURTIDA"
+  before insert on "CURTIDA"
+  for each row
+begin
+  select "CURTIDA_SEQ".nextval into :NEW."ID" from dual;
+end;
+*/
+/*
+CREATE trigger "BI_POSTAGEM"
+  before insert on "POSTAGEM"
+  for each row
+begin
+  select "POSTAGEM_SEQ".nextval into :NEW."ID" from dual;
+end;
+*/
+/*
+CREATE trigger "BI_RELACIONAMENTO"
+  before insert on "RELACIONAMENTO"
+  for each row
+begin
+  select "RELACIONAMENTO_SEQ".nextval into :NEW."ID" from dual;
+end;
+*/
+/*
+CREATE trigger "BI_ALTERACAO_SENHA"
+  before insert on "ALTERACAO_SENHA"
+  for each row
+begin
+  select "ALTERACAO_SENHA_SEQ".nextval into :NEW."ID" from dual;
+end;
+*/
+/*
+CREATE trigger "BI_ESTADO"
+  before insert on "ESTADO"
+  for each row
+begin
+  select "ESTADO_SEQ".nextval into :NEW."ID" from dual;
+end;
+*/
+/*
+CREATE trigger BI_PERFIL
+  before insert on PERFIL
+  for each row
+begin
+  select "PERFIL_SEQ".nextval into :NEW."ID" from dual;
+end;
+*/
+/*
+CREATE trigger BI_USUARIO
+  before insert on USUARIO
+  for each row
+begin
+  Select USUARIO_SEQ.nextval into :NEW.ID from dual;
+end;*/
+/*
+DROP TABLE Comentario;
+DROP TABLE Alteracao_Senha;
+DROP TABLE Curtida;
+DROP TABLE Postagem;
+DROP TABLE Relacionamento;
+DROP TABLE Usuario;
+DROP TABLE Perfil;
+DROP TABLE Estado;
+
+DROP SEQUENCE SEQ_USUARIO;
+DROP SEQUENCE SEQ_CURTIDA;
+DROP SEQUENCE SEQ_ESTADO;
+DROP SEQUENCE SEQ_PERFIL;
+DROP SEQUENCE SEQ_POSTAGEM;
+DROP SEQUENCE SEQ_RELACIONAMENTO;
+DROP SEQUENCE SEQ_ALTERACAO_SENHA;
+DROP SEQUENCE SEQ_COMENTARIO;
+*/
