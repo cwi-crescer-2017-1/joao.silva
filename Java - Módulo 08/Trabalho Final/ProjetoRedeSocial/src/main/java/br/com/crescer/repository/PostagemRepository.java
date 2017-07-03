4f142b91ddc69d5809c4ba2471032bed5313c1d5
JPedroSSilva <joao.silva@cwi.com.br>
2017-07-03 19:38:27 -0300

    fix(Query): mudança de query para não nativa

--- Java - Módulo 08/Trabalho Final/ProjetoRedeSocial/src/main/java/br/com/crescer/repository/PostagemRepository.java
+++ Java - Módulo 08/Trabalho Final/ProjetoRedeSocial/src/main/java/br/com/crescer/repository/PostagemRepository.java
@@ -7,6 +7,8 @@ package br.com.crescer.repository;
 
 import br.com.crescer.entity.Postagem;
 import br.com.crescer.entity.Usuario;
+import br.com.crescer.entity.Relacionamento;
+import br.com.crescer.entity.Perfil;
 import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -36,8 +38,16 @@ public interface PostagemRepository extends JpaRepository<Postagem, Long>{
             + "IN(Select re.ID_PERFIL_SOLICITADO From Relacionamento re Where re.ID_PERFIL_SOLICITADO = ?1 "
             + "OR re.ID_PERFIL_SOLICITANTE = ?1 AND re.Resposta = 1) "
             + "OR pe.ID IN (Select re.ID_PERFIL_SOLICITANTE From Relacionamento re Where re.ID_PERFIL_SOLICITADO = ?1 "
-            + "OR re.ID_PERFIL_SOLICITANTE = ?1 AND re.Resposta = 1)) ORDER BY pos.DATA_POSTAGEM DESC /*#pageable*/",
+            + "OR re.ID_PERFIL_SOLICITANTE = ?1 AND re.Resposta = 1)) /*#pageable*/",
         countQuery="Select Count(pos.ID) From Postagem pos Where pos.ID_Perfil IN( Select pe.ID From Perfil pe Where pe.ID IN(Select re.ID_PERFIL_SOLICITADO From Relacionamento re Where re.ID_PERFIL_SOLICITADO = ?1 OR re.ID_PERFIL_SOLICITANTE = ?1 AND re.Resposta = 1) OR pe.ID IN (Select re.ID_PERFIL_SOLICITANTE From Relacionamento re Where re.ID_PERFIL_SOLICITADO = ?1 OR re.ID_PERFIL_SOLICITANTE = ?1 AND re.Resposta = 1))",
         nativeQuery=true)
-    Page<Postagem> Postagem_findByUsuario(Long idUsuario, Pageable pageable);
+    Page<Postagem> Postagem_findByUsuario_2(Long idUsuario, Pageable pageable);
+    
+        @Query(value="select pos from Postagem pos where pos.perfil.id "
+            + "In (select pe.id from Perfil pe where pe.id "
+            + "In (select re.perfilSolicitado.id from Relacionamento re where re.perfilSolicitado.usuario.id = ?1 "
+            + "Or re.perfilSolicitante.usuario.id = ?1 And re.resposta = true) "
+            + "Or pe.id in (select re.perfilSolicitante.id from Relacionamento re where re.perfilSolicitado.usuario.id = ?1 "
+            + "Or re.perfilSolicitante.usuario.id = ?1 And re.resposta = true))")
+        Page<Postagem> Postagem_findByUsuario(Long idUsuario, Pageable pageable);
 }
