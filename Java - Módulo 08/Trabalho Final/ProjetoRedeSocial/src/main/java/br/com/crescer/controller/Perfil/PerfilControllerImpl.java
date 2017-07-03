f4fbeb0bf2520e4f0286393032cd1eb76b26e82e
JPedroSSilva <joao.silva@cwi.com.br>
2017-07-03 00:58:53 -0300

    feat(PerfilController): implementação da controller para o perfil

---
+++ Java - Módulo 08/Trabalho Final/ProjetoRedeSocial/src/main/java/br/com/crescer/controller/Perfil/PerfilControllerImpl.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.crescer.controller.Perfil;
+
+import br.com.crescer.entity.Perfil;
+import br.com.crescer.service.Perfil.PerfilServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author jpedr
+ */
+@RestController
+@RequestMapping("/perfil")
+public class PerfilControllerImpl implements PerfilController{
+    @Autowired
+    PerfilServiceImpl perfilService;
+    
+    @Override
+    @PostMapping("/save")
+    public Perfil save(@RequestBody Perfil perfil) {
+        return perfilService.save(perfil);
+    }
+
+    @Override
+    @PutMapping("/delete")
+    public void delete(@RequestBody Perfil perfil) {
+        perfilService.delete(perfil);
+    }
+
+    @Override
+    @GetMapping(value = "/findOne/{id}")
+    public Perfil findOne(@PathVariable Long id) {
+        return perfilService.findOne(id);
+    }
+    
+}
