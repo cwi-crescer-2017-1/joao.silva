bbc670668a1dc4c8647d99e18ab79348af541424
JPedroSSilva <joao.silva@cwi.com.br>
2017-07-03 19:40:09 -0300

    feat(Postagem): implementação de método para criar postagem

--- Java - Módulo 08/Trabalho Final/RedeSocial_Front/controllers/controllerHome.js
+++ Java - Módulo 08/Trabalho Final/RedeSocial_Front/controllers/controllerHome.js
@@ -1,10 +1,15 @@
-modulo.controller('controllerHome',['$scope','toastr','$location','authService','servicePostagem', function(model,toastr,$location,authService,servicePostagem){
+modulo.controller('controllerHome',['$scope','toastr','$location','authService','servicePostagem','servicePerfil', function(model,toastr,$location,authService,servicePostagem,servicePerfil){
     model.perfilCorrente = authService.getUsuario().perfil;
     model.nomeCorrente = model.perfilCorrente.nome;
     model.verComentarios = false;
     model.verComentarioClick = 0;
-    model.botaoComentario = "Mostrar comentários";
-    let pagina = 1;
+    model.mostrarPesquisa = false;
+    let mensagemComentarioMostrar = "Mostrar comentários   .";
+    let mensagemComentarioEsconder = "Esconder comentários";
+    model.botaoComentario = mensagemComentarioMostrar;
+    model.pesquisaUsuario = "";
+
+    let pagina = 0;
     let tamanhoPagina = 5;
     let nomeConfig = function(){
         let nome = model.perfilCorrente.nome;
@@ -15,6 +20,43 @@ modulo.controller('controllerHome',['$scope','toastr','$location','authService',
         }
     }
     nomeConfig();
+    model.pesquisar = function(){
+        if(typeof model.pesquisaUsuario !== "undefined" && typeof model.pesquisaUsuario !== "null" && model.pesquisaUsuario.replace(/\s{2,}/g, ' ') !== ""){
+        servicePerfil.getFilterByName(model.pesquisaUsuario).then(
+                    function (response) {
+                        model.perfisPesquisados = response.data;
+                        if(model.perfisPesquisados.length===0){
+                            toastr.info('Nenhum perfil encontrado!');
+                        }else{
+                            model.mostrarPesquisa = true;
+                            console.log(model.perfisPesquisados);
+                        }
+                    },
+                    function (response) {
+                        toastr.error('Erro ao pesquisar por usuários!');
+                    });
+        }else{
+            toastr.warning('Necessário inserir nome para pesquisa!');
+        }
+    }
+    model.retirarPesquisa = function(){
+        model.mostrarPesquisa = false;
+    }
+    model.registrarPostagem = function (){
+        if(typeof model.postagemNova.texto.length === "undefined" || typeof model.postagemNova.texto.length === "null" ||model.postagemNova.texto.length<0){
+            toastr.error('Texto da postagem obrigatório!');
+        }else{
+            servicePostagem.registrar(model.postagemNova.fotoUrl,model.postagemNova.texto,model.perfilCorrente).then(
+                function (response) {
+                    $location.path('/home');
+                    toastr.success('Postagem realizada!');
+                    location.reload();
+                },
+                function (response) {
+                toastr.error('Erro inesperado durante a postagem!');
+            });
+        }
+    }
     model.isEmpty = function (variavel){
         if(typeof variavel === "undefined" || variavel === null){
             return true;
@@ -27,11 +69,11 @@ modulo.controller('controllerHome',['$scope','toastr','$location','authService',
         if(model.verComentarioClick === 0){
             model.verComentarios = true;
             model.verComentarioClick = 1;
-            model.botaoComentario = "Esconder comentários";
+            model.botaoComentario = mensagemComentarioEsconder;
         }else{
             model.verComentarios = false;
             model.verComentarioClick = 0;
-            model.botaoComentario = "Mostrar comentários";
+            model.botaoComentario = mensagemComentarioMostrar;
         }
  
     }
@@ -40,10 +82,21 @@ modulo.controller('controllerHome',['$scope','toastr','$location','authService',
         .then(
             function (response) {
             model.postagens = response.data.content;
+                    console.log(model.postagens);
             },
             function (response) {
             toastr.error('Erro ao carregar as postagens');
         });
+    }
+    model.calculoData = function(data){
+        var date1 = (new Date(data-388000));
+        var date2 = new Date();
+        var timeDiff = Math.abs(date2.getTime() - date1.getTime());
+        var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
+        if((timeDiff/(1000 * 3600 * 24)<60)){
+            return 'Há menos de uma hora atrás';
+        }
+        return 'Há '+diffDays+' horas atrás.';
     }
     model.getPostagens();
 }]);
