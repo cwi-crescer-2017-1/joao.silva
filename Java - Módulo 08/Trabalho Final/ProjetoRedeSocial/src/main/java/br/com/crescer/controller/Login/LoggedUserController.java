57b127ba25d14a1dc20c83b256dcea63183fbc12
JPedroSSilva <joao.silva@cwi.com.br>
2017-07-03 00:58:09 -0300

    feat(LoggedUserController): implementação de novos métodos para informações sobre o usuário logado

--- Java - Módulo 08/Trabalho Final/ProjetoRedeSocial/src/main/java/br/com/crescer/controller/Login/LoggedUserController.java
+++ Java - Módulo 08/Trabalho Final/ProjetoRedeSocial/src/main/java/br/com/crescer/controller/Login/LoggedUserController.java
@@ -1,7 +1,13 @@
 package br.com.crescer.controller.login;
 
+import br.com.crescer.entity.Perfil;
+import br.com.crescer.entity.Usuario;
+import br.com.crescer.service.Usuario.UsuarioServiceImpl;
 import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,16 +17,31 @@ import org.springframework.web.bind.annotation.RestController;
  * @author carloshenrique
  */
 @RestController
-@RequestMapping(LoggedUserController.PATH)
+@RequestMapping("/usuario")
 public class LoggedUserController {
 
-    public static final String PATH = "/logged-user";
-
-    @GetMapping
-    public User getUserDetails(Authentication authentication) {
-        return Optional.ofNullable(authentication)
+    @Autowired
+    UsuarioServiceImpl usuarioService;
+    
+    @GetMapping("/get")
+    public Usuario getUserDetails() {
+        return Optional.ofNullable(SecurityContextHolder.getContext())
+                .map(SecurityContext::getAuthentication)
                 .map(Authentication::getPrincipal)
                 .map(User.class::cast)
+                .map(User::getUsername)
+                .map(usuarioService::findOneByEmail)
                 .orElse(null);
     }
-}
+    @GetMapping(value = "/getPerfil")
+    public Perfil getUserDetailsPerfil() {
+       return Optional.ofNullable(SecurityContextHolder.getContext())
+                .map(SecurityContext::getAuthentication)
+                .map(Authentication::getPrincipal)
+                .map(User.class::cast)
+                .map(User::getUsername)
+                .map(usuarioService::findOneByEmail)
+                .map(Usuario::getPerfil)
+                .orElse(null);
+    }
+}       
