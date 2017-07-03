eb068ad69e13307ba8a15545f097d2cc7e7fbcda
JPedroSSilva <joao.silva@cwi.com.br>
2017-07-03 01:09:11 -0300

    feat(main.js): adição de script principal para gerenciar rotas

---
+++ Java - Módulo 08/Trabalho Final/RedeSocial_Front/js/main.js
@@ -0,0 +1,43 @@
+var modulo = angular.module('ProdutoraCrescer', ['ngRoute','auth','toastr']);  
+
+modulo.config(function ($routeProvider) {  
+  $routeProvider 
+    .when('/login', { 
+      controller: 'controllerLogin',
+      templateUrl: '../html/login.html' 
+    })  
+    .when('/home', { 
+      controller: 'controllerHome',
+      templateUrl: '../html/home.html',
+      resolve:{
+        autenticado: function (authService) {
+          return authService.isAutenticadoPromise();
+        }
+      } 
+    })  
+    .otherwise({redirectTo: '/home'}); 
+}); 
+
+modulo.directive("required", function() {
+  return {
+    template: `<p style="color:red;display:inline-block;">*</p>`
+  };
+}); 
+
+// Configurações utilizadas pelo módulo de autenticação (authService)
+modulo.constant('authConfig', {
+
+    // Obrigatória - URL da API que retorna o usuário
+    //urlUsuario: 'http://10.99.3.24/AutDemo.WebApi/api/acessos/usuario',
+    urlUsuario: 'http://localhost:8080/usuario/get',
+
+    // Obrigatória - URL da aplicação que possui o formulário de login
+    urlLogin: '/login',
+
+    // Opcional - URL da aplicação para onde será redirecionado (se for informado) após o LOGIN com sucesso
+    urlPrivado: '/home',
+
+    // Opcional - URL da aplicação para onde será redirecionado (se for informado) após o LOGOUT
+    urlLogout: '/login'
+});
+
