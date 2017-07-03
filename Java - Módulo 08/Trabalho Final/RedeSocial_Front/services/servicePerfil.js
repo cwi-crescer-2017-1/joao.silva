ad33ad8effd0c3abedf3e12fcc83d5fc2ed9f812
JPedroSSilva <joao.silva@cwi.com.br>
2017-07-03 01:09:50 -0300

    feat(servicePerfil): implementação de service para adquirir dados do perfil

---
+++ Java - Módulo 08/Trabalho Final/RedeSocial_Front/services/servicePerfil.js
@@ -0,0 +1,16 @@
+modulo.factory('servicePerfil',function($http){
+    let urlBase = 'http://localhost:8080/perfil/';
+    function registrar(nome,genero,fotoUrl,dataNascimento,estado){
+        let perfil = {};
+        perfil.nome = nome;
+        perfil.genero = genero;
+        perfil.fotoUrl = fotoUrl;
+        perfil.sexo = genero;
+        perfil.dataNascimento = dataNascimento;
+        perfil.estado = estado;
+        return $http.post(urlBase+'save',perfil);
+    }
+    return { 
+        registrar:registrar,
+    }; 
+});
