1cc620eedb3424fb50814f556be9eebd62a9052b
JPedroSSilva <joao.silva@cwi.com.br>
2017-07-04 11:21:42 -0300

    feat(remove): implementação de query para remover uma curtida e para retornar uma curtida existente pelo id do perfil em conjunto com o id da postagem

--- Java - Módulo 08/Trabalho Final/ProjetoRedeSocial/src/main/java/br/com/crescer/repository/CurtidaRepository.java
+++ Java - Módulo 08/Trabalho Final/ProjetoRedeSocial/src/main/java/br/com/crescer/repository/CurtidaRepository.java
@@ -6,10 +6,20 @@
 package br.com.crescer.repository;
 
 import br.com.crescer.entity.Curtida;
+import javax.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 /**
  * @author joao.silva
  */
 public interface CurtidaRepository extends PagingAndSortingRepository<Curtida, Long>{
+    @Modifying
+    @Transactional
+    @Query("delete from Curtida where postagem.id = ?1 And perfil.id = ?2")
+    public void remove(Long idPostagem, Long idPerfil);
+    
+    @Query("select cur from Curtida cur where cur.postagem.id = ?1 And cur.perfil.id = ?2")
+    public Curtida Curtida_findByIdPostagemAndIdPerfil(Long idPostagem, Long idPerfil);
 }
