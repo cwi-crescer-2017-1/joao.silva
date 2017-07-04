5fa30414a29a1501e4f336f49c133230f2eb65c0
JPedroSSilva <joao.silva@cwi.com.br>
2017-07-04 11:20:00 -0300

    feat(Query): implementação de querys para pegar dados especificos do perfil
    A primeira pega apenas os amigos de um perfil x, a segunda retorna as pessoas as quais o usuario solicitou amizade e a terceira retorna as pessoas que solicitaram amizade para o usuario x

--- Java - Módulo 08/Trabalho Final/ProjetoRedeSocial/src/main/java/br/com/crescer/repository/PerfilRepository.java
+++ Java - Módulo 08/Trabalho Final/ProjetoRedeSocial/src/main/java/br/com/crescer/repository/PerfilRepository.java
@@ -7,6 +7,7 @@ package br.com.crescer.repository;
 
 import br.com.crescer.entity.Perfil;
 import java.util.List;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 /**
@@ -14,5 +15,31 @@ import org.springframework.data.repository.PagingAndSortingRepository;
  */
 public interface PerfilRepository extends PagingAndSortingRepository<Perfil, Long>{
 
+    public List<Perfil> findByNomeStartsWith(String nome);
+    
     public List<Perfil> findByNome(String nome);
+    
+    @Query("select pe from Perfil pe where "
+            + "pe.id "
+            + "In (select re.perfilSolicitado.id from Relacionamento re where re.perfilSolicitante.id = ?1 And re.pendente = 0 And re.resposta = 1) "
+            + "Or "
+            + "pe.id "
+            + "In (select re.perfilSolicitante.id from Relacionamento re where re.perfilSolicitado.id = ?1 And re.pendente = 0 And re.resposta = 1) "
+            + "And pe.id != ?1")
+    List<Perfil> Perfil_fiendFriends(Long idPerfil);
+    
+    @Query("select pe from Perfil pe where pe.id "
+            + "In (select re.perfilSolicitado.id from Relacionamento re where re.perfilSolicitante.id = ?1 "
+            + "And re.resposta = null And re.pendente = true)"
+            + "And pe.id != ?1")
+    List<Perfil> Perfil_fiendPendingFriends(Long idPerfil);
+    //Retorna as pessoas as quais o usuário pediu amizade
+    
+    @Query("select pe from Perfil pe where pe.id "
+            + "In (select re.perfilSolicitante.id from Relacionamento re where re.perfilSolicitado.id = ?1 "
+            + "And re.resposta = null And re.pendente = true)"
+            + "And pe.id != ?1")
+    List<Perfil> Perfil_fiendRequestedFriendship(Long idPerfil);
+    //Retorna as pessoas que pediram amizade ao usuário
+    
 }
