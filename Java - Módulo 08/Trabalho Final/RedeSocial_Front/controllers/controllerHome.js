7fa70ee70d9fda596c337acda14c1246cb40ab80
JPedroSSilva <joao.silva@cwi.com.br>
2017-07-04 11:25:54 -0300

    feat(ControllerHome): implementação de novas funções para buscar dados nas services disponíveis

--- Java - Módulo 08/Trabalho Final/RedeSocial_Front/controllers/controllerHome.js
+++ Java - Módulo 08/Trabalho Final/RedeSocial_Front/controllers/controllerHome.js
@@ -1,4 +1,4 @@
-modulo.controller('controllerHome',['$scope','toastr','$location','authService','servicePostagem','servicePerfil', function(model,toastr,$location,authService,servicePostagem,servicePerfil){
+modulo.controller('controllerHome',['$scope','toastr','$location','authService','servicePostagem','servicePerfil','serviceRelacionamento','serviceComentario','serviceCurtida', function(model,toastr,$location,authService,servicePostagem,servicePerfil,serviceRelacionamento,serviceComentario,serviceCurtida){
     model.perfilCorrente = authService.getUsuario().perfil;
     model.nomeCorrente = model.perfilCorrente.nome;
     model.verComentarios = false;
@@ -8,13 +8,23 @@ modulo.controller('controllerHome',['$scope','toastr','$location','authService',
     let mensagemComentarioEsconder = "Esconder comentários";
     model.botaoComentario = mensagemComentarioMostrar;
     model.pesquisaUsuario = "";
+    model.postagemNova = {};
+    model.postagemNova.texto = "";
+    model.paginaFinal = false;
+    model.telaAtual = "ListaPosts";
+    model.relacionamentos = [];
+    model.amigos = [];
+    model.solicitacoesPendentes = [];
+    model.solicitacoesRecebidas = [];
+
 
     let pagina = 0;
     let tamanhoPagina = 5;
+
     let nomeConfig = function(){
         let nome = model.perfilCorrente.nome;
-        if(nome.length>16){
-             let excesso = nome.length-16;
+        if(nome.length>13){
+             let excesso = nome.length-13;
              model.nomeCorrente = nome.substr(0,(nome.length-excesso));
              model.nomeCorrente = model.nomeCorrente.concat("...");
         }
@@ -22,14 +32,13 @@ modulo.controller('controllerHome',['$scope','toastr','$location','authService',
     nomeConfig();
     model.pesquisar = function(){
         if(typeof model.pesquisaUsuario !== "undefined" && typeof model.pesquisaUsuario !== "null" && model.pesquisaUsuario.replace(/\s{2,}/g, ' ') !== ""){
-        servicePerfil.getFilterByName(model.pesquisaUsuario).then(
+        servicePerfil.getFilterByNameWithFriendship(model.perfilCorrente.id,model.pesquisaUsuario).then(
                     function (response) {
                         model.perfisPesquisados = response.data;
                         if(model.perfisPesquisados.length===0){
                             toastr.info('Nenhum perfil encontrado!');
                         }else{
                             model.mostrarPesquisa = true;
-                            console.log(model.perfisPesquisados);
                         }
                     },
                     function (response) {
@@ -41,9 +50,10 @@ modulo.controller('controllerHome',['$scope','toastr','$location','authService',
     }
     model.retirarPesquisa = function(){
         model.mostrarPesquisa = false;
+        model.pesquisaUsuario = "";
     }
     model.registrarPostagem = function (){
-        if(typeof model.postagemNova.texto.length === "undefined" || typeof model.postagemNova.texto.length === "null" ||model.postagemNova.texto.length<0){
+        if(typeof model.postagemNova.texto === "undefined" || typeof model.postagemNova.texto === "null" || model.postagemNova.texto.length===0){
             toastr.error('Texto da postagem obrigatório!');
         }else{
             servicePostagem.registrar(model.postagemNova.fotoUrl,model.postagemNova.texto,model.perfilCorrente).then(
@@ -81,22 +91,173 @@ modulo.controller('controllerHome',['$scope','toastr','$location','authService',
         servicePostagem.getPage(pagina,tamanhoPagina)
         .then(
             function (response) {
-            model.postagens = response.data.content;
-                    console.log(model.postagens);
+                model.postagens = response.data.content;
+                console.log(model.postagens);
             },
             function (response) {
-            toastr.error('Erro ao carregar as postagens');
-        });
+                toastr.error('Erro ao carregar as postagens');
+            }
+        );
+    }
+    model.carregarMaisPostagens =  function(){
+        pagina = pagina + 1;
+        servicePostagem.getPage(pagina,tamanhoPagina)
+        .then(
+            function (response) {
+                if(response.data.content.length<tamanhoPagina){
+                    model.paginaFinal = true;
+                }
+                let novasPostagens = response.data.content;
+                for(postagem of novasPostagens){
+                    model.postagens.push(postagem);
+                }
+            },
+            function (response) {
+                toastr.error('Erro ao carregar mais postagens');
+            }
+        );
     }
     model.calculoData = function(data){
-        var date1 = (new Date(data-388000));
-        var date2 = new Date();
-        var timeDiff = Math.abs(date2.getTime() - date1.getTime());
-        var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
-        if((timeDiff/(1000 * 3600 * 24)<60)){
-            return 'Há menos de uma hora atrás';
+        let date1 = (new Date(data));
+        let date2 = new Date();
+        let timeDiff = Math.abs(date2.getTime() - date1.getTime());
+        let diffDays = Math.ceil(timeDiff / (1000 * 3600)); 
+        if(diffDays<=1){
+            return 'Há menos de uma horas atrás.';
         }
         return 'Há '+diffDays+' horas atrás.';
     }
+    model.converterHoras = function(data){
+        let data1 = new Date(data);
+        return dataAtualFormatada(data1);
+    }
+    function dataAtualFormatada(data){
+        let dia = data.getDate();
+        if (dia.toString().length == 1)
+        dia = "0"+dia;
+        let mes = data.getMonth()+1;
+        if (mes.toString().length == 1)
+        mes = "0"+mes;
+        let ano = data.getFullYear();  
+        return dia+"/"+mes+"/"+ano;
+    }
+    model.solicitarAmigo = function(perfil){
+        serviceRelacionamento.registrar(model.perfilCorrente,perfil)
+            .then(
+                function (response) {
+                    toastr.success('Solicitado com sucesso!');
+                    model.pesquisar(model.pesquisaUsuario);
+                },
+                function (response) {
+                    toastr.error('Erro ao solicitar amizade!');
+                    model.pesquisaUsuario = "";
+                    model.mostrarPesquisa = false;
+            });        
+    }
+    model.removerAmigo = function(perfil){
+        serviceRelacionamento.removerAmigo(model.perfilCorrente.id,perfil).then(
+            function(response){
+                toastr.success('Removido com sucesso!');
+                model.pesquisar(model.pesquisaUsuario);
+            },
+            function(response){
+                toastr.error('Erro ocorrido ao remover o usuário!');
+            }
+        );
+    };
+    model.aceitarSolicitacao = function(perfil,resposta){
+        serviceRelacionamento.responder(model.perfilCorrente.id,perfil.id,resposta).then(
+            function(response){
+                toastr.success('Aceito com sucesso!');
+                model.pesquisar(model.pesquisaUsuario);
+                model.carregarRelacionamentos();
+            },
+            function(response){
+                toastr.error('Erro ocorrido ao aceitar a solicitação!');
+            }
+        );
+    }
+    model.comentar = function(postagem,comentarioNovo){
+        serviceComentario.salvar(model.perfilCorrente,postagem,comentarioNovo).then(
+            function(response){
+                serviceComentario.getByPostagem(postagem).then(
+                function(response){
+                    postagem.comentarioSet = response.data;
+                },
+                function(response){
+                    postagem.comentarioSet.push(response.data);
+                    toastr.error('Erro ao atualizar comentários!');
+                });
+            },
+            function(response){
+                toastr.error('Erro ao salvar comentário!');
+            }
+        );
+        
+    }
+    model.curtir = function(postagem){
+        serviceCurtida.salvar(model.perfilCorrente,postagem).then(
+            function(response){
+                postagem.curtida = true;
+            },
+            function(response){
+                toastr.error('Erro ao curtir esta postagem!');
+            }
+        );
+    }
+    model.descurtir = function(postagem){
+        serviceCurtida.deletar(model.perfilCorrente,postagem).then(
+            function(response){
+                postagem.curtida = false;
+            },
+            function(response){
+                toastr.error('Erro ao descurtir esta postagem!');
+            }
+        );
+    }
+    model.tela = function(nomeTela){
+        if(model.telaAtual === nomeTela){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    model.trocarTela = function(nomeNovaTela){
+        model.carregarRelacionamentos();
+        model.telaAtual = nomeNovaTela;
+    }
+    model.carregarRelacionamentos = function(){
+        servicePerfil.getFilterByRelation(model.perfilCorrente.id).then(
+            function(response){
+                model.relacionamentos = response.data;
+                model.amigos = [];
+                model.solicitacoesRecebidas = [];
+                model.solicitacoesPendentes = [];
+                for(relacionamento of model.relacionamentos){
+                    if(relacionamento.isFriend){
+                        model.amigos.push(relacionamento);
+                    }else if(relacionamento.isRequested){
+                        model.solicitacoesRecebidas.push(relacionamento);
+                    }else if(relacionamento.pendingRequest){
+                        model.solicitacoesPendentes.push(relacionamento);
+                    }
+                }
+            },
+            function(response){
+                toastr.error('Erro ao carregar amigos!');
+            }
+        );
+    }
+    model.logout = function(){
+        authService.logout().then(
+            function(response){
+                toastr.error('Você saiu com sucesso da aplicação!');
+            },
+            function(response){
+                toastr.error('Erro ao sair da aplicação!');
+            }
+        );
+    }
+    model.carregarRelacionamentos();
     model.getPostagens();
 }]);
